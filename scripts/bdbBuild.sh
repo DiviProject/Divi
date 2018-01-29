ebc0a7aca1a3bc34ae35ba1ce465a2bac47ae88d
David Caldwell <dcaldwell@sanityworx.com>
2018-01-29 12:31:00 -0700

    added ssetup scripts for Ubuntu

---
+++ scripts/bdbBuild.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+# run from directory that contains the tar.gz for BerklyDB.
+#    or delete tar and uncomment wget below to get a new file.
+PIVX_ROOT=$(pwd)
+
+# Pick some path to install BDB to, here we create a directory within the pivx directory
+BDB_PREFIX="${PIVX_ROOT}/db4"
+mkdir -p $BDB_PREFIX
+
+# Fetch the source and verify that it is not tampered with
+# wget 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
+# echo '12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef  db-4.8.30.NC.tar.gz' | sha256sum -c
+# -> db-4.8.30.NC.tar.gz: OK
+# tar -xzvf db-4.8.30.NC.tar.gz
+
+# Build the library and install to our prefix
+cd db-4.8.30.NC/build_unix/
+#  Note: Do a static build so that it can be embedded into the exectuable, instead of having to find a .so at runtime
+../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$BDB_PREFIX
+make install
+
+# Configure PIVX Core to use our own-built instance of BDB
+cd $PIVX_ROOT
+./configure LDFLAGS="-L${BDB_PREFIX}/lib/" CPPFLAGS="-I${BDB_PREFIX}/include/"

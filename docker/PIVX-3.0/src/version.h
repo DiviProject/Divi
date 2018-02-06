d020e6e7892ba7349ab9d1da945a5375571c8940
David Caldwell <dcaldwell@sanityworx.com>
2018-02-06 08:54:37 -0700

    unpacked PIVX-3.0 due to tar upack issues: all items extracted with ._* versions, some recursevely

---
+++ docker/PIVX-3.0/src/version.h
@@ -0,0 +1,40 @@
+// Copyright (c) 2012-2014 The Bitcoin developers
+// Copyright (c) 2014-2015 The Dash developers
+// Copyright (c) 2015-2017 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_VERSION_H
+#define BITCOIN_VERSION_H
+
+/**
+ * network protocol versioning
+ */
+
+static const int PROTOCOL_VERSION = 70912;
+
+//! initial proto version, to be increased after version/verack negotiation
+static const int INIT_PROTO_VERSION = 209;
+
+//! In this version, 'getheaders' was introduced.
+static const int GETHEADERS_VERSION = 70077;
+
+//! disconnect from peers older than this proto version
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70911;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70912;
+
+//! nTime field added to CAddress, starting with this version;
+//! if possible, avoid requesting addresses nodes older than this
+static const int CADDR_TIME_VERSION = 31402;
+
+//! BIP 0031, pong message, is enabled for all versions AFTER this one
+static const int BIP0031_VERSION = 60000;
+
+//! "mempool" command, enhanced "getdata" behavior starts with this version
+static const int MEMPOOL_GD_VERSION = 60002;
+
+//! "filter*" commands are disabled without NODE_BLOOM after and including this version
+static const int NO_BLOOM_VERSION = 70005;
+
+
+#endif // BITCOIN_VERSION_H
--- docker/pivxmake.sh
+++ docker/pivxmake.sh
@@ -1,42 +1,43 @@
 #! /usr/bin/env bash
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "Starting Build Process -------------------------------------------------"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-apt-get -y install libdb4.8-dev libdb4.8++-dev
-apt-get -y install bsdmainutils
-apt-get update
+# echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+# echo "Starting Build Process -------------------------------------------------"
+# echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+# apt-get -y install libdb4.8-dev libdb4.8++-dev
+# apt-get -y install bsdmainutils
+# apt-get update
+# 
+# cd /var/divi
+# tar xzvf PIVX-3.0.tgz
+# 
+# echo "MiniUPnPc Build ......"
+# echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+# cd /var/divi/PIVX-3.0/miniupnpc-2.0
+# make
+# make install
+# sleep 1
+# 
+# echo "Berkeley DB Build ......"
+# echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+# cd /var/divi/PIVX-3.0
+# 
+# 
+# # Pick some path to install BDB to, here we create a directory within the pivx directory
+# BDB_PREFIX="/var/divi/PIVX-3.0/db4"
+# mkdir -p $BDB_PREFIX
+# 
+# # Build the library and install to our prefix
+# cd db-4.8.30.NC/build_unix/
+# #  Note: Do a static build so that it can be embedded into the exectuable, instead of having to find a .so at runtime
+# ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$BDB_PREFIX
+# make install
+# 
+# cd /var/divi/PIVX-3.0
+# #/var/divi/PIVX-3.0/configure LDFLAGS="-L${BDB_PREFIX}/lib/" CPPFLAGS="-I${BDB_PREFIX}/include/"
+# 
+# sleep 1
 
-cd /var/divi
-tar xzvf PIVX-3.0.tgz
-
-echo "MiniUPnPc Build ......"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-cd /var/divi/PIVX-3.0/miniupnpc-2.0
-make
-make install
-sleep 1
-
-echo "Berkeley DB Build ......"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 cd /var/divi/PIVX-3.0
-
-
-# Pick some path to install BDB to, here we create a directory within the pivx directory
-BDB_PREFIX="/var/divi/PIVX-3.0/db4"
-mkdir -p $BDB_PREFIX
-
-# Build the library and install to our prefix
-cd db-4.8.30.NC/build_unix/
-#  Note: Do a static build so that it can be embedded into the exectuable, instead of having to find a .so at runtime
-../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$BDB_PREFIX
-make install
-
-
-cd /var/divi/PIVX-3.0
-/var/divi/PIVX-3.0/configure LDFLAGS="-L${BDB_PREFIX}/lib/" CPPFLAGS="-I${BDB_PREFIX}/include/"
-
-sleep 1
 echo "depends Build ......"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 cd /var/divi/PIVX-3.0/depends
@@ -46,10 +47,24 @@ make install
 sleep 1
 echo "PIVX Build ......"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "AUTOGEN  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 cd /var/divi/PIVX-3.0
 ./autogen.sh
-./configure
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "CONFIGURE ~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+./configure LDFLAGS="-L${BDB_PREFIX}/lib/" CPPFLAGS="-I${BDB_PREFIX}/include/"
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "MAKE      ~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 make
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "INSTALL    ~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 make install
 
 echo "DONE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"

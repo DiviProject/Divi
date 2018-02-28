44473df4773eacfa9ccfda9d613a55f95f991f70
David Caldwell <dcaldwell@sanityworx.com>
2018-02-28 16:46:46 -0700

    fixed cbr/pbr scripts for if clone/pull fails

--- divi/docker/cbr.sh
+++ divi/docker/cbr.sh
@@ -1,16 +1,13 @@
 #! /bin/bash
 PRESENTDIR=$(pwd)
 # try to run clone
-if [ git clone -b "$GITBRANCH" --depth 1 "$GITURI" /shared ] ; then
-  cd /shared/divi/
-  ./autogen.sh
-  ./configure --disable-zerocoin --without-gui --enable-debug
-  make
-  sudo make install
-  mkdir /home/ubuntu/.divx
-  cp /home/ubuntu/divx.conf /home/ubuntu/.divx/divx.conf
-  divxd -debug
-else 
-  echo "clone failed..."
-fi
+git clone -b "$GITBRANCH" --depth 1 "$GITURI" /shared
+cd /shared/divi/
+./autogen.sh
+./configure --disable-zerocoin --without-gui --enable-debug
+make
+sudo make install
+mkdir /home/ubuntu/.divx
+cp /home/ubuntu/divx.conf /home/ubuntu/.divx/divx.conf
+divxd -debug
 cd $PRESENTDIR

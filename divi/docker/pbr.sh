c9559f3eae1f00b90e996ed5190edbb659766ff1
David Caldwell <dcaldwell@sanityworx.com>
2018-03-01 10:05:37 -0700

    delete autom4te.cache folder from master, comment out autogen, configure statements in pbr.sh

--- divi/docker/pbr.sh
+++ divi/docker/pbr.sh
@@ -1,14 +1,8 @@
 #! /bin/bash
-PRESENTDIR={pwd}
-# attempt to pull and then rebuild...
+git pull 
 cd /shared/divi/
-if git pull ; then
-  ./autogen.sh
-  ./configure --disable-zerocoin --without-gui --enable-debug
-  make
-  sudo make install
-  divxd -debug
-else 
-	echo "Could not pull the selected git branch."
-fi
-cd $PRESENTDIR
+# ./autogen.sh
+# ./configure --disable-zerocoin --without-gui --enable-debug
+make
+sudo make install
+divxd -debug

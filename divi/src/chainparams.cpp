2d69588e4a38b3ec8fdeb860035599bf75909fbc
cbyrdsw <cbyrd@sanityworx.com>
2018-03-05 08:07:10 -0700

    fix checkpoints

--- divi/src/chainparams.cpp
+++ divi/src/chainparams.cpp
@@ -227,7 +227,7 @@ public:
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
         fRequireRPCPassword = true;
-        fMiningRequiresPeers = true;
+        fMiningRequiresPeers = false;
         fAllowMinDifficultyBlocks = false;
         fDefaultConsistencyChecks = false;
         fRequireStandard = true;

60b9b70b8c91b8b020864b5548c9ccf6f72b10ed
cbyrdsw <cbyrd@sanityworx.com>
2018-03-07 13:17:08 -0700

    reenable proof of work check

--- divi/src/chainparams.cpp
+++ divi/src/chainparams.cpp
@@ -234,7 +234,7 @@ public:
         fDefaultConsistencyChecks = false;
         fRequireStandard = true;
         fMineBlocksOnDemand = false;
-        fSkipProofOfWorkCheck = true;
+        fSkipProofOfWorkCheck = false;
         fTestnetToBeDeprecatedFieldRPC = false;
         fHeadersFirstSyncingActive = false;
 

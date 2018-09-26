54ec4a4b63d0bd8a1f36dbad21c6da206635fd87
Yura Oleksyshyn <yuraolex@gmail.com>
2018-09-26 15:20:19 +0300

    Updated protocol version

--- divi/src/version.h
+++ divi/src/version.h
@@ -20,7 +20,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70914;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70915;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this

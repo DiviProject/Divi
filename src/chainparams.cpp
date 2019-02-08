7696ea48c1070128de3f29bbded8392f7b975d4e
Yura Oleksyshyn <yuraolex@gmail.com>
2019-02-08 14:51:06 +0200

    Updated segwit and CSV activation times

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -95,13 +95,13 @@ public:
 
         // Deployment of BIP68, BIP112, and BIP113. // TODO
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
-        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1462060800; // May 1st, 2016
-        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 1493596800; // May 1st, 2017
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1580072400; // May 1st, 2016
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = NO_TIMEOUT; // May 1st, 2017
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147) // TODO
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1479168000; // November 15th, 2016.
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1510704000; // November 15th, 2017.
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1580072400; // November 15th, 2016.
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = NO_TIMEOUT; // November 15th, 2017.
 
         // The best chain should have at least this much work. // TODO
 //        consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000028822fef1c230963535a90d");

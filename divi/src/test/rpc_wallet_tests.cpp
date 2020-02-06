385272335302086db9b163d06d2980f2260433b8
galp <the.talion@gmail.com>
2020-02-06 14:05:52 -0700

    Fix rpc wallet signing and verifying issues

--- divi/src/test/rpc_wallet_tests.cpp
+++ divi/src/test/rpc_wallet_tests.cpp
@@ -66,6 +66,8 @@ BOOST_AUTO_TEST_CASE(rpc_wallet)
     Value r;
 
     LOCK2(cs_main, pwalletMain->cs_wallet);
+    ECCVerifyHandle verificationModule;
+    ECC_Start();
 
     CPubKey demoPubkey = pwalletMain->GenerateNewKey();
     CBitcoinAddress demoAddress = CBitcoinAddress(CTxDestination(demoPubkey.GetID()));
@@ -167,7 +169,7 @@ BOOST_AUTO_TEST_CASE(rpc_wallet)
     /* Correct address and signature but wrong message */
     BOOST_CHECK(CallRPC("verifymessage " + demoAddress.ToString() + " " + retValue.get_str() + " wrongmessage").get_bool() == false);
     /* Correct address, message and signature*/
-    BOOST_CHECK(CallRPC("verifymessage " + demoAddress.ToString() + " " + retValue.get_str() + " mymessage").get_bool() == true);
+    BOOST_CHECK(CallRPC("verifymessage " + demoAddress.ToString() + " " + retValue.get_str() + " mymessage").get_bool() == true);    
 
     /*********************************
      * 		getaddressesbyaccount
@@ -177,6 +179,7 @@ BOOST_AUTO_TEST_CASE(rpc_wallet)
     Array arr = retValue.get_array();
     BOOST_CHECK(arr.size() > 0);
     BOOST_CHECK(CBitcoinAddress(arr[0].get_str()).Get() == demoAddress.Get());
+    ECC_Stop();
 }
 
 

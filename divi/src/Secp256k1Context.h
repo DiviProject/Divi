8e25e26ebf780569394f3cbdecfdc51da8620fb3
galp <the.talion@gmail.com>
2020-02-14 08:25:03 -0700

    Make Secp context singleton

--- divi/src/Secp256k1Context.h
+++ divi/src/Secp256k1Context.h
@@ -8,9 +8,15 @@ class Secp256k1Context
 private:
     secp256k1_context* verifying_context = NULL;
     secp256k1_context* signing_context = NULL;
-public:
+
     Secp256k1Context();
     ~Secp256k1Context();
+public:
+    static Secp256k1Context& instance()
+    {
+        static Secp256k1Context uniqueInstance;
+        return uniqueInstance;
+    }
 
     secp256k1_context* GetVerifyContext();
     secp256k1_context* GetSigningContext();

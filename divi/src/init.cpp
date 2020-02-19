9cff15ee5170aaad97be8e14c165a974a9d21a8b
galp <the.talion@gmail.com>
2020-02-19 14:34:43 -0700

    Extract function

--- divi/src/init.cpp
+++ divi/src/init.cpp
@@ -839,14 +839,11 @@ bool InitializeDivi(boost::thread_group& threadGroup)
     }
 
     // Make sure enough file descriptors are available
-    int nBind = std::max((int)mapArgs.count("-bind") + (int)mapArgs.count("-whitebind"), 1);
-    nMaxConnections = GetArg("-maxconnections", 125);
-    nMaxConnections = std::max(std::min(nMaxConnections, (int)(FD_SETSIZE - nBind - MIN_CORE_FILEDESCRIPTORS)), 0);
-    int nFD = RaiseFileDescriptorLimit(nMaxConnections + MIN_CORE_FILEDESCRIPTORS);
-    if (nFD < MIN_CORE_FILEDESCRIPTORS)
-        return InitError(_("Not enough file descriptors available."));
-    if (nFD - MIN_CORE_FILEDESCRIPTORS < nMaxConnections)
-        nMaxConnections = nFD - MIN_CORE_FILEDESCRIPTORS;
+    int nFD;
+    if(!SetMaxConnectionsAndFileDescriptors(nFD))
+    {
+        return false;
+    }
 
     // ********************************************************* Step 3: parameter-to-internal-flags
 

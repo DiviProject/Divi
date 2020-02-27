cef127629c70efe1e49ec14200a6414d8eb3ccf2
galp <the.talion@gmail.com>
2020-02-27 16:12:26 -0700

    Refactor for clarity

--- divi/src/init.cpp
+++ divi/src/init.cpp
@@ -936,6 +936,9 @@ void StartScriptVerificationThreads(boost::thread_group& threadGroup)
     }
 }
 
+
+#ifdef ENABLE_WALLET
+
 void BackupFile(boost::filesystem::path& sourceFile,boost::filesystem::path& backupFile)
 {
 #if BOOST_VERSION >= 158000
@@ -952,6 +955,104 @@ void BackupFile(boost::filesystem::path& sourceFile,boost::filesystem::path& bac
 #endif
 }
 
+bool BackupDatabase()
+{
+    std::string strDataDir = GetDataDir().string();
+    if (!bitdb.Open(strDataDir)) 
+    {
+        // try moving the database env out of the way
+        boost::filesystem::path pathDatabase = GetDataDir() / "database";
+        boost::filesystem::path pathDatabaseBak = GetDataDir() / strprintf("database.%d.bak", GetTime());
+        try {
+            boost::filesystem::rename(pathDatabase, pathDatabaseBak);
+            LogPrintf("Moved old %s to %s. Retrying.\n", pathDatabase.string(), pathDatabaseBak.string());
+        } catch (boost::filesystem::filesystem_error& error) {
+            // failure is ok (well, not really, but it's not worse than what we started with)
+        }
+
+        // try again
+        if (!bitdb.Open(GetDataDir())) {
+            // if it still fails, it probably means we can't even create the database env
+            string msg = strprintf(_("Error initializing wallet database environment %s!"), strDataDir);
+            return InitError(msg);
+        }
+    }
+    return true;
+}
+
+bool VerifyWallet(std::string strWalletFile)
+{
+    std::string strDataDir = GetDataDir().string();
+    if (filesystem::exists(GetDataDir() / strWalletFile)) {
+        CDBEnv::VerifyResult r = bitdb.Verify(strWalletFile, NULL);
+        if (r == CDBEnv::RECOVER_OK) {
+            string msg = strprintf(_("Warning: wallet.dat corrupt, data salvaged!"
+                                        " Original wallet.dat saved as wallet.{timestamp}.bak in %s; if"
+                                        " your balance or transactions are incorrect you should"
+                                        " restore from a backup."),
+                strDataDir);
+            InitWarning(msg);
+        }
+        if (r == CDBEnv::RECOVER_FAIL)
+            return InitError(_("wallet.dat corrupt, salvage failed"));
+    }
+    return true;
+}
+
+void ClearFoldersForResync()
+{
+    uiInterface.InitMessage(_("Preparing for resync..."));
+    // Delete the local blockchain folders to force a resync from scratch to get a consitent blockchain-state
+    filesystem::path blocksDir = GetDataDir() / "blocks";
+    filesystem::path chainstateDir = GetDataDir() / "chainstate";
+    filesystem::path sporksDir = GetDataDir() / "sporks";
+    filesystem::path zerocoinDir = GetDataDir() / "zerocoin";
+    
+    LogPrintf("Deleting blockchain folders blocks, chainstate, sporks and zerocoin\n");
+    // We delete in 4 individual steps in case one of the folder is missing already
+    try {
+        if (filesystem::exists(blocksDir)){
+            boost::filesystem::remove_all(blocksDir);
+            LogPrintf("-resync: folder deleted: %s\n", blocksDir.string().c_str());
+        }
+
+        if (filesystem::exists(chainstateDir)){
+            boost::filesystem::remove_all(chainstateDir);
+            LogPrintf("-resync: folder deleted: %s\n", chainstateDir.string().c_str());
+        }
+
+        if (filesystem::exists(sporksDir)){
+            boost::filesystem::remove_all(sporksDir);
+            LogPrintf("-resync: folder deleted: %s\n", sporksDir.string().c_str());
+        }
+
+        if (filesystem::exists(zerocoinDir)){
+            boost::filesystem::remove_all(zerocoinDir);
+            LogPrintf("-resync: folder deleted: %s\n", zerocoinDir.string().c_str());
+        }
+    } catch (boost::filesystem::filesystem_error& error) {
+        LogPrintf("Failed to delete blockchain folders %s\n", error.what());
+    }
+}
+
+void BackupWalletFile(std::string strWalletFile, filesystem::path backupDir)
+{
+    // Create backup of the wallet
+    std::string dateTimeStr = DateTimeStrFormat(".%Y-%m-%d-%H-%M", GetTime());
+    std::string backupPathStr = backupDir.string();
+    backupPathStr += "/" + strWalletFile;
+    std::string sourcePathStr = GetDataDir().string();
+    sourcePathStr += "/" + strWalletFile;
+    boost::filesystem::path sourceFile = sourcePathStr;
+    boost::filesystem::path backupFile = backupPathStr + dateTimeStr;
+    sourceFile.make_preferred();
+    backupFile.make_preferred();
+    if (boost::filesystem::exists(sourceFile)) {
+        BackupFile(sourceFile,backupFile);
+    }
+}
+#endif
+
 bool BackupWallet(std::string strDataDir, bool fDisableWallet)
 {
 #ifdef ENABLE_WALLET
@@ -965,20 +1066,9 @@ bool BackupWallet(std::string strDataDir, bool fDisableWallet)
         nWalletBackups = GetArg("-createwalletbackups", 10);
         nWalletBackups = std::max(0, std::min(10, nWalletBackups));
         if (nWalletBackups > 0) {
-            if (filesystem::exists(backupDir)) {
-                // Create backup of the wallet
-                std::string dateTimeStr = DateTimeStrFormat(".%Y-%m-%d-%H-%M", GetTime());
-                std::string backupPathStr = backupDir.string();
-                backupPathStr += "/" + strWalletFile;
-                std::string sourcePathStr = GetDataDir().string();
-                sourcePathStr += "/" + strWalletFile;
-                boost::filesystem::path sourceFile = sourcePathStr;
-                boost::filesystem::path backupFile = backupPathStr + dateTimeStr;
-                sourceFile.make_preferred();
-                backupFile.make_preferred();
-                if (boost::filesystem::exists(sourceFile)) {
-                    BackupFile(sourceFile,backupFile);
-                }
+            if (filesystem::exists(backupDir)) 
+            {
+                BackupWalletFile(strWalletFile,backupDir);
                 // Keep only the last 10 backups, including the new one of course
                 typedef std::multimap<std::time_t, boost::filesystem::path> folder_set_t;
                 folder_set_t folder_set;
@@ -1014,76 +1104,14 @@ bool BackupWallet(std::string strDataDir, bool fDisableWallet)
             }
         }
 
-        if (GetBoolArg("-resync", false)) {
-            uiInterface.InitMessage(_("Preparing for resync..."));
-            // Delete the local blockchain folders to force a resync from scratch to get a consitent blockchain-state
-            filesystem::path blocksDir = GetDataDir() / "blocks";
-            filesystem::path chainstateDir = GetDataDir() / "chainstate";
-            filesystem::path sporksDir = GetDataDir() / "sporks";
-            filesystem::path zerocoinDir = GetDataDir() / "zerocoin";
-            
-            LogPrintf("Deleting blockchain folders blocks, chainstate, sporks and zerocoin\n");
-            // We delete in 4 individual steps in case one of the folder is missing already
-            try {
-                if (filesystem::exists(blocksDir)){
-                    boost::filesystem::remove_all(blocksDir);
-                    LogPrintf("-resync: folder deleted: %s\n", blocksDir.string().c_str());
-                }
-
-                if (filesystem::exists(chainstateDir)){
-                    boost::filesystem::remove_all(chainstateDir);
-                    LogPrintf("-resync: folder deleted: %s\n", chainstateDir.string().c_str());
-                }
-
-                if (filesystem::exists(sporksDir)){
-                    boost::filesystem::remove_all(sporksDir);
-                    LogPrintf("-resync: folder deleted: %s\n", sporksDir.string().c_str());
-                }
-
-                if (filesystem::exists(zerocoinDir)){
-                    boost::filesystem::remove_all(zerocoinDir);
-                    LogPrintf("-resync: folder deleted: %s\n", zerocoinDir.string().c_str());
-                }
-            } catch (boost::filesystem::filesystem_error& error) {
-                LogPrintf("Failed to delete blockchain folders %s\n", error.what());
-            }
-        }
+        if (GetBoolArg("-resync", false)) ClearFoldersForResync();
 
         LogPrintf("Using wallet %s\n", strWalletFile);
         uiInterface.InitMessage(_("Verifying wallet..."));
 
-        if (!bitdb.Open(GetDataDir())) {
-            // try moving the database env out of the way
-            boost::filesystem::path pathDatabase = GetDataDir() / "database";
-            boost::filesystem::path pathDatabaseBak = GetDataDir() / strprintf("database.%d.bak", GetTime());
-            try {
-                boost::filesystem::rename(pathDatabase, pathDatabaseBak);
-                LogPrintf("Moved old %s to %s. Retrying.\n", pathDatabase.string(), pathDatabaseBak.string());
-            } catch (boost::filesystem::filesystem_error& error) {
-                // failure is ok (well, not really, but it's not worse than what we started with)
-            }
+        if(!BackupDatabase()) return false;
 
-            // try again
-            if (!bitdb.Open(GetDataDir())) {
-                // if it still fails, it probably means we can't even create the database env
-                string msg = strprintf(_("Error initializing wallet database environment %s!"), strDataDir);
-                return InitError(msg);
-            }
-        }
-
-        if (filesystem::exists(GetDataDir() / strWalletFile)) {
-            CDBEnv::VerifyResult r = bitdb.Verify(strWalletFile, NULL);
-            if (r == CDBEnv::RECOVER_OK) {
-                string msg = strprintf(_("Warning: wallet.dat corrupt, data salvaged!"
-                                         " Original wallet.dat saved as wallet.{timestamp}.bak in %s; if"
-                                         " your balance or transactions are incorrect you should"
-                                         " restore from a backup."),
-                    strDataDir);
-                InitWarning(msg);
-            }
-            if (r == CDBEnv::RECOVER_FAIL)
-                return InitError(_("wallet.dat corrupt, salvage failed"));
-        }
+        if(!VerifyWallet(strWalletFile)) return false;
 
     }  
 #endif // ENABLE_WALLET

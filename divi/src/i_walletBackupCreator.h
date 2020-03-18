c328ce2297ead29b714bced00b0fd9f686f38893
Octal <octal@encke.com>
2020-03-18 10:44:44 -0600

    Wallet Backup Interface

---
+++ divi/src/i_walletBackupCreator.h
@@ -0,0 +1,11 @@
+#ifndef I_WALLETBACKUPCREATOR_H
+#define I_WALLETBACKUPCREATOR_H
+class I_WalletBackupCreator
+{
+public:
+    virtual ~I_WalletBackupCreator(){}
+    virtual bool BackupWallet() = 0;
+    virtual bool CheckWalletIntegrity(bool resync) = 0;
+};
+
+#endif //I_FILESYSTEM_H 

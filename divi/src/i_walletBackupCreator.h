7c3694f9fde35752b78ce803e86bf9ff8476bef8
Octal <octal@encke.com>
2020-03-23 10:24:26 -0600

    Added method to interface

--- divi/src/i_walletBackupCreator.h
+++ divi/src/i_walletBackupCreator.h
@@ -1,11 +1,14 @@
 #ifndef I_WALLETBACKUPCREATOR_H
 #define I_WALLETBACKUPCREATOR_H
+
+#include <string>
 class I_WalletBackupCreator
 {
 public:
     virtual ~I_WalletBackupCreator(){}
     virtual bool BackupWallet() = 0;
     virtual bool CheckWalletIntegrity(bool resync) = 0;
+    virtual std::string GetBackupSubfolderDirectory() const = 0;
 };
 
 #endif //I_WALLETBACKUPCREATOR_H 

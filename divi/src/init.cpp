845ebac3d8a1551950ad725a16070e6dff18c0e0
Octal <octal@encke.com>
2020-03-30 15:19:51 -0600

    Replaced functions with class

--- divi/src/init.cpp
+++ divi/src/init.cpp
@@ -970,66 +970,6 @@ void StartScriptVerificationThreads(boost::thread_group& threadGroup)
 
 #ifdef ENABLE_WALLET
 
-void BackupFile(boost::filesystem::path& sourceFile,boost::filesystem::path& backupFile)
-{
-#if BOOST_VERSION >= 158000
-    try {
-        boost::filesystem::copy_file(sourceFile, backupFile);
-        LogPrintf("Creating backup of %s -> %s\n", sourceFile, backupFile);
-    } catch (boost::filesystem::filesystem_error& error) {
-        LogPrintf("Failed to create backup %s\n", error.what());
-    }
-#else
-    std::ifstream src(sourceFile.string(), std::ios::binary);
-    std::ofstream dst(backupFile.string(), std::ios::binary);
-    dst << src.rdbuf();
-#endif
-}
-
-bool BackupDatabase()
-{
-    std::string strDataDir = GetDataDir().string();
-    if (!CDB::bitdb.Open(strDataDir)) 
-    {
-        // try moving the database env out of the way
-        boost::filesystem::path pathDatabase = GetDataDir() / "database";
-        boost::filesystem::path pathDatabaseBak = GetDataDir() / strprintf("database.%d.bak", GetTime());
-        try {
-            boost::filesystem::rename(pathDatabase, pathDatabaseBak);
-            LogPrintf("Moved old %s to %s. Retrying.\n", pathDatabase.string(), pathDatabaseBak.string());
-        } catch (boost::filesystem::filesystem_error& error) {
-            // failure is ok (well, not really, but it's not worse than what we started with)
-        }
-
-        // try again
-        if (!CDB::bitdb.Open(GetDataDir())) {
-            // if it still fails, it probably means we can't even create the database env
-            string msg = strprintf(_("Error initializing wallet database environment %s!"), strDataDir);
-            return InitError(msg);
-        }
-    }
-    return true;
-}
-
-bool VerifyWallet(std::string strWalletFile)
-{
-    std::string strDataDir = GetDataDir().string();
-    if (boost::filesystem::exists(GetDataDir() / strWalletFile)) {
-        CDBEnv::VerifyResult r = CDB::bitdb.Verify(strWalletFile, NULL);
-        if (r == CDBEnv::RECOVER_OK) {
-            string msg = strprintf(_("Warning: wallet.dat corrupt, data salvaged!"
-                                        " Original wallet.dat saved as wallet.{timestamp}.bak in %s; if"
-                                        " your balance or transactions are incorrect you should"
-                                        " restore from a backup."),
-                strDataDir);
-            InitWarning(msg);
-        }
-        if (r == CDBEnv::RECOVER_FAIL)
-            return InitError(_("wallet.dat corrupt, salvage failed"));
-    }
-    return true;
-}
-
 void ClearFoldersForResync()
 {
     uiInterface.InitMessage(_("Preparing for resync..."));
@@ -1066,58 +1006,6 @@ void ClearFoldersForResync()
     }
 }
 
-void BackupWalletFile(std::string strWalletFile, boost::filesystem::path backupDir)
-{
-    // Create backup of the wallet
-    std::string dateTimeStr = DateTimeStrFormat(".%Y-%m-%d-%H-%M", GetTime());
-    std::string backupPathStr = backupDir.string();
-    backupPathStr += "/" + strWalletFile;
-    std::string sourcePathStr = GetDataDir().string();
-    sourcePathStr += "/" + strWalletFile;
-    boost::filesystem::path sourceFile = sourcePathStr;
-    boost::filesystem::path backupFile = backupPathStr + dateTimeStr;
-    sourceFile.make_preferred();
-    backupFile.make_preferred();
-    if (boost::filesystem::exists(sourceFile)) {
-        BackupFile(sourceFile,backupFile);
-    }
-}
-
-void PruneOldBackups(std::string strWalletFile, boost::filesystem::path backupDir)
-{
-    // Keep only the last 10 backups, including the new one of course
-    typedef std::multimap<std::time_t, boost::filesystem::path> folder_set_t;
-    folder_set_t folder_set;
-    boost::filesystem::directory_iterator end_iter;
-    boost::filesystem::path backupFolder = backupDir.string();
-    backupFolder.make_preferred();
-    // Build map of backup files for current(!) wallet sorted by last write time
-    boost::filesystem::path currentFile;
-    for (boost::filesystem::directory_iterator dir_iter(backupFolder); dir_iter != end_iter; ++dir_iter) {
-        // Only check regular files
-        if (boost::filesystem::is_regular_file(dir_iter->status())) {
-            currentFile = dir_iter->path().filename();
-            // Only add the backups for the current wallet, e.g. wallet.dat.*
-            if (dir_iter->path().stem().string() == strWalletFile) {
-                folder_set.insert(folder_set_t::value_type(boost::filesystem::last_write_time(dir_iter->path()), *dir_iter));
-            }
-        }
-    }
-    // Loop backward through backup files and keep the N newest ones (1 <= N <= 10)
-    int counter = 0;
-    BOOST_REVERSE_FOREACH (PAIRTYPE(const std::time_t, boost::filesystem::path) file, folder_set) {
-        counter++;
-        if (counter > nWalletBackups) {
-            // More than nWalletBackups backups: delete oldest one(s)
-            try {
-                boost::filesystem::remove(file.second);
-                LogPrintf("Old backup deleted: %s\n", file.second);
-            } catch (boost::filesystem::filesystem_error& error) {
-                LogPrintf("Failed to delete backup %s\n", error.what());
-            }
-        }
-    }
-}
 #endif
 bool BackupWallet(std::string strDataDir, bool fDisableWallet)
 {

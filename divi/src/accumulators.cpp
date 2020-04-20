c2cb954b8e772e80b57e3ee5a845a5ca77fe970e
galp <the.talion@gmail.com>
2020-04-20 12:42:31 -0600

    Remove unused file

--- divi/src/accumulators.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2017 The PIVX Developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
-bool ThisDoesNothing()
-{
-    return false;
-}

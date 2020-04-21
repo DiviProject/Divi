3ae01617cb0a738b2bb775e9a9030f0bef38e56d
galp <the.talion@gmail.com>
2020-04-21 11:14:38 -0600

    Remove unused class

--- divi/src/txdb.h
+++ divi/src/txdb.h
@@ -86,25 +86,4 @@ public:
     bool LoadBlockIndexGuts();
 };
 
-class CZerocoinDB : public CLevelDBWrapper
-{
-public:
-    CZerocoinDB(size_t nCacheSize, bool fMemory = false, bool fWipe = false);
-
-private:
-    CZerocoinDB(const CZerocoinDB&);
-    void operator=(const CZerocoinDB&);
-
-public:
-    bool WriteCoinMint(const libzerocoin::PublicCoin& pubCoin, const uint256& txHash);
-    bool ReadCoinMint(const CBigNum& bnPubcoin, uint256& txHash);
-    bool WriteCoinSpend(const CBigNum& bnSerial, const uint256& txHash);
-    bool ReadCoinSpend(const CBigNum& bnSerial, uint256& txHash);
-    bool EraseCoinMint(const CBigNum& bnPubcoin);
-    bool EraseCoinSpend(const CBigNum& bnSerial);
-    bool WriteAccumulatorValue(const uint32_t& nChecksum, const CBigNum& bnValue);
-    bool ReadAccumulatorValue(const uint32_t& nChecksum, CBigNum& bnValue);
-    bool EraseAccumulatorValue(const uint32_t& nChecksum);
-};
-
 #endif // BITCOIN_TXDB_H

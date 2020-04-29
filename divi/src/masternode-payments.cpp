ae9e4f4541db1fd74b25647ba7efa3a1695623a6
galp <the.talion@gmail.com>
2020-04-29 15:04:37 -0600

    Remove dead code

--- divi/src/masternode-payments.cpp
+++ divi/src/masternode-payments.cpp
@@ -463,22 +463,10 @@ bool CMasternodeBlockPayees::IsTransactionValid(const CTransaction& txNew)
     LOCK(cs_vecPayments);
 
     int nMaxSignatures = 0;
-    int nMasternode_Drift_Count = 0;
 
     std::string strPayeesPossible = "";
 
     auto rewards = GetBlockSubsidity(nBlockHeight);
-
-    if (sporkManager.IsSporkActive(SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT)) {
-        // Get a stable number of masternodes by ignoring newly activated (< 8000 sec old) masternodes
-        nMasternode_Drift_Count = mnodeman.stable_size() + Params().MasternodeCountDrift();
-    }
-    else {
-        //account for the fact that all peers do not see the same masternode count. A allowance of being off our masternode count is given
-        //we only need to look at an increased masternode count because as count increases, the reward decreases. This code only checks
-        //for mnPayment >= required, so it only makes sense to check the max node count allowed.
-        nMasternode_Drift_Count = mnodeman.size() + Params().MasternodeCountDrift();
-    }
 
     CAmount requiredMasternodePayment = rewards.nMasternodeReward;
 

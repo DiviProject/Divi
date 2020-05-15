3cf7519367b4077118243a4426883c74db64e04d
galp <the.talion@gmail.com>
2020-05-15 13:17:32 -0600

    Fix windows compilation issue

--- divi/src/random.cpp
+++ divi/src/random.cpp
@@ -14,6 +14,7 @@
 #include "compat.h"
 #include "Logging.h"             // for LogPrint()
 #include "utilstrencodings.h" // for GetTime()
+#include "utiltime.h"
 
 #include <stdlib.h>
 #include <limits>

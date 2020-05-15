25f2e2a299bf713bd9339a7e8a020ef8129f085a
galp <the.talion@gmail.com>
2020-05-15 13:58:55 -0600

    Move declaration

--- divi/src/util.h
+++ divi/src/util.h
@@ -66,9 +66,6 @@ boost::filesystem::path GetPidFile();
 void CreatePidFile(const boost::filesystem::path& path, pid_t pid);
 #endif
 void ReadConfigFile(std::map<std::string, std::string>& mapSettingsRet, std::map<std::string, std::vector<std::string> >& mapMultiSettingsRet);
-#ifdef WIN32
-boost::filesystem::path GetSpecialFolderPath(int nFolder, bool fCreate = true);
-#endif
 boost::filesystem::path GetTempPath();
 void ShrinkDebugFile();
 void runCommand(std::string strCommand);

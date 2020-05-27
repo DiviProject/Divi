1ea0b7539f07ded6cae481407dcf1a5f209e1195
galp <the.talion@gmail.com>
2020-05-27 11:16:55 -0600

    Remove non-copyright PIVX references

--- divi/src/spork.cpp
+++ divi/src/spork.cpp
@@ -27,7 +27,7 @@ CSporkManager sporkManager;
 static std::map<int, std::string> mapSporkDefaults = {
     {SPORK_2_SWIFTTX_ENABLED,                "0"},             // ON
     {SPORK_3_SWIFTTX_BLOCK_FILTERING,        "0"},             // ON
-    {SPORK_5_INSTANTSEND_MAX_VALUE,          "1000"},          // 1000 PIVX
+    {SPORK_5_INSTANTSEND_MAX_VALUE,          "1000"},          // 1000 DIVI
     {SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT, "1537971708"},    // ON
     {SPORK_9_SUPERBLOCKS_ENABLED,            "4070908800"},    // OFF
     {SPORK_10_MASTERNODE_PAY_UPDATED_NODES,  "4070908800"},    // OFF

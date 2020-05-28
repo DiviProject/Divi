cd123821f0f37de7eec0945912cd97852d2cbb69
galp <the.talion@gmail.com>
2020-05-28 15:53:29 -0600

    Refactor to allow for backward compatibility testing

--- divi/src/SuperblockHelpers.cpp
+++ divi/src/SuperblockHelpers.cpp
@@ -1,14 +1,25 @@
 #include <SuperblockHelpers.h>
 #include <chainparams.h>
 
-bool IsValidLotteryBlockHeight(int nBlockHeight)
+bool OldIsValidLotteryBlockHeight(int nBlockHeight)
 {
     return nBlockHeight >= Params().GetLotteryBlockStartBlock() &&
             ((nBlockHeight % Params().GetLotteryBlockCycle()) == 0);
 }
 
-bool IsValidTreasuryBlockHeight(int nBlockHeight)
+bool OldIsValidTreasuryBlockHeight(int nBlockHeight)
 {
     return nBlockHeight >= Params().GetTreasuryPaymentsStartBlock() &&
             ((nBlockHeight % Params().GetTreasuryPaymentsCycle()) == 0);
 }
+
+bool IsValidLotteryBlockHeight(int nBlockHeight)
+{
+    return OldIsValidLotteryBlockHeight(nBlockHeight);
+}
+
+bool IsValidTreasuryBlockHeight(int nBlockHeight)
+{
+    return OldIsValidTreasuryBlockHeight(nBlockHeight);
+}
+
--- divi/src/SuperblockHelpers.h
+++ divi/src/SuperblockHelpers.h
@@ -1,5 +1,7 @@
 #ifndef SUPERBLOCK_HELPERS_H
 #define SUPERBLOCK_HELPERS_H
+bool OldIsValidLotteryBlockHeight(int nBlockHeight);
+bool OldIsValidTreasuryBlockHeight(int nBlockHeight);
 bool IsValidLotteryBlockHeight(int nBlockHeight);
 bool IsValidTreasuryBlockHeight(int nBlockHeight);
 #endif // SUPERBLOCK_HELPERS_H

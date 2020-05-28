cf7d4213fada52c9502905f827cb79732fb294eb
galp <the.talion@gmail.com>
2020-05-28 15:52:55 -0600

    Failing test

---
+++ divi/src/test/SuperblockHelper_tests.cpp
@@ -0,0 +1,42 @@
+#include <test/test_only.h>
+#include <SuperblockHelpers.h>
+#include <chainparams.h>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(SuperblockHelper_tests)
+
+
+int getMaxBlockConflictHeight()
+{
+    int blockLotteryStart = Params().GetLotteryBlockStartBlock();
+    int lotteryCycleLength = Params().GetLotteryBlockCycle();
+    int blockTreasuryStart = Params().GetTreasuryPaymentsStartBlock();
+    int treasuryCycleLength = Params().GetTreasuryPaymentsCycle();
+    
+    return (lotteryCycleLength+blockLotteryStart)*(treasuryCycleLength+blockTreasuryStart);
+}
+void CheckForBlockTypeConflicts()
+{
+    for(int blockHeight = 0; blockHeight <= getMaxBlockConflictHeight(); blockHeight++)
+    {
+        if(IsValidLotteryBlockHeight(blockHeight) && IsValidTreasuryBlockHeight(blockHeight))
+        {
+            BOOST_CHECK_MESSAGE(false, "A treasury block cannot also be a lottery block! Failure at height " << blockHeight << "!" );
+            break;
+        }
+    }
+}
+BOOST_AUTO_TEST_CASE(willNeverAssertAHeightIsValidForBothTreasuryAndLottery)
+{
+    {
+        SelectParams(CBaseChainParams::MAIN);
+        CheckForBlockTypeConflicts();
+    }
+    {
+        SelectParams(CBaseChainParams::TESTNET);
+        CheckForBlockTypeConflicts();
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()

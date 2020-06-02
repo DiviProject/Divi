b7aa9275311d06e97160c01b0c1592bd37b4412b
galp <the.talion@gmail.com>
2020-06-02 07:44:04 -0600

    Update makefile & create mock for tests

---
+++ divi/src/test/MockSuperblockHeightValidator.h
@@ -0,0 +1,13 @@
+#ifndef MOCK_SUPERBLOCK_HEIGHT_VALIDATOR_H
+#define MOCK_SUPERBLOCK_HEIGHT_VALIDATOR_H
+#include <I_SuperblockHeightValidator.h>
+
+#include <gmock/gmock.h>
+
+class MockSuperblockHeightValidator: public I_SuperblockHeightValidator
+{
+public:
+    MOCK_CONST_METHOD1(IsValidLotteryBlockHeight, bool(int nBlockHeight));
+    MOCK_CONST_METHOD1(IsValidTreasuryBlockHeight, bool(int nBlockHeight));
+};
+#endif // MOCK_SUPERBLOCK_HEIGHT_VALIDATOR_H

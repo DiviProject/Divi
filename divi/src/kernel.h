f24613a17d5a2359f2ce0e3aefbbef698213afa5
galp <the.talion@gmail.com>
2020-07-01 13:05:30 -0600

    Remove irrelevant extern & place 1 parameter per line

--- divi/src/kernel.h
+++ divi/src/kernel.h
@@ -16,7 +16,6 @@ class CBlockIndex;
 static const unsigned int MODIFIER_INTERVAL = 60;
 static const unsigned int MODIFIER_INTERVAL_TESTNET = 60;
 static const unsigned int MAX_KERNEL_COMBINED_INPUTS = 20;
-extern unsigned int nModifierInterval;
 extern unsigned int getIntervalVersion(bool fTestNet);
 
 // MODIFIER_INTERVAL_RATIO:
@@ -24,28 +23,58 @@ extern unsigned int getIntervalVersion(bool fTestNet);
 static const int MODIFIER_INTERVAL_RATIO = 3;
 
 // Compute the hash modifier for proof-of-stake
-bool ComputeNextStakeModifier(const CBlockIndex* pindexPrev, uint64_t& nStakeModifier, bool& fGeneratedStakeModifier);
+bool ComputeNextStakeModifier(
+    const CBlockIndex* pindexPrev,
+    uint64_t& nStakeModifier,
+    bool& fGeneratedStakeModifier);
 
 // Check whether stake kernel meets hash target
 // Sets hashProofOfStake on success return
-uint256 stakeHash(unsigned int nTimeTx, CDataStream ss, unsigned int prevoutIndex, uint256 prevoutHash, unsigned int nTimeBlockFrom);
-bool stakeTargetHit(uint256 hashProofOfStake, int64_t nValueIn, uint256 bnTargetPerCoinDay, int64_t nTimeWeight);
-bool CheckStakeKernelHash(unsigned int nBits, const CBlock blockFrom, const CTransaction txPrev, const COutPoint prevout, unsigned int& nTimeTx, unsigned int nHashDrift, bool fCheck, uint256& hashProofOfStake, bool fPrintProofOfStake = false);
+uint256 stakeHash(
+    unsigned int nTimeTx,
+    CDataStream ss,
+    unsigned int prevoutIndex,
+    uint256 prevoutHash,
+    unsigned int nTimeBlockFrom);
+bool stakeTargetHit(
+    uint256 hashProofOfStake,
+    int64_t nValueIn,
+    uint256 bnTargetPerCoinDay,
+    int64_t nTimeWeight);
+bool CheckStakeKernelHash(
+    unsigned int nBits,
+    const CBlock blockFrom,
+    const CTransaction txPrev,
+    const COutPoint prevout,
+    unsigned int& nTimeTx,
+    unsigned int nHashDrift,
+    bool fCheck,
+    uint256& hashProofOfStake,
+    bool fPrintProofOfStake = false);
 
 // Check kernel hash target and coinstake signature
 // Sets hashProofOfStake on success return
-bool CheckProofOfStake(const CBlock block, uint256& hashProofOfStake);
+bool CheckProofOfStake(
+    const CBlock block,
+    uint256& hashProofOfStake);
 
 // Check whether the coinstake timestamp meets protocol
-bool CheckCoinStakeTimestamp(int64_t nTimeBlock, int64_t nTimeTx);
+bool CheckCoinStakeTimestamp(
+    int64_t nTimeBlock,
+    int64_t nTimeTx);
 
 // Get stake modifier checksum
-unsigned int GetStakeModifierChecksum(const CBlockIndex* pindex);
+unsigned int GetStakeModifierChecksum(
+    const CBlockIndex* pindex);
 
 // Check stake modifier hard checkpoints
-bool CheckStakeModifierCheckpoints(int nHeight, unsigned int nStakeModifierChecksum);
+bool CheckStakeModifierCheckpoints(
+    int nHeight,
+    unsigned int nStakeModifierChecksum);
 
 // Get time weight using supplied timestamps
-int64_t GetWeight(int64_t nIntervalBeginning, int64_t nIntervalEnd);
+int64_t GetWeight(
+    int64_t nIntervalBeginning,
+    int64_t nIntervalEnd);
 
 #endif // BITCOIN_KERNEL_H

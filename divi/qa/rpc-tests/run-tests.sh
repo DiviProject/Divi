3f0a00bacf818296d568e307d06cbc613f2406b9
Daniel Kraft <d@domob.eu>
2020-07-21 15:56:30 +0200

    Fix the wallet.py regtest.
    This updates the wallet.py regtest (qa/rpc-tests/wallet.py) so it
    actually passes with Divi.  For this, some numbers and assumptions
    have to be tweaked slightly, and some options set.
    
    Mostly, we adjust the data to the block reward of 1250 coins in Divi,
    and also to the fact that fees are burnt and not given to the miner.

---
+++ divi/qa/rpc-tests/run-tests.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# This is a convenience script that runs all regtests that
+# are supposed to work for Divi.  It is essentially a "variant" of
+# qa/pull-tester/rpc-tests.sh that is not meant to be run automatically
+# but just manually during development.
+
+set -ex
+
+./wallet.py

2d124caa600716b29733f56732f294e5aefebb98
Daniel Kraft <d@domob.eu>
2020-07-30 17:05:26 +0200

    squash: fixes to PowToPosTransition

--- divi/qa/rpc-tests/PowToPosTransition.py
+++ divi/qa/rpc-tests/PowToPosTransition.py
@@ -17,33 +17,29 @@ from datetime import datetime
 
 def createPoSStacks(node,node0_address,blocksToMine):
     node.setgenerate(True,blocksToMine)
-    node.sendtoaddress(node0_address,60000.0)
-    return True
+    node.sendtoaddress(node0_address,30000.0)
 
 class PowToPosTransitionTest(BitcoinTestFramework):
 
     def setup_network(self):
         self.nodes = []
-        self.options.nocleanup = True
         self.nodes.append(start_node(0, self.options.tmpdir))
         self.is_network_split = False
 
     def run_test(self):
         targetNumberOfBlocks = 101
-        try:
-            node = self.nodes[0]
-            node0_address = node.getnewaddress("")
-            for j in range(2):
-                createPoSStacks(node,node0_address,50)
-            set_node_times(self.nodes, int(datetime.utcnow().strftime('%s')) + 60*60)
-            while True:
-                node.setgenerate(True,1)
-                if node.getblockcount() == targetNumberOfBlocks:
-                    break
-        except Exception as e:
-            print('Error: {0}'.format(str(e)))
+        node = self.nodes[0]
+        node0_address = node.getnewaddress("")
 
-        self.sync_all()
+        for j in range(2):
+            createPoSStacks(node,node0_address,50)
+        set_node_times(self.nodes, int(datetime.utcnow().strftime('%s')) + 60*60)
+
+        missing = targetNumberOfBlocks - node.getblockcount()
+        assert missing > 0
+        node.setgenerate(True, missing)
+
         assert_equal(self.nodes[0].getblockcount(), targetNumberOfBlocks)
+
 if __name__ == '__main__':
     PowToPosTransitionTest().main()

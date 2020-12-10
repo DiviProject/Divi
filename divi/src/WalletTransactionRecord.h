6fca25d2d649f74ec01e3fde6907461135699f0d
Daniel Kraft <d@domob.eu>
2020-12-10 15:15:38 +0100

    Bare txid for GetTransaction / GetWalletTx.
    This extends the two functions GetWalletTx and GetTransaction to (also)
    find transactions by bare txid, not just by the normal txid.
    
    These methods are mainly used in places where we need to look up e.g.
    the previous transaction to a spend, so that we can know the address
    that is being spent or the value of the input.
    
    The change is fine to do (won't cause any extra consensus changes)
    because all it does is make those methods return the correct previous
    transaction (for after the fork) in cases where they would have
    failed otherwise (since both are SHA-256d hashes and thus cannot have
    collisions).  Also actual checks that some spent coin actually exists
    are the explicitly on the consensus-level anyway.

--- divi/src/WalletTransactionRecord.h
+++ divi/src/WalletTransactionRecord.h
@@ -10,12 +10,20 @@ private:
     CCriticalSection& cs_walletTxRecord;
     const std::string walletFilename_;
     const bool databaseWritesAreDisallowed_;
+
+    /** Map from the bare txid of transactions in the wallet to the matching
+     *  transactions themselves.  */
+    std::map<uint256, const CWalletTx*> mapBareTxid;
+
 public:
     std::map<uint256, CWalletTx> mapWallet;
 
     WalletTransactionRecord(CCriticalSection& requiredWalletLock,const std::string& walletFilename);
     WalletTransactionRecord(CCriticalSection& requiredWalletLock);
     const CWalletTx* GetWalletTx(const uint256& hash) const;
+
+    /** Tries to look up a transaction in the wallet, either by hash (txid) or
+     *  the bare txid that is used after segwit-light to identify outputs.  */
     std::vector<const CWalletTx*> GetWalletTransactionReferences() const;
     std::pair<std::map<uint256, CWalletTx>::iterator, bool> AddTransaction(const CWalletTx& newlyAddedTransaction);
     void UpdateMetadata(const uint256& hashOfTransactionToUpdate, const CWalletTx& updatedTransaction, bool updateDiskAndTimestamp,bool writeToWalletDb=false);

15f79c291149f64f58941dd5e6536539c1682a4b
galp <the.talion@gmail.com>
2020-12-15 13:06:39 -0700

    Update test to separate vault funding/staking from block generation

--- divi/qa/rpc-tests/StakingVaultStaking.py
+++ divi/qa/rpc-tests/StakingVaultStaking.py
@@ -23,29 +23,35 @@ def createVaultPoSStacks(ownerNodes,vaultNode):
     Ideally this should be done right on top of a clean chain."""
 
     # Make sure all nodes have matured coins.
+    minting_node = ownerNodes[0]
+    owner_node = ownerNodes[1]
+
     all_nodes = ownerNodes[:]
     all_nodes.append(vaultNode)
-    for node in ownerNodes:
-      node.setgenerate(True, 40)
-      sync_blocks(all_nodes)
-    ownerNodes[0].setgenerate(True, 21)
+
+    owner_node.setgenerate(True, 40)
+    sync_blocks(all_nodes)
+    minting_node.setgenerate(True, 21)
+    sync_blocks(all_nodes)
 
     # Split those coins up into many pieces that can each be used
     # individually for staking.
     parts = 40
-    value = 400
+    value = 1249.865
     funding_data = []
     vault_node_address = vaultNode.getnewaddress()
-    for node in ownerNodes:
-      assert node.getbalance() > parts*value
+    vault_encoding = ""
+    for node in [owner_node]:
+      assert node.getbalance() >= parts*value, "Result: {}".format(node.getbalance())
       node_address=node.getnewaddress()
       vault_encoding = node_address+":"+vault_node_address
       for _ in range(parts):
         funding_data.append( node.fundvault(vault_encoding,value) )
+
     # Make sure to get all those transactions mined.
     sync_mempools(all_nodes)
-    while len(ownerNodes[0].getrawmempool()) > 0:
-      ownerNodes[0].setgenerate(True, 1)
+    while len(minting_node.getrawmempool()) > 0:
+      minting_node.setgenerate(True, 1)
     sync_blocks(all_nodes)
     for funding_datum in funding_data:
       assert vaultNode.addvaultscript(funding_datum["vault"],funding_datum["txhash"])["succeeded"]
@@ -56,15 +62,20 @@ class StakingVaultStakingTest(BitcoinTestFramework):
         self.nodes = []
         self.nodes.append(start_node(0, self.options.tmpdir,["-debug","-autocombine=0"]))
         self.nodes.append(start_node(1, self.options.tmpdir,["-debug","-autocombine=0"]))
+        self.nodes.append(start_node(2, self.options.tmpdir,["-debug","-autocombine=0"]))
+
         stakingVaultForkActivationTime = 2000000000
         set_node_times(self.nodes,stakingVaultForkActivationTime)
         connect_nodes_bi(self.nodes, 0, 1)
+        connect_nodes_bi(self.nodes, 0, 2)
+        connect_nodes_bi(self.nodes, 2, 1)
         self.is_network_split = False
 
     def run_test(self):
         posStart = 100
         targetNumberOfBlocks = posStart + 100
-        firstOwnerNode = self.nodes[0]
+        minting_node = self.nodes[0]
+        owner_node = self.nodes[1]
 
         print ("Setting up PoS coins...")
         ownerNodes = self.nodes[:-1]
@@ -73,25 +84,22 @@ class StakingVaultStakingTest(BitcoinTestFramework):
         self.sync_all()
 
         print ("Mining remaining PoW blocks...")
-        missing = posStart - firstOwnerNode.getblockcount()
+        missing = posStart - minting_node.getblockcount()
         assert missing > 0
-        firstOwnerNode.setgenerate(True, missing)
+        minting_node.setgenerate(True, missing)
         self.sync_all()
 
         print ("Vault trying to mine PoS blocks now...")
-        missing = targetNumberOfBlocks - firstOwnerNode.getblockcount()
+        missing = targetNumberOfBlocks - minting_node.getblockcount()
         assert missing > 0
-        balanceBeforeVaultStaking = vaultNode.getcoinavailability()["Stakable"]
         generatePoSBlocks(self.nodes, -1, missing)
-        generatePoSBlocks(self.nodes, 0, 20)
 
-        assert_equal(vaultNode.getblockcount(), targetNumberOfBlocks+20)
-        balanceAfterVaultStaking = balanceBeforeVaultStaking + missing*456
-        assert_near(balanceAfterVaultStaking, vaultNode.getcoinavailability()["Stakable"],1000)
+        assert_equal(vaultNode.getblockcount(), targetNumberOfBlocks)
         fee = 100
-        expectedUnconfirmedBalance = vaultNode.getcoinavailability()["Stakable"]-fee
-        firstOwnerNode.reclaimvaultfunds(firstOwnerNode.getnewaddress(),expectedUnconfirmedBalance)
-        assert_equal(firstOwnerNode.getwalletinfo()["unconfirmed_balance"],expectedUnconfirmedBalance)
+        assert_equal(owner_node.getwalletinfo()["unconfirmed_balance"],0)
+        expectedMinimumUnconfirmedBalance = vaultNode.getcoinavailability()["Stakable"]-fee
+        owner_node.reclaimvaultfunds(owner_node.getnewaddress(),expectedMinimumUnconfirmedBalance)
+        assert_greater_than(owner_node.getwalletinfo()["unconfirmed_balance"],expectedMinimumUnconfirmedBalance)
 
 if __name__ == '__main__':
     StakingVaultStakingTest().main()

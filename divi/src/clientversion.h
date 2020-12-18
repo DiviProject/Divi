4818f766b49ae3b84ec7e3af43e3a250ceb721d1
galp <the.talion@gmail.com>
2020-12-17 18:55:51 -0700

    Update version number to 2.0.0

--- divi/src/clientversion.h
+++ divi/src/clientversion.h
@@ -10,10 +10,10 @@
 #include "config/divi-config.h"
 #else
 //! These need to be macros, as clientversion.cpp's and divi*-res.rc's voodoo requires it
-#define CLIENT_VERSION_MAJOR 1
-#define CLIENT_VERSION_MINOR 01
-#define CLIENT_VERSION_REVISION 2
-#define CLIENT_VERSION_BUILD 00
+#define CLIENT_VERSION_MAJOR 2
+#define CLIENT_VERSION_MINOR 0
+#define CLIENT_VERSION_REVISION 0
+#define CLIENT_VERSION_BUILD 0
 
 #define CLIENT_VERSION_IS_RELEASE true		// Todo: !! Set to true for release, false for prerelease or test build
 #define COPYRIGHT_YEAR 2018					// Todo: update this when changing our copyright comments in the source
@@ -53,7 +53,7 @@ extern const std::string CLIENT_BUILD;
 extern const std::string CLIENT_DATE;
 
 const std::string CLIENT_NAME_STR("DIVI Core");
-const std::string CLIENT_VERSION_STR("1.1.2.0");
+const std::string CLIENT_VERSION_STR("2.0.0.0");
 
 
 std::string FormatFullVersion();

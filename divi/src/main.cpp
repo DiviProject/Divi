8d6824335a3c7a3442ad4d67510b1cd6123887e6
galp <the.talion@gmail.com>
2021-01-05 15:47:45 -0700

    Inject mnSync,mnPayments dependency into masternode manager

--- divi/src/main.cpp
+++ divi/src/main.cpp
@@ -4852,7 +4852,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
     } else {
         //probably one the extensions
         // obfuScationPool.ProcessMessageObfuscation(pfrom, strCommand, vRecv);
-        mnodeman.ProcessMessage(pfrom, strCommand, vRecv);
+        mnodeman.ProcessMessage(masternodePayments,masternodeSync,pfrom, strCommand, vRecv);
         // budget.ProcessMessage(pfrom, strCommand, vRecv);
         masternodePayments.ProcessMessageMasternodePayments(pfrom, strCommand, vRecv);
         // ProcessMessageSwiftTX(pfrom, strCommand, vRecv);

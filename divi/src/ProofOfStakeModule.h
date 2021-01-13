629832f1a68f99dc6e4e1a80803e2ef9fd5c1f7d
galp <the.talion@gmail.com>
2021-01-13 14:51:38 -0700

    Module for creating/verifying PoS

---
+++ divi/src/ProofOfStakeModule.h
@@ -0,0 +1,24 @@
+#ifndef PROOF_OF_STAKE_MODULE_H
+#define PROOF_OF_STAKE_MODULE_H
+#include <memory>
+class I_PoSStakeModifierService;
+class I_PoSStakeModifierService;
+class I_ProofOfStakeGenerator;
+class CChainParams;
+class CChain;
+class BlockMap;
+
+class ProofOfStakeModule
+{
+    std::unique_ptr<I_PoSStakeModifierService> legacyStakeModifierService_;
+    std::unique_ptr<I_PoSStakeModifierService> stakeModifierService_;
+    std::unique_ptr<I_ProofOfStakeGenerator> proofGenerator_;
+public:
+    ProofOfStakeModule(
+        const CChainParams& chainParameters,
+        CChain& activeChain,
+        BlockMap& blockIndexByHash);
+    ~ProofOfStakeModule();
+    I_ProofOfStakeGenerator& proofOfStakeGenerator();
+};
+#endif// PROOF_OF_STAKE_MODULE_H

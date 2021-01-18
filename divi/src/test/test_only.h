c3f361af30414ccada8fce9060442c0e3211bb3c
galp <the.talion@gmail.com>
2021-01-18 16:19:37 -0700

    Multiple wallet instances

--- divi/src/test/test_only.h
+++ divi/src/test/test_only.h
@@ -3,4 +3,5 @@
 #define TEST_ONLY_H
 #include <boost/test/unit_test.hpp>
 #define SKIP_TEST *boost::unit_test::disabled()
-#endif
+#define BOOST_CHECK_EQUAL_MESSAGE(L, R, M)      { BOOST_TEST_INFO(M); BOOST_CHECK_EQUAL(L, R); }
+#endif //TEST_ONLY_H

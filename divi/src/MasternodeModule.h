fc65db2ea29a221d7e833677507079aa738f5c59
galp <the.talion@gmail.com>
2021-01-29 11:52:20 -0700

    Move parameter setting of active masternode into module

--- divi/src/MasternodeModule.h
+++ divi/src/MasternodeModule.h
@@ -1,4 +1,7 @@
 #ifndef MASTERNODE_MODULE_H
 #define MASTERNODE_MODULE_H
+#include <string>
+class Settings;
 void ThreadMasternodeBackgroundSync();
+bool SetupActiveMasternode(const Settings& settings, std::string& errorMessage);
 #endif //MASTERNODE_MODULE_H

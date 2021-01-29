b52bdb130403d2612c431b44352085eb0c03f72d
galp <the.talion@gmail.com>
2021-01-29 14:03:18 -0700

    Move loop into MasternodeModule

--- divi/src/MasternodeModule.h
+++ divi/src/MasternodeModule.h
@@ -1,7 +1,10 @@
 #ifndef MASTERNODE_MODULE_H
 #define MASTERNODE_MODULE_H
 #include <string>
+#include <primitives/transaction.h>
+#include <functional>
 class Settings;
 void ThreadMasternodeBackgroundSync();
 bool SetupActiveMasternode(const Settings& settings, std::string& errorMessage);
+void LockUpMasternodeCollateral(const Settings& settings, std::function<void(const COutPoint&)> walletUtxoLockingFunction);
 #endif //MASTERNODE_MODULE_H

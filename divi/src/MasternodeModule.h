b65852ac40f8d97a4713b804d5f4e928f27e094e
galp <the.talion@gmail.com>
2021-02-01 14:27:49 -0700

    Encapsulate masternode message processing calls

--- divi/src/MasternodeModule.h
+++ divi/src/MasternodeModule.h
@@ -5,8 +5,11 @@
 #include <functional>
 class Settings;
 class CBlockIndex;
+class CDataStream;
+class CNode;
 void ThreadMasternodeBackgroundSync();
 bool SetupActiveMasternode(const Settings& settings, std::string& errorMessage);
 void LockUpMasternodeCollateral(const Settings& settings, std::function<void(const COutPoint&)> walletUtxoLockingFunction);
 bool VoteForMasternodePayee(const CBlockIndex* pindex);
+void ProcessMasternodeMessages(CNode* pfrom, std::string strCommand, CDataStream& vRecv);
 #endif //MASTERNODE_MODULE_H

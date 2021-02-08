8fcd17b927de95ee23eda1fc45935d0480fb78d6
galp <the.talion@gmail.com>
2021-02-08 14:27:32 -0700

    Add method for checking presence of masternode winner votes

--- divi/src/MasternodePaymentData.cpp
+++ divi/src/MasternodePaymentData.cpp
@@ -11,6 +11,14 @@ MasternodePaymentData::~MasternodePaymentData()
 {
 }
 
+bool MasternodePaymentData::masternodeWinnerVoteIsKnown(const uint256& hash) const
+{
+    const auto mit = mapMasternodePayeeVotes.find(hash);
+    if (mit == mapMasternodePayeeVotes.end())
+        return false;
+    return true;
+}
+
 std::string MasternodePaymentData::ToString() const
 {
     std::ostringstream info;

32a20cbcc19c8f1600b5b9bdc74ec61803d135cd
galp <the.talion@gmail.com>
2021-02-16 08:52:30 -0700

    Change mnsync& parameter to const&

--- divi/src/masternode-payments.h
+++ divi/src/masternode-payments.h
@@ -50,7 +50,7 @@ private:
 
     bool GetBlockPayee(const uint256& seedHash, CScript& payee) const;
     bool CheckMasternodeWinnerSignature(const CMasternodePaymentWinner& winner) const;
-    bool CheckMasternodeWinnerValidity(CMasternodeSync& masternodeSynchronization,const CMasternodePaymentWinner& winner, CNode* pnode, std::string& strError) const;
+    bool CheckMasternodeWinnerValidity(const CMasternodeSync& masternodeSynchronization,const CMasternodePaymentWinner& winner, CNode* pnode, std::string& strError) const;
 public:
     static const int MNPAYMENTS_SIGNATURES_REQUIRED;
     static const int MNPAYMENTS_SIGNATURES_TOTAL;

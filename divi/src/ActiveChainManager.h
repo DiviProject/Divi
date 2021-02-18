11fff88a8a20c90f2292946b851f2ad877e7ed99
galp <the.talion@gmail.com>
2021-02-18 15:11:47 -0700

    Add const members for enabled indexing state

--- divi/src/ActiveChainManager.h
+++ divi/src/ActiveChainManager.h
@@ -7,7 +7,11 @@ class CCoinsViewCache;
 class CBlockTreeDB;
 class ActiveChainManager
 {
+private:
+    const bool& addressIndexingIsEnabled_;
+    const bool& spentInputIndexingIsEnabled_;
 public:
+    ActiveChainManager(const bool& addressIndexingIsEnabled, const bool& spentInputIndexingIsEnabled);
     static bool DisconnectBlock(CBlock& block, CValidationState& state, CBlockIndex* pindex, CCoinsViewCache& coins, CBlockTreeDB* pblocktree, bool* pfClean = nullptr);
 };
 #endif// ACTIVE_CHAIN_MANAGER_H

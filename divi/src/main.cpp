9491b44335c0dfe03403e50b843bcd620a0351a2
galp <the.talion@gmail.com>
2021-03-01 14:18:56 -0700

    Extract recording of spending activity for index updates

--- divi/src/main.cpp
+++ divi/src/main.cpp
@@ -50,6 +50,7 @@
 #include <TransactionOpCounting.h>
 #include <OrphanTransactions.h>
 #include <MasternodeModule.h>
+#include <IndexDatabaseUpdates.h>
 
 using namespace boost;
 using namespace std;
@@ -1302,6 +1303,49 @@ bool CheckEnforcedPoSBlocksAndBIP30(const CBlock& block, CValidationState& state
     return true;
 }
 
+void UpdateSpendingActivityInIndexDatabaseUpdates(
+    const CTransaction& tx,
+    const int blockHeight,
+    const int transactionIndex,
+    const CCoinsViewCache& view,
+    IndexDatabaseUpdates& indexDatabaseUpdates)
+{
+    const uint256 txhash = tx.GetHash();
+    if (fAddressIndex || fSpentIndex)
+    {
+        for (size_t j = 0; j < tx.vin.size(); j++) {
+
+            const CTxIn input = tx.vin[j];
+            const CTxOut &prevout = view.GetOutputFor(tx.vin[j]);
+            uint160 hashBytes;
+            int addressType;
+
+            if (prevout.scriptPubKey.IsPayToScriptHash()) {
+                hashBytes = uint160(vector <unsigned char>(prevout.scriptPubKey.begin()+2, prevout.scriptPubKey.begin()+22));
+                addressType = 2;
+            } else if (prevout.scriptPubKey.IsPayToPublicKeyHash()) {
+                hashBytes = uint160(vector <unsigned char>(prevout.scriptPubKey.begin()+3, prevout.scriptPubKey.begin()+23));
+                addressType = 1;
+            } else {
+                hashBytes.SetNull();
+                addressType = 0;
+            }
+            if (fAddressIndex && addressType > 0) {
+                // record spending activity
+                indexDatabaseUpdates.addressIndex.push_back(std::make_pair(CAddressIndexKey(addressType, hashBytes, blockHeight, transactionIndex, txhash, j, true), prevout.nValue * -1));
+                // remove address from unspent index
+                indexDatabaseUpdates.addressUnspentIndex.push_back(std::make_pair(CAddressUnspentKey(addressType, hashBytes, input.prevout.hash, input.prevout.n), CAddressUnspentValue()));
+            }
+            if (fSpentIndex) {
+                // add the spent index to determine the txid and input that spent an output
+                // and to find the amount and address from an input
+                indexDatabaseUpdates.spentIndex.push_back(std::make_pair(CSpentIndexKey(input.prevout.hash, input.prevout.n), CSpentIndexValue(txhash, j, blockHeight, prevout.nValue, addressType, hashBytes)));
+            }
+        }
+
+    }
+}
+
 bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pindex, CCoinsViewCache& view, bool fJustCheck, bool fAlreadyChecked)
 {
     AssertLockHeld(cs_main);
@@ -1341,9 +1385,11 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
     std::vector<std::pair<uint256, CDiskTxPos> > vPos;
     vPos.reserve(block.vtx.size());
     blockundo.vtxundo.reserve(block.vtx.size() - 1);
-    std::vector<std::pair<CAddressIndexKey, CAmount> > addressIndex;
-    std::vector<std::pair<CAddressUnspentKey, CAddressUnspentValue> > addressUnspentIndex;
-    std::vector<std::pair<CSpentIndexKey, CSpentIndexValue> > spentIndex;
+
+    IndexDatabaseUpdates indexDatabaseUpdates;
+    std::vector<std::pair<CAddressIndexKey, CAmount> >& addressIndex = indexDatabaseUpdates.addressIndex;
+    std::vector<std::pair<CAddressUnspentKey, CAddressUnspentValue> >& addressUnspentIndex = indexDatabaseUpdates.addressUnspentIndex;
+    std::vector<std::pair<CSpentIndexKey, CSpentIndexValue> >& spentIndex = indexDatabaseUpdates.spentIndex;
 
     CAmount nValueOut = 0;
     CAmount nValueIn = 0;
@@ -1378,43 +1424,7 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
                                  REJECT_INVALID, "bad-txns-inputs-missingorspent");
 
 
-            if (fAddressIndex || fSpentIndex)
-            {
-                for (size_t j = 0; j < tx.vin.size(); j++) {
-
-                    const CTxIn input = tx.vin[j];
-                    const CTxOut &prevout = view.GetOutputFor(tx.vin[j]);
-                    uint160 hashBytes;
-                    int addressType;
-
-                    if (prevout.scriptPubKey.IsPayToScriptHash()) {
-                        hashBytes = uint160(vector <unsigned char>(prevout.scriptPubKey.begin()+2, prevout.scriptPubKey.begin()+22));
-                        addressType = 2;
-                    } else if (prevout.scriptPubKey.IsPayToPublicKeyHash()) {
-                        hashBytes = uint160(vector <unsigned char>(prevout.scriptPubKey.begin()+3, prevout.scriptPubKey.begin()+23));
-                        addressType = 1;
-                    } else {
-                        hashBytes.SetNull();
-                        addressType = 0;
-                    }
-
-                    if (fAddressIndex && addressType > 0) {
-                        // record spending activity
-                        addressIndex.push_back(std::make_pair(CAddressIndexKey(addressType, hashBytes, pindex->nHeight, i, txhash, j, true), prevout.nValue * -1));
-
-                        // remove address from unspent index
-                        addressUnspentIndex.push_back(std::make_pair(CAddressUnspentKey(addressType, hashBytes, input.prevout.hash, input.prevout.n), CAddressUnspentValue()));
-                    }
-
-                    if (fSpentIndex) {
-                        // add the spent index to determine the txid and input that spent an output
-                        // and to find the amount and address from an input
-                        spentIndex.push_back(std::make_pair(CSpentIndexKey(input.prevout.hash, input.prevout.n), CSpentIndexValue(txhash, j, pindex->nHeight, prevout.nValue, addressType, hashBytes)));
-                    }
-                }
-
-            }
-
+            UpdateSpendingActivityInIndexDatabaseUpdates(tx,pindex->nHeight,i,view, indexDatabaseUpdates);
             // Add in sigops done by pay-to-script-hash inputs;
             // this is to prevent a "rogue miner" from creating
             // an incredibly-expensive-to-validate block.

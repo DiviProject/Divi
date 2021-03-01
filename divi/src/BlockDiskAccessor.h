df032d991a68fcf18ccc92f275393409215cd7b1
galp <the.talion@gmail.com>
2021-03-01 10:32:55 -0700

    Wrap block disk data reads in interface

--- divi/src/BlockDiskAccessor.h
+++ divi/src/BlockDiskAccessor.h
@@ -1,6 +1,7 @@
 #ifndef BLOCK_DISK_ACCESSOR_H
 #define BLOCK_DISK_ACCESSOR_H
 #include <stdint.h>
+#include <I_BlockDataReader.h>
 class CBlock;
 class CDiskBlockPos;
 class CBlockIndex;
@@ -9,4 +10,12 @@ class CBlockIndex;
 bool WriteBlockToDisk(CBlock& block, CDiskBlockPos& pos);
 bool ReadBlockFromDisk(CBlock& block, const CDiskBlockPos& pos);
 bool ReadBlockFromDisk(CBlock& block, const CBlockIndex* pindex);
+
+class BlockDiskDataReader: public I_BlockDataReader
+{
+public:
+    BlockDiskDataReader(){}
+    virtual bool ReadBlock(const CBlockIndex* blockIndex, CBlock& block) const;
+    virtual bool ReadBlockUndo(const CBlockIndex* blockIndex, CBlockUndo& blockUndo) const;
+};
 #endif // BLOCK_DISK_ACCESSOR_H

4a06f727a7911035ce5ed30b7c79391f971eb467
galp <the.talion@gmail.com>
2021-03-07 02:35:05 -0700

    Create new class for collecting index database updates

---
+++ divi/src/IndexDatabaseUpdateCollector.h
@@ -0,0 +1,8 @@
+#ifndef INDEX_DATABASE_UPDATE_COLLECTOR_H
+#define INDEX_DATABASE_UPDATE_COLLECTOR_H
+class IndexDatabaseUpdateCollector
+{
+private:
+    IndexDatabaseUpdateCollector(){};
+};
+#endif// INDEX_DATABASE_UPDATE_COLLECTOR_H

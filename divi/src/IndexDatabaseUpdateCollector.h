4ae9b8429707a9e190985eb460256522654a1b84
galp <the.talion@gmail.com>
2021-03-07 02:52:18 -0700

    Move index db update gathering into class

--- divi/src/IndexDatabaseUpdateCollector.h
+++ divi/src/IndexDatabaseUpdateCollector.h
@@ -1,8 +1,19 @@
 #ifndef INDEX_DATABASE_UPDATE_COLLECTOR_H
 #define INDEX_DATABASE_UPDATE_COLLECTOR_H
+class CTransaction;
+struct TransactionLocationReference;
+class CCoinsViewCache;
+struct IndexDatabaseUpdates;
+
 class IndexDatabaseUpdateCollector
 {
 private:
     IndexDatabaseUpdateCollector(){};
+public:
+    static void RecordTransaction(
+        const CTransaction& tx,
+        const TransactionLocationReference& txLocationRef,
+        const CCoinsViewCache& view,
+        IndexDatabaseUpdates& indexDatabaseUpdates);
 };
 #endif// INDEX_DATABASE_UPDATE_COLLECTOR_H

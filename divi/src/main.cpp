5974c661682bb453365da0a6afd997ed0af27204
galp <the.talion@gmail.com>
2021-03-08 08:44:07 -0700

    Avoid using BOOST_FOREACH

--- divi/src/main.cpp
+++ divi/src/main.cpp
@@ -753,7 +753,8 @@ bool CheckTransaction(const CTransaction& tx, bool fRejectBadUTXO, CValidationSt
 
     // Check for negative or overflow output values
     CAmount nValueOut = 0;
-    BOOST_FOREACH (const CTxOut& txout, tx.vout) {
+    for(const CTxOut& txout: tx.vout)
+    {
         if (txout.IsEmpty() && !tx.IsCoinBase() && !tx.IsCoinStake())
             return state.DoS(100, error("%s: txout empty for user transaction",__func__));
         if (txout.nValue < 0)

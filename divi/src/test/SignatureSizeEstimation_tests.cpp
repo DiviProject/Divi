18e8547018a0c383f2381b22b93d56ea4ac17a24
galp <the.talion@gmail.com>
2021-03-23 06:49:42 -0600

    Expand scriptSig tests & improve test fixture

--- divi/src/test/SignatureSizeEstimation_tests.cpp
+++ divi/src/test/SignatureSizeEstimation_tests.cpp
@@ -6,21 +6,73 @@
 #include <keystore.h>
 #include <SignatureSizeEstimator.h>
 #include <destination.h>
+#include <script/standard.h>
 
 class SignatureSizeTestFixture
 {
+private:
+    std::vector<CKey> keys_;
+    CBasicKeyStore keyStore_;
 public:
-    SignatureSizeTestFixture(){}
+    SignatureSizeTestFixture(
+        ): keys_()
+        , keyStore_()
+    {
+    }
+    void createKeys(unsigned numberOfKeys)
+    {
+        keys_.resize(numberOfKeys);
+        for(CKey& key: keys_)
+        {
+            key.MakeNewKey(true);
+        }
+    }
+    bool addKeyToStoreByIndex(unsigned keyIndex)
+    {
+        if(!(keyIndex< keys_.size()) ) return false;
+        return keyStore_.AddKey(keys_[keyIndex]);
+    }
+    void addKeysToStore()
+    {
+        for(CKey& key: keys_)
+        {
+            keyStore_.AddKey(key);
+        }
+    }
+    CKey& getKeyByIndex(unsigned keyIndex)
+    {
+        assert(keyIndex < keys_.size());
+        return keys_[keyIndex];
+    }
+    CBasicKeyStore& getKeyStore()
+    {
+        return keyStore_;
+    }
 };
 
 BOOST_FIXTURE_TEST_SUITE(SignatureSizeEstimationTests, SignatureSizeTestFixture)
 
 BOOST_AUTO_TEST_CASE(willDefaultToALargeByteSizeWhenScriptIsEmpty)
 {
-    CBasicKeyStore emptyKeyStore;
     BOOST_CHECK_EQUAL(
-        SignatureSizeEstimator::MaxBytesNeededForSigning(emptyKeyStore,CScript()),
+        SignatureSizeEstimator::MaxBytesNeededForSigning(getKeyStore(),CScript()),
         std::numeric_limits<unsigned>::max());
+}
+BOOST_AUTO_TEST_CASE(willDefaultToALargeByteSizeWhenScriptIsNonStandard)
+{
+    CScript nonstandardScript = CScript() << OP_FALSE << OP_DROP << OP_TRUE;
+    BOOST_CHECK_EQUAL(
+        SignatureSizeEstimator::MaxBytesNeededForSigning(getKeyStore(),nonstandardScript),
+        std::numeric_limits<unsigned>::max());
+}
+BOOST_AUTO_TEST_CASE(willDefaultToALargestByteSizePossibleWhenKeyIsUnknown)
+{
+    createKeys(2);
+    addKeyToStoreByIndex(0);
+    CKey& unknownKey = getKeyByIndex(1);
+    CScript alternateScript = GetScriptForDestination(unknownKey.GetPubKey().GetID());
+    BOOST_CHECK_EQUAL(
+        SignatureSizeEstimator::MaxBytesNeededForSigning(getKeyStore(),alternateScript), 139u);
 }
 
 BOOST_AUTO_TEST_SUITE_END()

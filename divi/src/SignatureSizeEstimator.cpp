fd2c4c42ef67ec0b008d5a9a68f94b221d5237ed
galp <the.talion@gmail.com>
2021-03-23 15:27:28 -0600

    Add test for vault scripts

--- divi/src/SignatureSizeEstimator.cpp
+++ divi/src/SignatureSizeEstimator.cpp
@@ -5,6 +5,7 @@
 #include <script/standard.h>
 #include <destination.h>
 #include <pubkey.h>
+#include <script/StakingVaultScript.h>
 
 const unsigned SignatureSizeEstimator::MaximumScriptSigBytesForP2PK = 74u;//71-73u for sig, +1u for push
 const unsigned SignatureSizeEstimator::MaximumScriptSigBytesForP2PKH = 140u;
@@ -36,6 +37,21 @@ unsigned SignatureSizeEstimator::MaxBytesNeededForSigning(const CKeyStore& keyst
             numberOfKnownKeys += keystore.HaveKey(keyID)? 1u:0u;
         }
         return 1u + numberOfKnownKeys*MaximumScriptSigBytesForP2PK;
+    }
+    else if(whichType == TX_VAULT)
+    {
+        CKeyID keyID = CKeyID(uint160(vSolutions[0]));
+        CPubKey pubkey;
+        if(keystore.GetPubKey(keyID,pubkey))
+        {
+            return MaximumScriptSigBytesForP2PK+1u+pubkey.size();
+        }
+        keyID = CKeyID(uint160(vSolutions[1]));
+        if(keystore.GetPubKey(keyID,pubkey))
+        {
+            return MaximumScriptSigBytesForP2PK+1u+pubkey.size();
+        }
+        return MaximumScriptSigBytesForP2PKH;
     }
     else if(!isSubscript && whichType == TX_SCRIPTHASH)
     {

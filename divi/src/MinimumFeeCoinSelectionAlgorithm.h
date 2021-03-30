8687efd4555c6b607c270e6bc23246425cc546ad
galp <the.talion@gmail.com>
2021-03-30 15:37:04 -0600

    Implement fee minimzing coin-selection algorithm

---
+++ divi/src/MinimumFeeCoinSelectionAlgorithm.h
@@ -0,0 +1,16 @@
+#ifndef MINIMUM_FEE_COIN_SELECTION_ALGORITHM_H
+#define MINIMUM_FEE_COIN_SELECTION_ALGORITHM_H
+#include <I_CoinSelectionAlgorithm.h>
+class CKeyStore;
+class MinimumFeeCoinSelectionAlgorithm: public I_CoinSelectionAlgorithm
+{
+private:
+    const CKeyStore& keyStore_;
+public:
+    MinimumFeeCoinSelectionAlgorithm(const CKeyStore& keyStore);
+    virtual std::set<COutput> SelectCoins(
+        const CMutableTransaction& transactionToSelectCoinsFor,
+        const CAmount& nTargetValue,
+        const std::vector<COutput>& vCoins) const;
+};
+#endif// MINIMUM_FEE_COIN_SELECTION_ALGORITHM_H

9a0c7edf838b0d29cfaca1dbe42e2f627a12be59
Daniel Kraft <d@domob.eu>
2021-03-31 16:18:51 +0200

    Update txindex.py test for bare txid lookups.
    This extends the txindex.py test, to check lookups also by bare txid
    where this is possible (mempool and txindex).  Lookups by UTXO set only
    work on whatever hash is used for the UTXO before/after segwit light.

--- divi/qa/rpc-tests/txindex.py
+++ divi/qa/rpc-tests/txindex.py
@@ -22,12 +22,12 @@ class TxIndexTest (BitcoinTestFramework):
         self.is_network_split = False
         self.sync_all ()
 
-    def expect_found (self, node, txid):
-        data = node.getrawtransaction (txid, 1)
-        assert_equal (data["txid"], txid)
+    def expect_found (self, node, key, value):
+        data = node.getrawtransaction (value, 1)
+        assert_equal (data[key], value)
 
-    def expect_not_found (self, node, txid):
-        assert_raises (JSONRPCException, node.getrawtransaction, txid)
+    def expect_not_found (self, node, value):
+        assert_raises (JSONRPCException, node.getrawtransaction, value)
 
     def run_test (self):
         self.nodes[0].setgenerate (True, 30)
@@ -46,13 +46,17 @@ class TxIndexTest (BitcoinTestFramework):
         tx = self.nodes[0].createrawtransaction (inputs, {addr: value - 1})
         signed = self.nodes[0].signrawtransaction (tx)
         assert_equal (signed["complete"], True)
-        txid = self.nodes[0].sendrawtransaction (signed["hex"])
+        self.nodes[0].sendrawtransaction (signed["hex"])
+        data = self.nodes[0].decoderawtransaction (signed["hex"])
+        txid = data["txid"]
+        bareTxid = data["baretxid"]
         sync_mempools (self.nodes)
 
         print ("Lookup through mempool...")
         for n in self.nodes:
           assert_equal (n.getrawmempool (), [txid])
-          self.expect_found (n, txid)
+          self.expect_found (n, "txid", txid)
+          self.expect_found (n, "baretxid", bareTxid)
 
         print ("Lookup through UTXO set...")
         self.nodes[0].setgenerate (True, 1)
@@ -60,7 +64,7 @@ class TxIndexTest (BitcoinTestFramework):
         for n in self.nodes:
           assert_equal (n.getrawmempool (), [])
           assert n.gettxout (txid, 0) is not None
-          self.expect_found (n, txid)
+          self.expect_found (n, "txid", txid)
 
         print ("Spending test output...")
         tx = self.nodes[0].createrawtransaction ([{"txid": txid, "vout": 0}], {addr: value - 2})
@@ -74,8 +78,10 @@ class TxIndexTest (BitcoinTestFramework):
         for n in self.nodes:
           assert_equal (n.getrawmempool (), [])
           assert_equal (n.gettxout (txid, 0), None)
-        self.expect_found (self.nodes[0], txid)
+        self.expect_found (self.nodes[0], "txid", txid)
+        self.expect_found (self.nodes[0], "baretxid", bareTxid)
         self.expect_not_found (self.nodes[1], txid)
+        self.expect_not_found (self.nodes[1], bareTxid)
 
 
 if __name__ == '__main__':

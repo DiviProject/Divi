02b4bbdea7d9a30ef081632b1a55f88e70805c96
Daniel Kraft <d@domob.eu>
2021-04-09 14:03:09 +0200

    Automatic conversion of some types in logging.
    This adds a framework that allows us to automatically convert some types
    to std::string when logging, without having to explicitly invoke
    ToString() or GetHex() on them for each log call.
    
    In this commit, we only add the functionality itself but not yet make
    use of it.  This will come as follow-ups.

--- divi/src/Logging.h
+++ divi/src/Logging.h
@@ -20,13 +20,57 @@ bool LogAcceptCategory(const char* category);
 /** Send a string to the log output */
 int LogPrintStr(const std::string& str);
 
+/** Explicitly converts a particular value to a string for logging.  This is
+ *  used for some types (e.g. uint256) that are not literally passed on to
+ *  the format engine.  Template specialisations are defined in source files
+ *  that are linked into the respective libraries.  */
+template <typename T>
+  std::string LogFormatConvert(const T& v);
+
+/** Helper type for converting some types with LogFormatConvert, while keeping
+ *  others literal at whatever they are.  */
+template <typename T>
+  struct LogFormatConvertWorker
+{
+    static const T& apply(const T& v)
+    {
+        return v;
+    }
+};
+#define LOG_WITH_CONVERSION(type) \
+    class type; \
+    template <> \
+      std::string LogFormatConvert<type>(const type& v); \
+    template <> \
+      struct LogFormatConvertWorker<const type&> \
+    { \
+        static std::string apply(const type& v) \
+        { \
+            return LogFormatConvert(v); \
+        } \
+    };
+LOG_WITH_CONVERSION(uint256)
+
+/** This macro defines a custom implementation of LogFormatConvert for
+ *  a type that then just calls through to the instance's ToString
+ *  method.  The actual definition has to be done in a source file
+ *  linked in with the correct library, since it needs the definition of
+ *  the type in question.  */
+#define LOG_FORMAT_WITH_TOSTRING(type) \
+    template <> \
+      std::string LogFormatConvert<type>(const type& v) \
+    { \
+        return v.ToString(); \
+    }
+
 /** Formats a string (consisting of a format template and zero or more
  *  arguments) for logging.  This mostly invokes tinyformat, but has some
- *  extra tweaks like supporting zero arguments.  */
+ *  extra tweaks like supporting zero arguments and applying conversion
+ *  for some types.  */
 template <typename... Args>
   std::string LogFormat(const char* format, const Args&... args)
 {
-    return tfm::format(format, args...);
+    return tfm::format(format, LogFormatConvertWorker<decltype(args)>::apply(args)...);
 }
 /* This is an explicit specialisation, so we have to declare it "inline"
    to prevent ODR violations.  */

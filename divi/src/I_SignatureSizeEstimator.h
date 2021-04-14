b6af333fcebdb234868e2f8baff05fcfd04033e0
galp <the.talion@gmail.com>
2021-04-14 07:10:08 -0600

    Include MaxBytesNeededForScriptSig in interface to avoid divergence

--- divi/src/I_SignatureSizeEstimator.h
+++ divi/src/I_SignatureSizeEstimator.h
@@ -7,5 +7,9 @@ class I_SignatureSizeEstimator
 public:
     virtual ~I_SignatureSizeEstimator(){}
     virtual unsigned MaxBytesNeededForSigning(const CKeyStore& keystore,const CScript& scriptPubKey) const = 0;
+    unsigned MaxBytesNeededForScriptSig(const CKeyStore& keystore,const CScript& scriptPubKey) const
+    {
+        return MaxBytesNeededForSigning(keystore,scriptPubKey) + 40u;
+    };
 };
 #endif// I_SIGNATURE_SIZE_ESTIMATOR_H

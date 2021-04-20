1a4f7e69220d57753b4fc2025085c4c8d33007b5
galp <the.talion@gmail.com>
2021-04-20 11:27:44 -0600

    Check configuration to identify our broadcast from peer msgs

--- divi/src/masternodeman.h
+++ divi/src/masternodeman.h
@@ -47,7 +47,7 @@ private:
     bool UpdateWithNewBroadcast(const CMasternodeBroadcast &mnb, CMasternode& masternode) const;
     bool CheckInputsForMasternode(const CMasternodeBroadcast& mnb, int& nDoS);
     bool CheckAndUpdateMasternode(CMasternodeSync& masternodeSynchronization,CMasternodeBroadcast& mnb, int& nDoS);
-    bool CheckAndUpdatePing(CMasternode& mn,CMasternodePing& mnp, int& nDoS);
+    bool CheckAndUpdatePing(CMasternode& mn,CMasternodePing& mnp, int& nDoS, bool skipPingChainSyncCheck = false);
 public:
 
     CMasternodeMan(

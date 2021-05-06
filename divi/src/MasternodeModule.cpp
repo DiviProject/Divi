7df3844fb1a45d597d4d08800f455e465cafb717
galpHub <the.talion@gmail.com>
2021-05-06 06:02:38 -0600

    Fix capitatlization

--- divi/src/MasternodeModule.cpp
+++ divi/src/MasternodeModule.cpp
@@ -90,7 +90,7 @@ MasternodeModule mnModule(localClock,blockchainSyncQueryService,peerSyncQuerySer
 
 MasternodeModule::MasternodeModule(
     const I_Clock& clock,
-    const I_BlockchainSyncQueryService& blockChainSyncQueryService,
+    const I_BlockchainSyncQueryService& blockchainSyncQueryService,
     const PeerSyncQueryService& peerSyncQueryService,
     const CChain& activeChain,
     const BlockMap& blockIndexByHash,
@@ -102,7 +102,7 @@ MasternodeModule::MasternodeModule(
     , networkMessageManager_( new MasternodeNetworkMessageManager)
     , masternodePaymentData_(new MasternodePaymentData)
     , masternodeConfig_( new CMasternodeConfig)
-    , masternodeSync_(new CMasternodeSync(*networkFulfilledRequestManager_,peerSyncQueryService,clock,blockChainSyncQueryService,*networkMessageManager_,*masternodePaymentData_))
+    , masternodeSync_(new CMasternodeSync(*networkFulfilledRequestManager_,peerSyncQueryService,clock,blockchainSyncQueryService,*networkMessageManager_,*masternodePaymentData_))
     , mnodeman_(new CMasternodeMan(*networkMessageManager_,*masternodeSync_,activeChain_,blockIndexByHash_,addressManager))
     , activeMasternode_(new CActiveMasternode(*masternodeConfig_, fMasterNode_))
     , masternodePayments_(new CMasternodePayments(*networkFulfilledRequestManager_,*masternodePaymentData_,*networkMessageManager_,*mnodeman_,*masternodeSync_,activeChain_))

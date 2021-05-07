323340b8ed395d75af0204f6da2b4a8fe725e172
galpHub <the.talion@gmail.com>
2021-05-07 10:10:11 -0600

    Extract peer-banning functionality into separate file

--- divi/src/Node.h
+++ divi/src/Node.h
@@ -115,10 +115,6 @@ public:
     int nSporksCount = -1;
 
 protected:
-    // Denial-of-service detection/prevention
-    // Key is IP address, value is banned-until-time
-    static std::map<CNetAddr, int64_t> setBanned;
-    static CCriticalSection cs_setBanned;
     // Basic fuzz-testing
     void Fuzz(int nChance); // modifies ssSend
 
@@ -222,26 +218,6 @@ public:
     /** Checks if we should send a ping message to this peer, and does it
      *  if we should.  */
     void MaybeSendPing();
-
-    // Denial-of-service detection/prevention
-    // The idea is to detect peers that are behaving
-    // badly and disconnect/ban them, but do it in a
-    // one-coding-mistake-won't-shatter-the-entire-network
-    // way.
-    // IMPORTANT:  There should be nothing I can give a
-    // node that it will forward on that will make that
-    // node's peers drop it. If there is, an attacker
-    // can isolate a node and/or try to split the network.
-    // Dropping a node for sending stuff that is invalid
-    // now but might be valid in a later version is also
-    // dangerous, because it can cause a network split
-    // between nodes running old code and nodes running
-    // new code.
-    static void ClearBanned(); // needed for unit testing
-    static std::string ListBanned();
-    static bool IsBanned(CNetAddr ip);
-    static bool Ban(const CNetAddr& ip);
-    static bool Ban(const CNetAddr& addr, int64_t banTime);
 };
 
 class NetworkUsageStats

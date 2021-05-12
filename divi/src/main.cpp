2af829495193fd8308a1226d5509b49e9b4961d0
galpHub <the.talion@gmail.com>
2021-05-12 14:56:47 -0600

    Extract BeginSyncingWithPeer

--- divi/src/main.cpp
+++ divi/src/main.cpp
@@ -4127,6 +4127,20 @@ static void CommunicateRejectedBlocksToPeer(CNode* pto, std::vector<CBlockReject
     }
     rejectedBlocks.clear();
 }
+static void BeginSyncingWithPeer(CNode* pto, CNodeState* state)
+{
+    if (!state->fSyncStarted && !pto->fClient && !fReindex) {
+        // Only actively request headers from a single peer, unless we're close to end of initial download.
+        if (nSyncStarted == 0 || pindexBestHeader->GetBlockTime() > GetAdjustedTime() - 6 * 60 * 60) { // NOTE: was "close to today" and 24h in Bitcoin
+            state->fSyncStarted = true;
+            nSyncStarted++;
+            //CBlockIndex *pindexStart = pindexBestHeader->pprev ? pindexBestHeader->pprev : pindexBestHeader;
+            //LogPrint("net", "initial getheaders (%d) to peer=%d (startheight:%d)\n", pindexStart->nHeight, pto->id, pto->nStartingHeight);
+            //pto->PushMessage("getheaders", chainActive.GetLocator(pindexStart), uint256(0));
+            pto->PushMessage("getblocks", chainActive.GetLocator(chainActive.Tip()), uint256(0));
+        }
+    }
+}
 bool SendMessages(CNode* pto, bool fSendTrickle)
 {
     {
@@ -4149,17 +4163,7 @@ bool SendMessages(CNode* pto, bool fSendTrickle)
         bool fFetch = state->fPreferredDownload || (nPreferredDownload == 0 && !pto->fClient && !pto->fOneShot); // Download if this is a nice peer, or we have no nice peers and this one might do.
         if(fFetch)
         {
-            if (!state->fSyncStarted && !pto->fClient && !fReindex) {
-                // Only actively request headers from a single peer, unless we're close to end of initial download.
-                if (nSyncStarted == 0 || pindexBestHeader->GetBlockTime() > GetAdjustedTime() - 6 * 60 * 60) { // NOTE: was "close to today" and 24h in Bitcoin
-                    state->fSyncStarted = true;
-                    nSyncStarted++;
-                    //CBlockIndex *pindexStart = pindexBestHeader->pprev ? pindexBestHeader->pprev : pindexBestHeader;
-                    //LogPrint("net", "initial getheaders (%d) to peer=%d (startheight:%d)\n", pindexStart->nHeight, pto->id, pto->nStartingHeight);
-                    //pto->PushMessage("getheaders", chainActive.GetLocator(pindexStart), uint256(0));
-                    pto->PushMessage("getblocks", chainActive.GetLocator(chainActive.Tip()), uint256(0));
-                }
-            }
+            BeginSyncingWithPeer(pto, state);
         }
 
         // Resend wallet transactions that haven't gotten in a block yet

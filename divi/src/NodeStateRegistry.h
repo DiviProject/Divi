f884955c5acb785947ba3e755eb1a07b30b4237f
galpHub <the.talion@gmail.com>
2021-05-13 15:23:24 -0600

    Move FindNextBlocksToDownload into NodeStateRegistry.h

--- divi/src/NodeStateRegistry.h
+++ divi/src/NodeStateRegistry.h
@@ -1,6 +1,9 @@
 #ifndef NODE_STATE_REGISTRY_H
 #define NODE_STATE_REGISTRY_H
 #include <NodeId.h>
+#include <vector>
+
+class CChain;
 class CNode;
 struct CNodeState;
 class uint256;
@@ -21,4 +24,11 @@ bool BlockIsInFlight(const uint256& hash);
 NodeId GetSourceOfInFlightBlock(const uint256& hash);
 void ProcessBlockAvailability(const BlockMap& blockIndicesByHash, NodeId nodeid);
 void UpdateBlockAvailability(const BlockMap& blockIndicesByHash, NodeId nodeid, const uint256& hash);
+void FindNextBlocksToDownload(
+    const BlockMap& blockIndicesByHash,
+    const CChain& activeChain,
+    NodeId nodeid,
+    unsigned int count,
+    std::vector<CBlockIndex*>& vBlocks,
+    NodeId& nodeStaller);
 #endif// NODE_STATE_REGISTRY_H

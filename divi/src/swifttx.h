dc529e6224c69406e31cc88c966bb5bb6db48820
Daniel Kraft <d@domob.eu>
2021-05-19 16:02:06 +0200

    Remove CTransactionLock and CConsensusVote.
    The two classes CTransactionLock and CConsensusVote are now unused
    and can just be removed from swifttx.

--- divi/src/swifttx.h
+++ divi/src/swifttx.h
@@ -19,49 +19,4 @@ extern std::map<uint256, CTransaction> mapTxLockReq;
 extern std::map<uint256, CTransaction> mapTxLockReqRejected;
 extern std::map<COutPoint, uint256> mapLockedInputs;
 
-class CConsensusVote
-{
-public:
-    CTxIn vinMasternode;
-    uint256 txHash;
-    int nBlockHeight;
-    std::vector<unsigned char> vchMasterNodeSignature;
-
-    uint256 GetHash() const;
-
-    bool SignatureValid();
-    bool Sign();
-
-    ADD_SERIALIZE_METHODS;
-
-    template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion)
-    {
-        READWRITE(txHash);
-        READWRITE(vinMasternode);
-        READWRITE(vchMasterNodeSignature);
-        READWRITE(nBlockHeight);
-    }
-};
-
-class CTransactionLock
-{
-public:
-    int nBlockHeight;
-    uint256 txHash;
-    std::vector<CConsensusVote> vecConsensusVotes;
-    int nExpiration;
-    int nTimeout;
-
-    bool SignaturesValid();
-    int CountSignatures();
-    void AddSignature(CConsensusVote& cv);
-
-    uint256 GetHash() const
-    {
-        return txHash;
-    }
-};
-
-
 #endif

9ebcbd7ae9771b7bb6f3b650f75bedf21c52f4e2
galpHub <the.talion@gmail.com>
2021-05-19 18:13:34 -0600

    Provide constructor for CBlockRejects

--- divi/src/BlockRejects.h
+++ divi/src/BlockRejects.h
@@ -6,5 +6,11 @@ struct CBlockReject {
     unsigned char chRejectCode;
     std::string strRejectReason;
     uint256 hashBlock;
+    CBlockReject(unsigned char rejectCode, std::string reasonForRejection, uint256 blockHash)
+    {
+        chRejectCode = rejectCode;
+        strRejectReason = reasonForRejection;
+        hashBlock = blockHash;
+    }
 };
 #endif// BLOCK_REJECTS_H

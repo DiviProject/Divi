39e82bbcfc8bf31316e46850fcf03b60dadc139d
galpHub <the.talion@gmail.com>
2021-06-08 11:40:45 -0600

    Manage inventory ids with map instead of array Handle out-of-range command accesses

---
+++ divi/src/test/InventoryTypes_tests.cpp
@@ -0,0 +1,47 @@
+#include <test_only.h>
+#include <protocol.h>
+
+
+BOOST_AUTO_TEST_SUITE(InventoryTypeTests)
+
+BOOST_AUTO_TEST_CASE(willCheckInventoryTypesAreKnown)
+{
+    for(int inventoryId = MSG_TX; inventoryId <= MSG_MASTERNODE_PING; ++inventoryId)
+    {
+        CInv inv(inventoryId,0);
+        BOOST_CHECK_MESSAGE(inv.IsKnownType(),"Inventory is of unknown type\n");
+    }
+}
+BOOST_AUTO_TEST_CASE(willCheckInventoryIdsAreWithinRange)
+{
+    {
+        CInv inv(0,0);
+        BOOST_CHECK_MESSAGE(!inv.IsKnownType(),"Zero is an invalid inventory id\n");
+    }
+    {
+        CInv inv(MSG_MASTERNODE_PING+1,0);
+        BOOST_CHECK_MESSAGE(!inv.IsKnownType(),"Invalid inventory id being treated as known\n");
+    }
+}
+BOOST_AUTO_TEST_CASE(willCheckInventoryCommandsCanBeConvertedToMatchingTypes)
+{
+    for(int inventoryId = MSG_TX; inventoryId <= MSG_MASTERNODE_PING; ++inventoryId)
+    {
+        CInv inv(inventoryId,0);
+        CInv copiedInventory(inv.GetCommand(),0);
+        BOOST_CHECK_MESSAGE(inv.type==copiedInventory.type, "Inventory type does not match inventory command");
+    }
+    {
+        CInv inv(0,0);
+        CInv copiedInventory(inv.GetCommand(),0);
+        BOOST_CHECK_MESSAGE(inv.type==copiedInventory.type, "Inventory type does not match inventory command");
+    }
+    {
+        CInv inv(MSG_MASTERNODE_PING+1,0);
+        CInv copiedInventory(inv.GetCommand(),0);
+        BOOST_CHECK_MESSAGE(inv.type==copiedInventory.type, "Inventory type for invalid object was copied into a valid type");
+        BOOST_CHECK_MESSAGE(copiedInventory.type == 0, "Erroneous inventory object has been assigned valid type");
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()

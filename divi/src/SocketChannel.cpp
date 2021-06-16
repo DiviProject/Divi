a8596564408cfb66ad1dcc9c15660be1ca871577
galpHub <the.talion@gmail.com>
2021-06-16 09:51:00 -0600

    Move SocketChannel into separate file

---
+++ divi/src/SocketChannel.cpp
@@ -0,0 +1,26 @@
+#include <SocketChannel.h>
+#include <netbase.h>
+
+SocketChannel::SocketChannel(SOCKET socket): socket_(socket)
+{
+}
+int SocketChannel::sendData(const void* buffer, size_t len) const
+{
+    return send(socket_, buffer, len, MSG_NOSIGNAL | MSG_DONTWAIT);
+}
+int SocketChannel::receiveData(void* buffer, size_t len) const
+{
+    return recv(socket_, buffer, len, MSG_DONTWAIT);
+}
+void SocketChannel::close()
+{
+    CloseSocket(socket_);
+}
+SOCKET SocketChannel::getSocket() const
+{
+    return socket_;
+}
+bool SocketChannel::isValid() const
+{
+    return socket_ != INVALID_SOCKET;
+}

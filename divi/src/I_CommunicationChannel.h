533cbc93b6aa953e1e31639f569f6c4e040d86f0
galpHub <the.talion@gmail.com>
2021-06-16 09:34:36 -0600

    Extract communication channel interface into separate header

---
+++ divi/src/I_CommunicationChannel.h
@@ -0,0 +1,12 @@
+#ifndef I_COMMUNICATION_CHANNEL_H
+#define I_COMMUNICATION_CHANNEL_H
+class I_CommunicationChannel
+{
+public:
+    virtual ~I_CommunicationChannel(){}
+    virtual int sendData(const void* buffer, size_t len) const = 0;
+    virtual int receiveData(void* buffer, size_t len) const = 0;
+    virtual void close() = 0;
+    virtual bool isValid() const = 0;
+};
+#endif// I_COMMUNICATION_CHANNEL_H

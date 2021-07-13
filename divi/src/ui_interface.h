bc02a9ba1b2b0a459d5a077fc22800bd4ff34b44
galpHub <the.talion@gmail.com>
2021-07-12 18:47:58 -0600

    Disambiguate notification messages

--- divi/src/ui_interface.h
+++ divi/src/ui_interface.h
@@ -18,9 +18,10 @@ class uint256;
 
 /** General change type (added, updated, removed). */
 enum ChangeType {
-    CT_NEW,
-    CT_UPDATED,
-    CT_DELETED
+    CT_DELETED = 0,
+    CT_NEW = 1 << 0,
+    CT_UPDATED = 1 << 1,
+    CT_ADDED = 1 << 2,
 };
 
 /** Signals for UI communication. */

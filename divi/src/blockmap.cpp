cc231d10e6d0676523e24475dde99bdebb8ffeb1
galpHub <the.talion@gmail.com>
2021-07-28 15:51:11 -0600

    Move insertion of new blocks into block map

---
+++ divi/src/blockmap.cpp
@@ -0,0 +1,20 @@
+#include <blockmap.h>
+
+CBlockIndex* BlockMap::CreateUniqueBlockIndex(uint256 blockHash)
+{
+    if (blockHash == 0)
+        return NULL;
+
+    // Return existing
+    BlockMap::iterator mi = find(blockHash);
+    if (mi != end())
+        return (*mi).second;
+
+    // Create new
+    CBlockIndex* pindexNew = new CBlockIndex();
+    if (!pindexNew)
+        throw std::runtime_error("LoadBlockIndex() : new CBlockIndex failed");
+    mi = insert(std::make_pair(blockHash, pindexNew)).first;
+    pindexNew->phashBlock = &((*mi).first);
+    return pindexNew;
+}

c110066b7ce84944955339b065357c42fcc7af68
galpHub <the.talion@gmail.com>
2021-08-02 06:34:37 -0600

    Move confirmation calculator into separate file

---
+++ divi/src/MerkleTxConfirmationNumberCalculator.cpp
@@ -0,0 +1,56 @@
+#include <MerkleTxConfirmationNumberCalculator.h>
+#include <chain.h>
+#include <blockmap.h>
+#include <sync.h>
+#include <txmempool.h>
+#include <merkletx.h>
+
+MerkleTxConfirmationNumberCalculator::MerkleTxConfirmationNumberCalculator(
+    const CChain& activeChain,
+    const BlockMap& blockIndices,
+    CTxMemPool& mempool,
+    AnnotatedMixin<boost::recursive_mutex>& mainCS
+    ): activeChain_(activeChain)
+    , blockIndices_(blockIndices)
+    , mempool_(mempool)
+    , mainCS_(mainCS)
+{
+}
+
+std::pair<const CBlockIndex*,int> MerkleTxConfirmationNumberCalculator::FindConfirmedBlockIndexAndDepth(const CMerkleTx& merkleTx) const
+{
+    static const std::pair<const CBlockIndex*,int> defaultValue = std::make_pair(nullptr,0);
+    if(!merkleTx.MerkleBranchIsSet())
+        return defaultValue;
+
+    // Find the block it claims to be in
+    int depth;
+    CBlockIndex* pindex;
+    {
+        LOCK(mainCS_);
+        BlockMap::const_iterator mi = blockIndices_.find(merkleTx.hashBlock);
+        if (mi == blockIndices_.end())
+        {
+            return defaultValue;
+        }
+        pindex = (*mi).second;
+        if (!pindex || !activeChain_.Contains(pindex))
+        {
+            return defaultValue;
+        }
+        depth = activeChain_.Height() - pindex->nHeight + 1;
+    }
+    return std::make_pair(pindex,depth);
+}
+
+int MerkleTxConfirmationNumberCalculator::GetBlockHeightOfFirstConfirmation(const CMerkleTx& merkleTx) const
+{
+    const CBlockIndex* firstConfirmationBlockIndex = FindConfirmedBlockIndexAndDepth(merkleTx).first;
+    return firstConfirmationBlockIndex? firstConfirmationBlockIndex->nHeight:0;
+}
+int MerkleTxConfirmationNumberCalculator::GetNumberOfBlockConfirmations(const CMerkleTx& merkleTx) const
+{
+    const int depth = FindConfirmedBlockIndexAndDepth(merkleTx).second;
+    if(depth==0 && !mempool_.exists(merkleTx.GetHash())) return -1;
+    return depth;
+}

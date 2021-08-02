6861cb6a673414c516f78a9b52d8b3825d871a8c
galpHub <the.talion@gmail.com>
2021-08-02 06:34:37 -0600

    Move confirmation calculator into separate file

---
+++ divi/src/MerkleTxConfirmationNumberCalculator.h
@@ -0,0 +1,33 @@
+#ifndef MERKLE_TX_CONFIRMATION_CALCULATOR_H
+#define MERKLE_TX_CONFIRMATION_CALCULATOR_H
+#include <boost/thread/recursive_mutex.hpp>
+#include <I_MerkleTxConfirmationNumberCalculator.h>
+#include <utility>
+class CBlockIndex;
+class CChain;
+class BlockMap;
+class CTxMemPool;
+class CMerkleTx;
+
+template <typename MutexObj>
+class AnnotatedMixin;
+
+class MerkleTxConfirmationNumberCalculator final: public I_MerkleTxConfirmationNumberCalculator
+{
+private:
+    const CChain& activeChain_;
+    const BlockMap& blockIndices_;
+    CTxMemPool& mempool_;
+    AnnotatedMixin<boost::recursive_mutex>& mainCS_;
+public:
+    MerkleTxConfirmationNumberCalculator(
+        const CChain& activeChain,
+        const BlockMap& blockIndices,
+        CTxMemPool& mempool,
+        AnnotatedMixin<boost::recursive_mutex>& mainCS);
+    std::pair<const CBlockIndex*,int> FindConfirmedBlockIndexAndDepth(const CMerkleTx& merkleTx) const;
+    int GetBlockHeightOfFirstConfirmation(const CMerkleTx& merkleTx) const;
+    int GetNumberOfBlockConfirmations(const CMerkleTx& merkleTx) const;
+};
+
+#endif// MERKLE_TX_CONFIRMATION_CALCULATOR_H
--- divi/src/merkletx.h
+++ divi/src/merkletx.h
@@ -3,38 +3,10 @@
 /** A transaction with a merkle branch linking it to the block chain. */
 #include <primitives/transaction.h>
 #include <uint256.h>
-#include <boost/thread/recursive_mutex.hpp>
 #include <I_MerkleTxConfirmationNumberCalculator.h>
 
-class CBlockIndex;
 class CBlock;
-class CChain;
 class BlockMap;
-
-class CTxMemPool;
-class CMerkleTx;
-
-template <typename MutexObj>
-class AnnotatedMixin;
-
-class MerkleTxConfirmationNumberCalculator final: public I_MerkleTxConfirmationNumberCalculator
-{
-private:
-    const CChain& activeChain_;
-    const BlockMap& blockIndices_;
-    CTxMemPool& mempool_;
-    AnnotatedMixin<boost::recursive_mutex>& mainCS_;
-public:
-    MerkleTxConfirmationNumberCalculator(
-        const CChain& activeChain,
-        const BlockMap& blockIndices,
-        CTxMemPool& mempool,
-        AnnotatedMixin<boost::recursive_mutex>& mainCS);
-    std::pair<const CBlockIndex*,int> FindConfirmedBlockIndexAndDepth(const CMerkleTx& merkleTx) const;
-    int GetBlockHeightOfFirstConfirmation(const CMerkleTx& merkleTx) const;
-    int GetNumberOfBlockConfirmations(const CMerkleTx& merkleTx) const;
-};
-
 
 class CMerkleTx : public CTransaction
 {

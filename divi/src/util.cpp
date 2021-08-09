769334de2c192a6c166132abb0c80dae56fca787
galpHub <the.talion@gmail.com>
2021-08-09 01:19:59 -0600

    Drop single use global

--- divi/src/util.cpp
+++ divi/src/util.cpp
@@ -115,8 +115,6 @@ bool fSucessfullyLoaded = false;
 
 Settings& settings = Settings::instance();
 
-bool fDaemon = false;
-
 #ifndef WIN32
 boost::filesystem::path GetPidFile(const Settings& settings)
 {

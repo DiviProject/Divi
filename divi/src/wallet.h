adeaa0d55cf0601f289bff560e72ac834ce90da5
galpHub <the.talion@gmail.com>
2021-08-18 11:05:43 -0600

    Drop unused type of 'wallet keys'

--- divi/src/wallet.h
+++ divi/src/wallet.h
@@ -451,34 +451,6 @@ public:
     std::set<uint256> GetConflicts(const uint256& txid) const;
 };
 
-/** Private key that includes an expiration date in case it never gets used. */
-class CWalletKey
-{
-public:
-    CPrivKey vchPrivKey;
-    int64_t nTimeCreated;
-    int64_t nTimeExpires;
-    std::string strComment;
-    //! todo: add something to note what created it (user, getnewaddress, change)
-    //!   maybe should have a map<string, string> property map
-
-    CWalletKey(int64_t nExpires = 0);
-
-    ADD_SERIALIZE_METHODS;
-
-    template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion)
-    {
-        if (!(nType & SER_GETHASH))
-            READWRITE(nVersion);
-        READWRITE(vchPrivKey);
-        READWRITE(nTimeCreated);
-        READWRITE(nTimeExpires);
-        READWRITE(LIMITED_STRING(strComment, 65536));
-    }
-};
-
-
 /**
  * Account information.
  * Stored in wallet with key "acc"+string account name.

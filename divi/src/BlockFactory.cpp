4c4800e5ee1ab737a8d0b22d9f98c6aee717ee88
galpHub <the.talion@gmail.com>
2021-09-02 12:19:28 -0600

    Approximately duplicate PoW logic from CoinMinter in BlockFactory
    The intent is to allow the block factory to fully autonomously determine
    how both kinds of blocks are produced. Some deviation from the original logic
    is required in order to decouple the creation of the block from its submission
    to the network.

--- divi/src/BlockFactory.cpp
+++ divi/src/BlockFactory.cpp
@@ -14,6 +14,8 @@
 #include <Logging.h>
 #include <script/standard.h>
 #include <Settings.h>
+#include <utiltime.h>
+#include <ThreadManagementHelpers.h>
 
 // Actual mining functions
 BlockFactory::BlockFactory(
@@ -81,6 +83,126 @@ bool BlockFactory::AppendProofOfStakeToBlock(
         return true;
     }
 
+    return false;
+}
+
+void BlockFactory::UpdateTime(CBlockHeader& block, const CBlockIndex* pindexPrev) const
+{
+    block.nTime = std::max(pindexPrev->GetMedianTimePast() + 1, GetAdjustedTime());
+
+    // Updating time can change work required on testnet:
+    if (chainParameters_.AllowMinDifficultyBlocks())
+        block.nBits = GetNextWorkRequired(pindexPrev, &block,chainParameters_);
+}
+
+void BlockFactory::IncrementExtraNonce(
+    CBlock& block,
+    const CBlockIndex* pindexPrev,
+    unsigned int& nExtraNonce) const
+{
+    /** Constant stuff for coinbase transactions we create: */
+    static CScript COINBASE_FLAGS;
+    // Update nExtraNonce
+    static uint256 hashPrevBlock;
+    if (hashPrevBlock != block.hashPrevBlock) {
+        nExtraNonce = 0;
+        hashPrevBlock = block.hashPrevBlock;
+    }
+    ++nExtraNonce;
+    unsigned int nHeight = pindexPrev->nHeight + 1; // Height first in coinbase required for block.version=2
+    CMutableTransaction txCoinbase(block.vtx[0]);
+    txCoinbase.vin[0].scriptSig = (CScript() << nHeight << CScriptNum(nExtraNonce)) + COINBASE_FLAGS;
+    assert(txCoinbase.vin[0].scriptSig.size() <= 100);
+
+    block.vtx[0] = txCoinbase;
+    block.hashMerkleRoot = block.BuildMerkleTree();
+}
+
+void BlockFactory::SetBlockHeaders(
+    CBlockTemplate& pblocktemplate,
+    const bool& proofOfStake) const
+{
+    // Fill in header
+    CBlock& block = pblocktemplate.block;
+    block.hashPrevBlock = pblocktemplate.previousBlockIndex->GetBlockHash();
+    if (!proofOfStake)
+        UpdateTime(block, pblocktemplate.previousBlockIndex);
+    block.nBits = GetNextWorkRequired(pblocktemplate.previousBlockIndex, &block, chainParameters_);
+    block.nNonce = 0;
+    block.nAccumulatorCheckpoint = static_cast<uint256>(0);
+}
+
+void BlockFactory::SetCoinbaseRewardAndHeight (
+    CBlockTemplate& pblocktemplate,
+    const bool& fProofOfStake) const
+{
+    // Compute final coinbase transaction.
+    int nHeight = pblocktemplate.previousBlockIndex->nHeight+1;
+    CBlock& block = pblocktemplate.block;
+    CMutableTransaction& coinbaseTx = *pblocktemplate.coinbaseTransaction;
+    block.vtx[0].vin[0].scriptSig = CScript() << nHeight << OP_0;
+    if (!fProofOfStake) {
+        coinbaseTx.vout[0].nValue = blockSubsidies_.GetBlockSubsidity(nHeight).nStakeReward;
+        coinbaseTx.vin[0].scriptSig = CScript() << nHeight << OP_0;
+        block.vtx[0] = coinbaseTx;
+    }
+}
+
+void BlockFactory::UpdateBlockCoinBaseAndHeaders (
+    CBlockTemplate& blocktemplate,
+    const bool& fProofOfStake) const
+{
+    unsigned int extraNonce = 0u;
+    CBlock& block = blocktemplate.block;
+    SetCoinbaseRewardAndHeight(blocktemplate, fProofOfStake);
+    SetBlockHeaders(blocktemplate, fProofOfStake);
+    IncrementExtraNonce(block, blocktemplate.previousBlockIndex, extraNonce);
+}
+
+bool BlockFactory::AppendProofOfWorkToBlock(
+    CBlockTemplate& blocktemplate)
+{
+    CBlock& block = blocktemplate.block;
+    int64_t nStart = GetTime();
+    uint256 hashTarget = uint256().SetCompact(block.nBits);
+    while (true)
+    {
+        unsigned int nHashesDone = 0;
+        uint256 hash;
+        while (true)
+        {
+            hash = block.GetHash();
+            if (hash <= hashTarget)
+            {
+                // Found a solution
+                LogPrintf("%s:\n",__func__);
+                LogPrintf("proof-of-work found  \n  hash: %s  \ntarget: %s\n", hash, hashTarget);
+                return true;
+            }
+            block.nNonce += 1;
+            nHashesDone += 1;
+            if ((block.nNonce & 0xFF) == 0)
+                break;
+        }
+
+        // Check for stop or if block needs to be rebuilt
+        boost::this_thread::interruption_point();
+
+        if (block.nNonce >= 0xffff0000)
+            break;
+        if (GetTime() - nStart > 60)
+            break;
+        if (blocktemplate.previousBlockIndex != chain_.Tip())
+            break;
+
+        // Update nTime every few seconds
+        UpdateTime(block, blocktemplate.previousBlockIndex);
+        if (chainParameters_.AllowMinDifficultyBlocks())
+        {
+            // Changing block->nTime can change work required on testnet:
+            hashTarget.SetCompact(block.nBits);
+        }
+    }
     return false;
 }
 

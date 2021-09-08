9c1ee4e24cf290e5a99072572889781c3b8033c0
galpHub <the.talion@gmail.com>
2021-09-08 14:39:06 -0600

    Drop unused variables

--- divi/src/MasternodeBroadcastFactory.cpp
+++ divi/src/MasternodeBroadcastFactory.cpp
@@ -204,9 +204,6 @@ bool CMasternodeBroadcastFactory::Create(
 {
     const bool collateralPrivateKeyIsRemote = false;
     std::string strService = configEntry.getIp();
-    std::string strKeyMasternode = configEntry.getPrivKey();
-    std::string strTxHash = configEntry.getTxHash();
-    std::string strOutputIndex = configEntry.getOutputIndex();
 
     CTxIn txin;
     std::pair<CKey,CPubKey> masternodeCollateralKeyPair;

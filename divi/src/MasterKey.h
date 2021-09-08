351ef55fbfc00bf75237e440e17e42e7627a88eb
galpHub <the.talion@gmail.com>
2021-09-08 08:21:53 -0600

    Move CMasterKey into separate file

---
+++ divi/src/MasterKey.h
@@ -0,0 +1,40 @@
+#ifndef MASTER_KEY_H
+#define MASTER_KEY_H
+#include <vector>
+#include <serialize.h>
+/** Master key for wallet encryption */
+class CMasterKey
+{
+public:
+    std::vector<unsigned char> vchCryptedKey;
+    std::vector<unsigned char> vchSalt;
+    //! 0 = EVP_sha512()
+    //! 1 = scrypt()
+    unsigned int nDerivationMethod;
+    unsigned int nDeriveIterations;
+    //! Use this for more parameters to key derivation,
+    //! such as the various parameters to scrypt
+    std::vector<unsigned char> vchOtherDerivationParameters;
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion)
+    {
+        READWRITE(vchCryptedKey);
+        READWRITE(vchSalt);
+        READWRITE(nDerivationMethod);
+        READWRITE(nDeriveIterations);
+        READWRITE(vchOtherDerivationParameters);
+    }
+
+    CMasterKey()
+    {
+        // 25000 rounds is just under 0.1 seconds on a 1.86 GHz Pentium M
+        // ie slightly lower than the lowest hardware we need bother supporting
+        nDeriveIterations = 25000;
+        nDerivationMethod = 0;
+        vchOtherDerivationParameters = std::vector<unsigned char>(0);
+    }
+};
+#endif// MASTER_KEY_H

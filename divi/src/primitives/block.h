52f38d5f46c826837b3083ff47c7a14082a24a10
galpHub <the.talion@gmail.com>
2021-09-08 10:06:40 -0600

    Fix confusing indentation

--- divi/src/primitives/block.h
+++ divi/src/primitives/block.h
@@ -109,8 +109,10 @@ public:
     inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
         READWRITE(*(CBlockHeader*)this);
         READWRITE(vtx);
-	if(vtx.size() > 1 && vtx[1].IsCoinStake())
-		READWRITE(vchBlockSig);
+        if(vtx.size() > 1 && vtx[1].IsCoinStake())
+        {
+            READWRITE(vchBlockSig);
+        }
     }
 
     void SetNull()

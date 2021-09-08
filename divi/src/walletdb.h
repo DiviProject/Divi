0c69f04ea2644234264b7357fc4c1d486fe046ab
galpHub <the.talion@gmail.com>
2021-09-08 07:15:21 -0600

    Move I_WalletLoader into separate file

--- divi/src/walletdb.h
+++ divi/src/walletdb.h
@@ -30,6 +30,7 @@ class uint160;
 class uint256;
 class BlockMap;
 class CChain;
+class I_WalletLoader;
 using WalletTxVector = std::vector<CWalletTx>;
 
 /** Error statuses for the wallet database */
@@ -84,29 +85,6 @@ public:
         hdkeypath = "";
         hdchainid = "";
     }
-};
-
-class CAddressBookData;
-class I_WalletLoader
-{
-public:
-    virtual void LoadWalletTransaction(const CWalletTx& wtxIn) = 0;
-    virtual bool LoadWatchOnly(const CScript& dest) = 0;
-    virtual bool LoadMinVersion(int nVersion) = 0;
-    virtual bool LoadMultiSig(const CScript& dest) = 0;
-    virtual bool LoadKey(const CKey& key, const CPubKey& pubkey) = 0;
-    virtual bool LoadMasterKey(unsigned int masterKeyIndex, CMasterKey& masterKey) = 0;
-    virtual bool LoadCryptedKey(const CPubKey& vchPubKey, const std::vector<unsigned char>& vchCryptedSecret) = 0;
-    virtual bool LoadKeyMetadata(const CPubKey& pubkey, const CKeyMetadata& metadata, const bool updateFirstKeyTimestamp) = 0;
-    virtual bool SetDefaultKey(const CPubKey& vchPubKey, bool updateDatabase) = 0;
-    virtual void LoadKeyPool(int nIndex, const CKeyPool &keypool) = 0;
-    virtual bool LoadCScript(const CScript& redeemScript) = 0;
-    virtual void UpdateNextTransactionIndexAvailable(int64_t transactionIndex) = 0;
-    virtual bool SetHDChain(const CHDChain& chain, bool memonly) = 0;
-    virtual bool SetCryptedHDChain(const CHDChain& chain, bool memonly) = 0;
-    virtual bool LoadHDPubKey(const CHDPubKey &hdPubKey) = 0;
-    virtual void ReserializeTransactions(const std::vector<uint256>& transactionIDs) = 0;
-    virtual CAddressBookData& ModifyAddressBookData(const CTxDestination& address) = 0;
 };
 
 /** Access to the wallet database (wallet.dat) */

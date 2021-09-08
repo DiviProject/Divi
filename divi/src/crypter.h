604c80b0fcfa5b9e3a402403de3d24d40fee83e7
galpHub <the.talion@gmail.com>
2021-09-08 08:21:53 -0600

    Move CMasterKey into separate file

--- divi/src/crypter.h
+++ divi/src/crypter.h
@@ -8,6 +8,7 @@
 #include "allocators.h"
 #include "keystore.h"
 #include "serialize.h"
+#include <MasterKey.h>
 
 class uint256;
 
@@ -29,42 +30,6 @@ const unsigned int WALLET_CRYPTO_IV_SIZE = 16;
  * with the double-sha256 of the public key as the IV, and the
  * master key's key as the encryption key (see keystore.[ch]).
  */
-
-/** Master key for wallet encryption */
-class CMasterKey
-{
-public:
-    std::vector<unsigned char> vchCryptedKey;
-    std::vector<unsigned char> vchSalt;
-    //! 0 = EVP_sha512()
-    //! 1 = scrypt()
-    unsigned int nDerivationMethod;
-    unsigned int nDeriveIterations;
-    //! Use this for more parameters to key derivation,
-    //! such as the various parameters to scrypt
-    std::vector<unsigned char> vchOtherDerivationParameters;
-
-    ADD_SERIALIZE_METHODS;
-
-    template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion)
-    {
-        READWRITE(vchCryptedKey);
-        READWRITE(vchSalt);
-        READWRITE(nDerivationMethod);
-        READWRITE(nDeriveIterations);
-        READWRITE(vchOtherDerivationParameters);
-    }
-
-    CMasterKey()
-    {
-        // 25000 rounds is just under 0.1 seconds on a 1.86 GHz Pentium M
-        // ie slightly lower than the lowest hardware we need bother supporting
-        nDeriveIterations = 25000;
-        nDerivationMethod = 0;
-        vchOtherDerivationParameters = std::vector<unsigned char>(0);
-    }
-};
 
 typedef std::vector<unsigned char, secure_allocator<unsigned char> > CKeyingMaterial;
 

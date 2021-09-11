ed844f874244f580aeab531ee9513882dcf2fe05
galpHub <the.talion@gmail.com>
2021-09-11 13:35:59 -0600

    Fix test to explicitly mine the timelocked transaction

--- divi/qa/rpc-tests/AtomicTrading.py
+++ divi/qa/rpc-tests/AtomicTrading.py
@@ -34,18 +34,13 @@ class AtomicTradingTest (BitcoinTestFramework):
 
         raise AssertionError ("failed to find destination address")
 
-    def buildSpend (self, node, output, address, secret, lockTime=0):
-        """Builds a transaction that spends the given output.  It uses
-        the passed secret in the scriptSig, and the pubkey/signature
-        associated to the address to sign with the given node (which
-        must know the private key to the address)."""
-
+    def buildSpendingTx(self,node,output,address,lockTime =0):
         (txid, n) = output
         outpoint = COutPoint (txid=txid, n=n)
 
         tx = CTransaction ()
         tx.nLockTime = lockTime
-        tx.vin.append (CTxIn (outpoint));
+        tx.vin.append (CTxIn (outpoint))
         tx.vout.append (CTxOut (1, CScript ([OP_META])))
 
         prevout = node.gettxout (txid, n)
@@ -57,24 +52,38 @@ class AtomicTradingTest (BitcoinTestFramework):
         }]
 
         privkeys = [node.dumpprivkey (address)]
+        signedTx = FromHex(CTransaction(), node.signrawtransaction (ToHex (tx), prevtx, privkeys)["hex"])
+        return signedTx
 
-        signed = node.signrawtransaction (ToHex (tx), prevtx, privkeys)
+    def appendSecretToSpendingTx(self,signedTx,secrets = {}):
+        for inputIndex in secrets:
+            scriptSig = [x for x in CScript(signedTx.vin[inputIndex].scriptSig)]
+            scriptSig[2] = secrets[inputIndex]
+            signedTx.vin[inputIndex].scriptSig = CScript(scriptSig)
+        return signedTx
+
+    def buildSpend (self, node, output, address, secret, lockTime=0):
+        """Builds a transaction that spends the given output.  It uses
+        the passed secret in the scriptSig, and the pubkey/signature
+        associated to the address to sign with the given node (which
+        must know the private key to the address)."""
+
+        signed = self.buildSpendingTx(node,output,address,lockTime)
         # It is not necessarily the case that signed["complete"] is true
         # here, as the solver does not have the secret preimage.  It will
         # add the right signatures, though, so then we just need to fill
         # in the secret.
-
-        sigTx = FromHex (CTransaction (), signed["hex"])
-        sig = [x for x in CScript (sigTx.vin[0].scriptSig)]
-        sig[2] = secret
-        tx.vin[0].scriptSig = CScript (sig)
-
+        tx = self.appendSecretToSpendingTx(signed,{0:secret})
         return tx
 
     def expectInvalid (self, tx):
         """Expects that a given transaction is invalid."""
         assert_raises (JSONRPCException, self.nodes[0].generateblock,
                        {"extratx": [ToHex (tx)]})
+
+    def expectValidAndMine (self, tx):
+        """Expects that a given transaction is valid."""
+        self.nodes[0].generateblock({"extratx": [ToHex (tx)]})
 
     def run_test (self):
         set_node_times (self.nodes, ACTIVATION_TIME)
@@ -151,12 +160,14 @@ class AtomicTradingTest (BitcoinTestFramework):
         # Bump the block height to after the time lock.  Now addr[0] can
         # spend the output (with a wrong secret, since otherwise
         # the codepath for addr[1] activates).
-        self.nodes[0].setgenerate (True, 100)
+        numberOfBlocks = 99 - self.nodes[0].getblockcount()
+        self.nodes[0].setgenerate (True, numberOfBlocks)
         sync_blocks (self.nodes)
         tx = self.buildSpend (self.nodes[0], outputs[1], addr[0], b"wrong",
                               lockTime=100)
-        self.nodes[0].sendrawtransaction (ToHex (tx))
+        self.expectInvalid(tx)
         self.nodes[0].setgenerate (True, 1)
+        self.expectValidAndMine(tx)
 
         # Both outputs should now really be spent.
         for txid, n in outputs:

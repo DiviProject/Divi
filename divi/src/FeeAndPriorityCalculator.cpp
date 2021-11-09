98450a02bc412f13a25e00843f9528e90090f69e
galpHub <the.talion@gmail.com>
2021-11-09 14:25:04 -0700

    Move modified size calculation into mempool entry class

--- divi/src/FeeAndPriorityCalculator.cpp
+++ divi/src/FeeAndPriorityCalculator.cpp
@@ -44,21 +44,3 @@ bool FeeAndPriorityCalculator::IsDust(const CTxOut& txout) const
 {
     return txout.nValue < MinimumValueForNonDust(txout);
 }
-
-unsigned int FeeAndPriorityCalculator::CalculateModifiedSize(const CTransaction& tx, unsigned int nTxSize) const
-{
-    // In order to avoid disincentivizing cleaning up the UTXO set we don't count
-    // the constant overhead for each txin and up to 110 bytes of scriptSig (which
-    // is enough to cover a compressed pubkey p2sh redemption) for priority.
-    // Providing any more cleanup incentive than making additional inputs free would
-    // risk encouraging people to create junk outputs to redeem later.
-    if (nTxSize == 0)
-        nTxSize = tx.GetSerializeSize(SER_NETWORK, PROTOCOL_VERSION);
-    for (std::vector<CTxIn>::const_iterator it(tx.vin.begin()); it != tx.vin.end(); ++it)
-    {
-        unsigned int offset = 41U + std::min(110U, (unsigned int)it->scriptSig.size());
-        if (nTxSize > offset)
-            nTxSize -= offset;
-    }
-    return nTxSize;
-}
--- divi/src/FeeAndPriorityCalculator.h
+++ divi/src/FeeAndPriorityCalculator.h
@@ -27,7 +27,6 @@ public:
     CAmount MinimumValueForNonDust() const;
     CAmount MinimumValueForNonDust(const CTxOut& txout) const;
     bool IsDust(const CTxOut& txout) const;
-    unsigned int CalculateModifiedSize(const CTransaction& tx, unsigned int nTxSize = 0) const;
 };
 
 #endif //FEE_AND_PRIORITY_CALCULATOR

df66645b77df3b01d2d3c6e00d18f51b27dff2df
galpHub <the.talion@gmail.com>
2021-11-09 14:25:04 -0700

    Move modified size calculation into mempool entry class

--- divi/src/MemPoolEntry.cpp
+++ divi/src/MemPoolEntry.cpp
@@ -1,10 +1,25 @@
 #include <MemPoolEntry.h>
 
 #include <serialize.h>
-#include <FeeAndPriorityCalculator.h>
 #include <version.h>
 
 const unsigned int CTxMemPoolEntry::MEMPOOL_HEIGHT = 0x7FFFFFFF;
+
+static unsigned int CalculateModifiedSize(const CTransaction& tx, unsigned int nTxSize)
+{
+    // In order to avoid disincentivizing cleaning up the UTXO set we don't count
+    // the constant overhead for each txin and up to 110 bytes of scriptSig (which
+    // is enough to cover a compressed pubkey p2sh redemption) for priority.
+    // Providing any more cleanup incentive than making additional inputs free would
+    // risk encouraging people to create junk outputs to redeem later.
+    for (std::vector<CTxIn>::const_iterator it(tx.vin.begin()); it != tx.vin.end(); ++it)
+    {
+        unsigned int offset = 41U + std::min(110U, (unsigned int)it->scriptSig.size());
+        if (nTxSize > offset)
+            nTxSize -= offset;
+    }
+    return nTxSize;
+}
 
 CTxMemPoolEntry::CTxMemPoolEntry(
     const CTransaction& _tx,
@@ -21,7 +36,7 @@ CTxMemPoolEntry::CTxMemPoolEntry(
     , nHeight(_nHeight)
 {
     nTxSize = ::GetSerializeSize(tx, SER_NETWORK, PROTOCOL_VERSION);
-    nModSize = FeeAndPriorityCalculator::instance().CalculateModifiedSize(tx,nTxSize);
+    nModSize = CalculateModifiedSize(tx,nTxSize);
     initialCoinAgePerByteOfInputs = nModSize? _initialCoinAgeOfInputs/ nModSize: 0.0;
 }
 

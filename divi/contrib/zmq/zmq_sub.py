c5ea906642199d0eab2d2d40647bca2a6963d866
galpHub <the.talion@gmail.com>
2021-11-22 10:44:03 -0700

    Drop ZMQ tx lock notifications

--- divi/contrib/zmq/zmq_sub.py
+++ divi/contrib/zmq/zmq_sub.py
@@ -11,10 +11,8 @@ zmqContext = zmq.Context()
 zmqSubSocket = zmqContext.socket(zmq.SUB)
 zmqSubSocket.setsockopt(zmq.SUBSCRIBE, b"hashblock")
 zmqSubSocket.setsockopt(zmq.SUBSCRIBE, b"hashtx")
-zmqSubSocket.setsockopt(zmq.SUBSCRIBE, b"hashtxlock")
 zmqSubSocket.setsockopt(zmq.SUBSCRIBE, b"rawblock")
 zmqSubSocket.setsockopt(zmq.SUBSCRIBE, b"rawtx")
-zmqSubSocket.setsockopt(zmq.SUBSCRIBE, b"rawtxlock")
 zmqSubSocket.connect("tcp://127.0.0.1:%i" % port)
 
 try:
@@ -34,17 +32,11 @@ try:
         elif topic == "hashtx":
             print ('- HASH TX ('+sequence+') -')
             print(binascii.hexlify(body).decode("utf-8"))
-        elif topic == "hashtxlock":
-            print('- HASH TX LOCK ('+sequence+') -')
-            print(binascii.hexlify(body).decode("utf-8"))
         elif topic == "rawblock":
             print('- RAW BLOCK HEADER ('+sequence+') -')
             print(binascii.hexlify(body[:80]).decode("utf-8"))
         elif topic == "rawtx":
             print('- RAW TX ('+sequence+') -')
-            print(binascii.hexlify(body).decode("utf-8"))
-        elif topic == "rawtxlock":
-            print('- RAW TX LOCK ('+sequence+') -')
             print(binascii.hexlify(body).decode("utf-8"))
 
 except KeyboardInterrupt:

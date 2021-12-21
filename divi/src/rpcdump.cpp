802fa6ce5db9a963040353ca9fd795a3c165c39b
galpHub <the.talion@gmail.com>
2021-12-20 17:28:45 -0700

    Remove dump encoding methods

--- divi/src/rpcdump.cpp
+++ divi/src/rpcdump.cpp
@@ -42,53 +42,6 @@ extern CChain chainActive;
 
 void EnsureWalletIsUnlocked();
 
-std::string static EncodeDumpTime(int64_t nTime)
-{
-    return DateTimeStrFormat("%Y-%m-%dT%H:%M:%SZ", nTime);
-}
-
-int64_t static DecodeDumpTime(const std::string& str)
-{
-    static const boost::posix_time::ptime epoch = boost::posix_time::from_time_t(0);
-    static const std::locale loc(std::locale::classic(),
-        new boost::posix_time::time_input_facet("%Y-%m-%dT%H:%M:%SZ"));
-    std::istringstream iss(str);
-    iss.imbue(loc);
-    boost::posix_time::ptime ptime(boost::date_time::not_a_date_time);
-    iss >> ptime;
-    if (ptime.is_not_a_date_time())
-        return 0;
-    return (ptime - epoch).total_seconds();
-}
-
-std::string static EncodeDumpString(const std::string& str)
-{
-    std::stringstream ret;
-    BOOST_FOREACH (unsigned char c, str) {
-        if (c <= 32 || c >= 128 || c == '%') {
-            ret << '%' << HexStr(&c, &c + 1);
-        } else {
-            ret << c;
-        }
-    }
-    return ret.str();
-}
-
-std::string DecodeDumpString(const std::string& str)
-{
-    std::stringstream ret;
-    for (unsigned int pos = 0; pos < str.length(); pos++) {
-        unsigned char c = str[pos];
-        if (c == '%' && pos + 2 < str.length()) {
-            c = (((str[pos + 1] >> 6) * 9 + ((str[pos + 1] - '0') & 15)) << 4) |
-                ((str[pos + 2] >> 6) * 9 + ((str[pos + 2] - '0') & 15));
-            pos += 2;
-        }
-        ret << c;
-    }
-    return ret.str();
-}
-
 Value importprivkey(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() < 1 || params.size() > 3)

c32b2bd221dd5fe2cd26743cb1f0528da8353bed
galpHub <the.talion@gmail.com>
2022-01-03 14:46:25 -0700

    Add global method for syncing array of transactions

--- divi/src/NotificationInterface.cpp
+++ divi/src/NotificationInterface.cpp
@@ -34,6 +34,7 @@ MainNotificationSignals& NotificationInterfaceRegistry::getSignals() const
 void NotificationInterface::RegisterWith(MainNotificationSignals& signals){
     signals.UpdatedBlockTip.connect(boost::bind(&NotificationInterface::UpdatedBlockTip, this, _1));
     signals.SyncTransaction.connect(boost::bind(&NotificationInterface::SyncTransaction, this, _1, _2,_3));
+    signals.SyncTransactions.connect(boost::bind(&NotificationInterface::SyncTransactions, this, _1, _2,_3));
     signals.SetBestChain.connect(boost::bind(&NotificationInterface::SetBestChain, this, _1));
 }
 
@@ -41,4 +42,5 @@ void NotificationInterface::UnregisterWith(MainNotificationSignals& signals) {
     signals.UpdatedBlockTip.disconnect(boost::bind(&NotificationInterface::UpdatedBlockTip, this, _1));
     signals.SetBestChain.disconnect(boost::bind(&NotificationInterface::SetBestChain, this, _1));
     signals.SyncTransaction.disconnect(boost::bind(&NotificationInterface::SyncTransaction, this, _1, _2,_3));
+    signals.SyncTransactions.disconnect(boost::bind(&NotificationInterface::SyncTransactions, this, _1, _2,_3));
 }

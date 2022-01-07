1f5d9c58ea6ac7b6f69f58e2d926f6838075c64c
galpHub <the.talion@gmail.com>
2022-01-07 09:18:30 -0700

    Move blockfile flushing into block file helpers namespace

--- divi/src/BlockFileHelpers.cpp
+++ divi/src/BlockFileHelpers.cpp
@@ -1,1 +1,27 @@
 #include <BlockFileHelpers.h>
+
+#include <sync.h>
+#include <util.h>
+#include <BlockDiskPosition.h>
+#include <BlockFileOpener.h>
+
+void BlockFileHelpers::FlushBlockFile(int nLastBlockFile, const std::vector<CBlockFileInfo>& vinfoBlockFile, bool fFinalize)
+{
+    CDiskBlockPos posOld(nLastBlockFile, 0);
+
+    FILE* fileOld = OpenBlockFile(posOld);
+    if (fileOld) {
+        if (fFinalize)
+            TruncateFile(fileOld, vinfoBlockFile[nLastBlockFile].nSize);
+        FileCommit(fileOld);
+        fclose(fileOld);
+    }
+
+    fileOld = OpenUndoFile(posOld);
+    if (fileOld) {
+        if (fFinalize)
+            TruncateFile(fileOld, vinfoBlockFile[nLastBlockFile].nUndoSize);
+        FileCommit(fileOld);
+        fclose(fileOld);
+    }
+}

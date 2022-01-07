abdedde0158410363944790bb6d3b7ab1182fdf6
galpHub <the.talion@gmail.com>
2022-01-07 06:50:01 -0700

    Rename file

--- divi/src/BlockFileInfo.cpp
+++ divi/src/BlockFileInfo.cpp
@@ -3,7 +3,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <blockFileInfo.h>
+#include <BlockFileInfo.h>
 #include <tinyformat.h>
 #include <utiltime.h>
 

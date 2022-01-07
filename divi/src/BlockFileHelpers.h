8aa3ae51386dd535b20929301d2492a62de0544c
galpHub <the.talion@gmail.com>
2022-01-07 09:18:30 -0700

    Move blockfile flushing into block file helpers namespace

--- divi/src/BlockFileHelpers.h
+++ divi/src/BlockFileHelpers.h
@@ -1,3 +1,10 @@
 #ifndef BLOCK_FILE_HELPERS_H
 #define BLOCK_FILE_HELPERS_H
+#include <vector>
+#include <BlockFileInfo.h>
+
+namespace BlockFileHelpers
+{
+    void FlushBlockFile(int nLastBlockFile, const std::vector<CBlockFileInfo>& vinfoBlockFile, bool fFinalize = false);
+};
 #endif// BLOCK_FILE_HELPERS_H

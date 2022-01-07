c71fba2003bdd1ec1900cc83d2cde3e1b432540b
galpHub <the.talion@gmail.com>
2022-01-07 09:30:23 -0700

    Move AllocateDiskSpaceForBlockUndo into block helpers

--- divi/src/BlockFileHelpers.cpp
+++ divi/src/BlockFileHelpers.cpp
@@ -4,6 +4,8 @@
 #include <util.h>
 #include <BlockDiskPosition.h>
 #include <BlockFileOpener.h>
+#include <defaultValues.h>
+#include <BlockDiskAccessor.h>
 
 void BlockFileHelpers::FlushBlockFile(int nLastBlockFile, const std::vector<CBlockFileInfo>& vinfoBlockFile, bool fFinalize)
 {
@@ -24,4 +26,39 @@ void BlockFileHelpers::FlushBlockFile(int nLastBlockFile, const std::vector<CBlo
         FileCommit(fileOld);
         fclose(fileOld);
     }
+}
+
+bool BlockFileHelpers::AllocateDiskSpaceForBlockUndo(
+    int nFile,
+    std::set<int>& setDirtyFileInfo,
+    std::vector<CBlockFileInfo>& vinfoBlockFile,
+    CDiskBlockPos& pos,
+    unsigned int nAddSize)
+{
+    pos.nFile = nFile;
+
+    unsigned int nNewSize;
+    pos.nPos = vinfoBlockFile[nFile].nUndoSize;
+    vinfoBlockFile[nFile].nUndoSize += nAddSize;
+    nNewSize = vinfoBlockFile[nFile].nUndoSize;
+    setDirtyFileInfo.insert(nFile);
+
+    unsigned int nOldChunks = (pos.nPos + UNDOFILE_CHUNK_SIZE - 1) / UNDOFILE_CHUNK_SIZE;
+    unsigned int nNewChunks = (nNewSize + UNDOFILE_CHUNK_SIZE - 1) / UNDOFILE_CHUNK_SIZE;
+    if (nNewChunks > nOldChunks) {
+        if (CheckDiskSpace(nNewChunks * UNDOFILE_CHUNK_SIZE - pos.nPos)) {
+            FILE* file = OpenUndoFile(pos);
+            if (file) {
+                LogPrintf("Pre-allocating up to position 0x%x in rev%05u.dat\n", nNewChunks * UNDOFILE_CHUNK_SIZE, pos.nFile);
+                AllocateFileRange(file, pos.nPos, nNewChunks * UNDOFILE_CHUNK_SIZE - pos.nPos);
+                fclose(file);
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
--- divi/src/BlockFileHelpers.h
+++ divi/src/BlockFileHelpers.h
@@ -1,10 +1,18 @@
 #ifndef BLOCK_FILE_HELPERS_H
 #define BLOCK_FILE_HELPERS_H
 #include <vector>
+#include <set>
 #include <BlockFileInfo.h>
 
+class CDiskBlockPos;
 namespace BlockFileHelpers
 {
     void FlushBlockFile(int nLastBlockFile, const std::vector<CBlockFileInfo>& vinfoBlockFile, bool fFinalize = false);
+    bool AllocateDiskSpaceForBlockUndo(
+        int nFile,
+        std::set<int>& setDirtyFileInfo,
+        std::vector<CBlockFileInfo>& vinfoBlockFile,
+        CDiskBlockPos& pos,
+        unsigned int nAddSize);
 };
 #endif// BLOCK_FILE_HELPERS_H

b10e737ba21b5e4dc9d648b3f7dfc61fd00586ae
galpHub <the.talion@gmail.com>
2022-01-12 13:20:49 -0700

    Replace actual class dependency with interface

--- divi/src/WalletBalanceCalculator.h
+++ divi/src/WalletBalanceCalculator.h
@@ -1,17 +1,17 @@
 #ifndef WALLET_BALANCE_CALCULATOR_H
 #define WALLET_BALANCE_CALCULATOR_H
 class I_AppendOnlyTransactionRecord;
-class SpentOutputTracker;
+class I_SpentOutputTracker;
 class I_MerkleTxConfirmationNumberCalculator;
 class WalletBalanceCalculator
 {
     const I_AppendOnlyTransactionRecord& txRecord_;
-    const SpentOutputTracker& spentOutputTracker_;
+    const I_SpentOutputTracker& spentOutputTracker_;
     const I_MerkleTxConfirmationNumberCalculator& confsCalculator_;
 public:
     WalletBalanceCalculator(
         const I_AppendOnlyTransactionRecord& txRecord,
-        const SpentOutputTracker& spentOutputTracker,
+        const I_SpentOutputTracker& spentOutputTracker,
         const I_MerkleTxConfirmationNumberCalculator& confsCalculator);
     ~WalletBalanceCalculator();
 };

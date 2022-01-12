0d688a2c06b15650b4d381ace9701b542fee6908
galpHub <the.talion@gmail.com>
2022-01-12 10:03:59 -0700

    Move interface for transcation record into separate file

--- divi/src/WalletTransactionRecord.h
+++ divi/src/WalletTransactionRecord.h
@@ -1,19 +1,7 @@
 #ifndef WALLET_TRANSACTION_RECORD_H
 #define WALLET_TRANSACTION_RECORD_H
-#include <uint256.h>
-#include <WalletTx.h>
-#include <map>
+#include <I_AppendOnlyTransactionRecord.h>
 class CCriticalSection;
-
-class I_AppendOnlyTransactionRecord
-{
-public:
-    virtual ~I_AppendOnlyTransactionRecord() {}
-    virtual const CWalletTx* GetWalletTx(const uint256& hash) const = 0;
-    virtual const std::map<uint256, CWalletTx>& GetWalletTransactions() const = 0;
-    virtual std::pair<std::map<uint256, CWalletTx>::iterator, bool> AddTransaction(const CWalletTx& newlyAddedTransaction) = 0;
-    virtual unsigned size() const = 0;
-};
 
 class WalletTransactionRecord final: public I_AppendOnlyTransactionRecord
 {

071cdcfc7231fb479b3a9f490333a5163bb4d403
galpHub <the.talion@gmail.com>
2022-01-12 21:11:18 -0700

    Fix potential inheritance issue when using nicemocks

--- divi/src/test/MockUtxoOwnershipDetector.h
+++ divi/src/test/MockUtxoOwnershipDetector.h
@@ -2,7 +2,7 @@
 #define MOCK_UTXO_OWNERSHIP_DETECTOR_H
 #include <gmock/gmock.h>
 #include <I_UtxoOwnershipDetector.h>
-class MockUtxoOwnershipDetector final: public I_UtxoOwnershipDetector
+class MockUtxoOwnershipDetector: public I_UtxoOwnershipDetector
 {
 public:
     MOCK_CONST_METHOD1(isMine, isminetype(const CTxOut& output));

35740904018231b718ba1c70e8fe1905b1590959
galpHub <the.talion@gmail.com>
2022-01-12 20:41:13 -0700

    Add helper functions for creating random transactions

---
+++ divi/src/test/RandomTransactionGenerator.h
@@ -0,0 +1,27 @@
+#ifndef RANDOM_TRANSACTION_GENERATOR_H
+#define RANDOM_TRANSACTION_GENERATOR_H
+#include <amount.h>
+#include <RandomScriptPubKeyGenerator.h>
+#include <primitives/transaction.h>
+#include <random.h>
+class RandomTransactionGenerator
+{
+public:
+    CTransaction operator()(CAmount maxAmount = 1*COIN, const unsigned inputCount = GetRandInt(15), const unsigned outputCount = GetRandInt(15)) const
+    {
+        RandomScriptPubKeyGenerator scriptGenerator;
+        CMutableTransaction tx;
+        for(unsigned inputIndex = 0u; inputIndex < inputCount; ++inputIndex)
+        {
+            tx.vin.emplace_back(GetRandHash(), inputIndex);
+        }
+        for(unsigned outputIndex =0u; outputIndex < outputCount; ++outputIndex)
+        {
+            const bool useP2PKH = static_cast<bool>(GetRandInt(8) % 2);
+            tx.vout.emplace_back(GetRand(maxAmount),scriptGenerator(useP2PKH?ScriptType::P2PKH:ScriptType::P2SH ));
+        }
+        return tx;
+    }
+};
+
+#endif// RANDOM_TRANSACTION_GENERATOR_H

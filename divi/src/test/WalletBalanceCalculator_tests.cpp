eb8c1dfdea15329383ce924edea390ea668b8a36
galpHub <the.talion@gmail.com>
2022-01-18 13:14:10 -0700

    Add test for immature coinbase transaction

--- divi/src/test/WalletBalanceCalculator_tests.cpp
+++ divi/src/test/WalletBalanceCalculator_tests.cpp
@@ -246,4 +246,19 @@ BOOST_AUTO_TEST_CASE(unconfirmedCoinbaseTransactionsDoNotContributeToBalance)
     BOOST_CHECK_EQUAL(calculator.getBalance(),CAmount(0));
 }
 
+BOOST_AUTO_TEST_CASE(confirmedButImmatureCoinbaseTransactionsDoNotContributeToBalance)
+{
+    CMutableTransaction txTemplate = RandomTransactionGenerator()();
+    txTemplate.vin.resize(1);
+    txTemplate.vin[0].prevout.SetNull();
+    CTransaction tx = txTemplate;
+    assert(tx.IsCoinBase());
+    addTransactionToMockWalletRecord(tx);
+    ON_CALL(confsCalculator,GetBlocksToMaturity(_)).WillByDefault(Return(1));
+    ON_CALL(confsCalculator,GetNumberOfBlockConfirmations(_)).WillByDefault(Return(1));
+    ON_CALL(utxoOwnershipDetector,isMine(_)).WillByDefault(Return(isminetype::ISMINE_SPENDABLE));
+    ON_CALL(spentOutputTracker,IsSpent(_,_,_)).WillByDefault(Return(false));
+    BOOST_CHECK_EQUAL(calculator.getBalance(),CAmount(0));
+}
+
 BOOST_AUTO_TEST_SUITE_END()

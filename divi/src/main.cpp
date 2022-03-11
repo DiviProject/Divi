df159e2fe71620b7fdb6d0d9b29cdefb13ad8508
galpHub <the.talion@gmail.com>
2022-03-11 13:06:52 -0700

    Replace function body by ResolveConflictsBetweenCoinDBAndBlockDB call

--- divi/src/main.cpp
+++ divi/src/main.cpp
@@ -2224,68 +2224,9 @@ bool static LoadBlockIndexState(string& strError)
         const unsigned int expectedNumberOfBlockIndices = BlockFileHelpers::GetLastBlockHeightWrittenIntoLastBlockFile() + 1;
         if (heightSortedBlockIndices.size() > expectedNumberOfBlockIndices)
         {
-            const auto mit = blockMap.find(coinsTip.GetBestBlock());
-            if (mit == blockMap.end())
+            if(!ResolveConflictsBetweenCoinDBAndBlockDB(heightSortedBlockIndices,blockMap,coinsTip,strError))
             {
-                strError = "The wallet has been not been closed gracefully, causing the transaction database to be out of sync with the block database";
                 return false;
-            }
-            const int coinsHeight = mit->second->nHeight;
-            const int blockIndexHeight = (heightSortedBlockIndices.size()>0)? heightSortedBlockIndices.back().first: 0;
-            LogPrintf("%s : pcoinstip synced to block height %d, block index height %d\n", __func__, coinsHeight, blockIndexHeight);
-            assert(coinsHeight <= blockIndexHeight);
-            if(coinsHeight < blockIndexHeight)
-            {
-                //The database is in a state where a block has been accepted and written to disk, but the
-                //transaction database (pcoinsTip) was not flushed to disk, and is therefore not in sync with
-                //the block index database.
-                auto nextBlockCandidate =
-                    std::find_if(heightSortedBlockIndices.begin(),heightSortedBlockIndices.end(),
-                        [coinsHeight](const std::pair<int,CBlockIndex*>& heightAndBlockIndex) -> bool
-                        {
-                            return heightAndBlockIndex.first > coinsHeight;
-                        });
-                auto endNextBlockCandidate = heightSortedBlockIndices.end();
-
-                // Start at the last block that was successfully added to the txdb (pcoinsTip) and manually add all transactions that occurred for each block up until
-                // the best known block from the block index db.
-                CCoinsViewCache view(&coinsTip);
-                int lastProcessedHeight = -1;
-                while (nextBlockCandidate != endNextBlockCandidate)
-                {
-                    const CBlockIndex* pindex = nextBlockCandidate->second;
-                    if(pindex->pprev && view.GetBestBlock() != pindex->pprev->GetBlockHash())
-                    {
-                        ++nextBlockCandidate;
-                        continue;
-                    }
-                    if(lastProcessedHeight==pindex->nHeight)
-                    {
-                        // Duplicate blocks at the same height let the rest sort themselves out?
-                        break;
-                    }
-                    lastProcessedHeight = pindex->nHeight;
-                    CBlock block;
-                    if (!ReadBlockFromDisk(block, pindex)) {
-                        strError = "The wallet has been not been closed gracefully and has caused corruption of blocks stored to disk. Data directory is in an unusable state";
-                        return false;
-                    }
-
-                    uint256 hashBlock = block.GetHash();
-                    for (unsigned int i = 0; i < block.vtx.size(); i++) {
-                        CTxUndo undoDummy;
-                        view.UpdateWithConfirmedTransaction(block.vtx[i], pindex->nHeight, undoDummy);
-                        view.SetBestBlock(hashBlock);
-                    }
-                    ++nextBlockCandidate;
-                }
-
-                // Save the updates to disk
-                if (!view.Flush() || !coinsTip.Flush())
-                    LogPrintf("%s : failed to flush view\n", __func__);
-
-                //get the index associated with the point in the chain that pcoinsTip is synced to
-                LogPrintf("%s : pcoinstip=%d %s\n", __func__, coinsHeight, coinsTip.GetBestBlock());
             }
         }
         if(settings.ParameterIsSet("-safe_shutdown"))

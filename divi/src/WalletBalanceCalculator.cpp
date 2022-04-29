708dd40d8c90c3ab5aeb2da31524287769ac132c
galpHub <the.talion@gmail.com>
2022-04-29 10:28:28 -0600

    Conflicted transactions never count towards balance

--- divi/src/WalletBalanceCalculator.cpp
+++ divi/src/WalletBalanceCalculator.cpp
@@ -48,6 +48,7 @@ CAmount WalletBalanceCalculator::calculateBalance(BalanceFlag flag) const
     {
         const CWalletTx& tx = txidAndTransaction.second;
         const int depth = confsCalculator_.GetNumberOfBlockConfirmations(tx);
+        if(depth < 0) continue;
         const bool txIsBlockReward = tx.IsCoinStake() || tx.IsCoinBase();
         const bool needsAtLeastOneConfirmation = txIsBlockReward || (flag != BalanceFlag::UNCONFIRMED && !debitsFunds(ownershipDetector_,transactionsByHash,tx));
         if( depth < (needsAtLeastOneConfirmation? 1: 0)) continue;

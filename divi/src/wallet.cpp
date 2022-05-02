277449147d54fb7548b79fa526b2bea953358194
galpHub <the.talion@gmail.com>
2022-05-02 10:17:35 -0600

    Inline single use variables

--- divi/src/wallet.cpp
+++ divi/src/wallet.cpp
@@ -92,16 +92,14 @@ bool CheckFinalTx(const CTransaction& tx, const CChain& activeChain)
     // evaluated is what is used. Thus if we want to know if a
     // transaction can be part of the *next* block, we need to call
     // IsFinalTx() with one more than activeChain_.Height().
-    const int nBlockHeight = activeChain.Height() + 1;
 
     // BIP113 will require that time-locked transactions have nLockTime set to
     // less than the median time of the previous block they're contained in.
     // When the next block is created its previous block will be the current
     // chain tip, so we use that to calculate the median time passed to
     // IsFinalTx() if LOCKTIME_MEDIAN_TIME_PAST is set.
-    const int64_t nBlockTime = GetAdjustedTime();
 
-    return IsFinalTx(tx, activeChain, nBlockHeight, nBlockTime);
+    return IsFinalTx(tx, activeChain, activeChain.Height() + 1, GetAdjustedTime());
 }
 
 bool IsAvailableType(const CKeyStore& keystore, const CScript& scriptPubKey, AvailableCoinsType coinType, isminetype& mine,VaultType& vaultType)

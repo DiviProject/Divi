71488e83961d5442d68ae0ed3333f72ed63c73d1
galpHub <the.talion@gmail.com>
2022-05-09 13:56:22 -0600

    Duplicate tx filter-and-apply logic templated interface

--- divi/src/WalletBalanceCalculator.h
+++ divi/src/WalletBalanceCalculator.h
@@ -6,6 +6,37 @@ class I_AppendOnlyTransactionRecord;
 class I_SpentOutputTracker;
 class I_MerkleTxConfirmationNumberCalculator;
 class I_UtxoOwnershipDetector;
+class CWalletTx;
+
+template<typename CalculationResult>
+class FilteredTransactionsCalculator
+{
+private:
+    const I_AppendOnlyTransactionRecord& txRecord_;
+    const I_MerkleTxConfirmationNumberCalculator& confsCalculator_;
+protected:
+    virtual void calculate(const CWalletTx& transaction, const UtxoOwnershipFilter& ownershipFilter, CalculationResult& intermediateResult) const = 0;
+public:
+    FilteredTransactionsCalculator(
+        const I_AppendOnlyTransactionRecord& txRecord,
+        const I_MerkleTxConfirmationNumberCalculator& confsCalculator);
+    ~FilteredTransactionsCalculator();
+
+    enum TxFlag
+    {
+        UNCONFIRMED = 1 << 0,
+        CONFIRMED = 1 << 1,
+        IMMATURE = 1 << 2,
+        MATURE = 1 << 3,
+        CONFIRMED_AND_MATURE = CONFIRMED | MATURE,
+        CONFIRMED_AND_IMMATURE = CONFIRMED | IMMATURE,
+    };
+    void applyCalculationToMatchingTransactions(
+        TxFlag flag,
+        const UtxoOwnershipFilter& ownershipFilter,
+        CalculationResult& initialValue) const;
+};
+
 class WalletBalanceCalculator
 {
 private:

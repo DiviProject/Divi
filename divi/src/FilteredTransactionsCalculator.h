462282db9f3d476d4dae85a3b3a1f984e7cf9494
galpHub <the.talion@gmail.com>
2022-05-11 15:18:10 -0600

    Make template class header-only

--- divi/src/FilteredTransactionsCalculator.h
+++ divi/src/FilteredTransactionsCalculator.h
@@ -1,8 +1,8 @@
 #ifndef FILTERED_TRANSACTION_CALCULATOR_H
 #define FILTERED_TRANSACTION_CALCULATOR_H
-class I_AppendOnlyTransactionRecord;
-class I_MerkleTxConfirmationNumberCalculator;
-class CWalletTx;
+#include <WalletTx.h>
+#include <I_AppendOnlyTransactionRecord.h>
+#include <I_MerkleTxConfirmationNumberCalculator.h>
 class UtxoOwnershipFilter;
 
 template<typename CalculationResult>
@@ -33,4 +33,46 @@ public:
         const UtxoOwnershipFilter& ownershipFilter,
         CalculationResult& initialValue) const;
 };
+
+template <typename CalculationResult>
+FilteredTransactionsCalculator<CalculationResult>::FilteredTransactionsCalculator(
+    const I_AppendOnlyTransactionRecord& txRecord,
+    const I_MerkleTxConfirmationNumberCalculator& confsCalculator
+    ): txRecord_(txRecord)
+    , confsCalculator_(confsCalculator)
+{
+}
+
+template <typename CalculationResult>
+FilteredTransactionsCalculator<CalculationResult>::~FilteredTransactionsCalculator()
+{
+}
+
+template <typename CalculationResult>
+void FilteredTransactionsCalculator<CalculationResult>::applyCalculationToMatchingTransactions(
+    TxFlag flag,
+    const UtxoOwnershipFilter& ownershipFilter,
+    CalculationResult& initialValue) const
+{
+    assert((flag & TxFlag::UNCONFIRMED & TxFlag::CONFIRMED) == 0);
+    assert((flag & TxFlag::IMMATURE & TxFlag::MATURE) == 0);
+    // Only block rewards need non trivial maturity, and unconfirmed coin rewards are therefore conflicted
+    assert((flag & TxFlag::UNCONFIRMED & TxFlag::IMMATURE) == 0);
+    // Unconfirmed transactions that are not conflicted are mature.
+    assert((flag & TxFlag::UNCONFIRMED & TxFlag::MATURE) == 0);
+    for(const auto& txidAndTransaction: txRecord_.GetWalletTransactions())
+    {
+        const CWalletTx& tx = txidAndTransaction.second;
+        const int depth = confsCalculator_.GetNumberOfBlockConfirmations(tx);
+        if(depth < 0) continue;
+        if(depth < 1 && (tx.IsCoinStake() || tx.IsCoinBase())) continue;
+        if( (flag & TxFlag::UNCONFIRMED) > 0 && depth != 0) continue;
+        if( (flag & TxFlag::CONFIRMED) > 0 && depth < 1) continue;
+        if( (flag & TxFlag::MATURE) > 0 && confsCalculator_.GetBlocksToMaturity(tx) > 0) continue;
+        if( (flag & TxFlag::IMMATURE) > 0 && confsCalculator_.GetBlocksToMaturity(tx) == 0) continue;
+
+        calculate(tx,depth, ownershipFilter,initialValue);
+    }
+}
+
 #endif//FILTERED_TRANSACTION_CALCULATOR_H

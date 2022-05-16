8ba7a2e36548c86285ab50c8b8cf758258861caa
galpHub <the.talion@gmail.com>
2022-05-16 11:54:28 -0600

    Remove confusing parsing strategy difference between cli and rpc

--- divi/src/rpcclient.h
+++ divi/src/rpcclient.h
@@ -6,10 +6,7 @@
 #ifndef BITCOIN_RPCCLIENT_H
 #define BITCOIN_RPCCLIENT_H
 
-#include "json/json_spirit_reader_template.h"
-#include "json/json_spirit_utils.h"
-#include "json/json_spirit_writer_template.h"
+#include "json/json_spirit_value.h"
 
 json_spirit::Array RPCConvertValues(const std::string& strMethod, const std::vector<std::string>& strParams);
-
 #endif // BITCOIN_RPCCLIENT_H

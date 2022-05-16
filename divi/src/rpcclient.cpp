67d98f7aec9d14ace3389ad389ec514ab8033173
galpHub <the.talion@gmail.com>
2022-05-16 11:54:28 -0600

    Remove confusing parsing strategy difference between cli and rpc

--- divi/src/rpcclient.cpp
+++ divi/src/rpcclient.cpp
@@ -7,153 +7,21 @@
 
 #include "rpcclient.h"
 
-#include "rpcprotocol.h"
-#include "ui_interface.h"
+#include "json/json_spirit_reader_template.h"
+#include "json/json_spirit_utils.h"
+#include "json/json_spirit_writer_template.h"
 
-#include <set>
 #include <stdint.h>
 
-using namespace std;
-using namespace json_spirit;
-
-class CRPCConvertParam
-{
-public:
-    std::string methodName; //! method whose params want conversion
-    int paramIdx;           //! 0-based idx of param to convert
-};
-// ***TODO***
-static const CRPCConvertParam vRPCConvertParams[] =
-    {
-        {"stop", 0},
-        {"setmocktime", 0},
-        {"getaddednodeinfo", 0},
-        {"setgenerate", 0},
-        {"generateblock", 0},
-        {"sendtoaddress", 1},
-        {"getcoinavailability", 0},
-        {"getlotteryblockwinners",0},
-        {"reclaimvaultfunds", 1},
-        {"fundvault", 0},
-        {"fundvault", 1},
-        {"getreceivedbyaddress", 1},
-        {"getreceivedbyaccount", 1},
-        {"listreceivedbyaddress", 0},
-        {"listreceivedbyaddress", 1},
-        {"listreceivedbyaddress", 2},
-        {"listreceivedbyaccount", 0},
-        {"listreceivedbyaccount", 1},
-        {"listreceivedbyaccount", 2},
-        {"getbalance", 1},
-        {"getbalance", 2},
-        {"getblockhash", 0},
-        {"move", 2},
-        {"move", 3},
-        {"sendfrom", 2},
-        {"listtransactions", 1},
-        {"listtransactions", 2},
-        {"listtransactions", 3},
-        {"listaccounts", 0},
-        {"listaccounts", 1},
-        {"walletpassphrase", 1},
-        {"walletpassphrase", 2},
-        {"getblocktemplate", 0},
-        {"listsinceblock", 1},
-        {"listsinceblock", 2},
-        {"sendmany", 1},
-        {"sendmany", 2},
-        {"addmultisigaddress", 0},
-        {"addmultisigaddress", 1},
-        {"createmultisig", 0},
-        {"createmultisig", 1},
-        {"listunspent", 0},
-        {"listunspent", 1},
-        {"listunspent", 2},
-        {"getblock", 1},
-        {"getblockheader", 1},
-        {"gettransaction", 1},
-        {"getrawtransaction", 1},
-        {"createrawtransaction", 0},
-        {"createrawtransaction", 1},
-        {"signrawtransaction", 1},
-        {"signrawtransaction", 2},
-        {"sendrawtransaction", 1},
-        {"getlotteryblockwinners",0},
-        {"gettxout", 1},
-        {"gettxout", 2},
-        {"lockunspent", 0},
-        {"lockunspent", 1},
-        {"importprivkey", 2},
-        {"importaddress", 2},
-        {"verifychain", 0},
-        {"verifychain", 1},
-        {"keypoolrefill", 0},
-        {"getrawmempool", 0},
-        {"prioritisetransaction", 1},
-        {"prioritisetransaction", 2},
-        {"mnbudget", 3},
-        {"mnbudget", 4},
-        {"mnbudget", 6},
-        {"mnbudget", 8},
-        {"preparebudget", 2},
-        {"preparebudget", 3},
-        {"preparebudget", 5},
-        {"submitbudget", 2},
-        {"submitbudget", 3},
-        {"submitbudget", 5},
-        {"submitbudget", 7},
-        // disabled until removal of the legacy 'masternode' command
-        {"startmasternode", 1},
-        {"mnvoteraw", 1},
-        {"mnvoteraw", 4},
-
-        {"getaddresstxids", 0},
-        {"getaddresstxids", 1},
-        {"getaddressdeltas", 0},
-        {"getaddressdeltas", 1},
-        {"getaddressbalance", 0},
-        {"getaddressbalance", 1},
-        {"getaddressutxos", 0},
-        {"getaddressutxos", 1},
-        {"getspentinfo", 0}
-    };
-
-class CRPCConvertTable
-{
-private:
-    std::set<std::pair<std::string, int> > members;
-
-public:
-    CRPCConvertTable();
-
-    bool convert(const std::string& method, int idx)
-    {
-        return (members.count(std::make_pair(method, idx)) > 0);
-    }
-};
-
-CRPCConvertTable::CRPCConvertTable()
-{
-    const unsigned int n_elem =
-        (sizeof(vRPCConvertParams) / sizeof(vRPCConvertParams[0]));
-
-    for (unsigned int i = 0; i < n_elem; i++) {
-        members.insert(std::make_pair(vRPCConvertParams[i].methodName,
-            vRPCConvertParams[i].paramIdx));
-    }
-}
-
-static CRPCConvertTable rpcCvtTable;
-
 /** Convert strings to command-specific RPC representation */
-Array RPCConvertValues(const std::string& strMethod, const std::vector<std::string>& strParams)
+json_spirit::Array RPCConvertValues(const std::string& strMethod, const std::vector<std::string>& strParams)
 {
-    Array params;
+    json_spirit::Array params;
 
     for (unsigned int idx = 0; idx < strParams.size(); idx++) {
         const std::string& strVal = strParams[idx];
 
-        Value jVal;
+        json_spirit::Value jVal;
         if (!read_string(strVal, jVal))
         {
             params.push_back(strVal);

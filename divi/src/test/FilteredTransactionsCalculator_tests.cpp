d61a3600977c0d84db678b51c52ccb7dd819b437
galpHub <the.talion@gmail.com>
2022-05-17 13:57:49 -0600

    Check that rewards need at least one confirmation always

--- divi/src/test/FilteredTransactionsCalculator_tests.cpp
+++ divi/src/test/FilteredTransactionsCalculator_tests.cpp
@@ -17,23 +17,104 @@ using ::testing::Ref;
 class FilteredTransactionsCalculatorTestFixture
 {
 public:
+    CWalletTx tx_;
+    std::map<uint256, CWalletTx> walletTransactions;
+
     MockTransactionRecord txRecord;
     MockMerkleTxConfirmationNumberCalculator confsCalculator;
     MockUtxoBalanceCalculator utxoBalanceCalculator;
     FilteredTransactionsCalculator<CAmount> txBalancesCalculator;
     FilteredTransactionsCalculatorTestFixture(
-        ): txRecord()
+        ): tx_()
+        , walletTransactions()
+        , txRecord()
         , confsCalculator()
         , utxoBalanceCalculator()
         , txBalancesCalculator(txRecord,confsCalculator,utxoBalanceCalculator)
     {
     }
+
+    void setWalletTransactionRecordToSingleTx()
+    {
+        walletTransactions.clear();
+        tx_ = RandomTransactionGenerator()();
+        walletTransactions[tx_.GetHash()] = tx_;
+        assert(!tx_.IsCoinBase() && !tx_.IsCoinStake());
+    }
+    void setWalletTransactionRecordToSingleCoinbaseTx()
+    {
+        walletTransactions.clear();
+        tx_ = RandomTransactionGenerator()();
+        tx_.vin.resize(1);
+        tx_.vin[0].prevout.SetNull();
+
+        walletTransactions[tx_.GetHash()] = tx_;
+        assert(tx_.IsCoinBase());
+    }
+    void setWalletTransactionRecordToSingleCoinstakeTx()
+    {
+        walletTransactions.clear();
+        tx_ = RandomTransactionGenerator()();
+        CTxOut emptyOutput;
+        emptyOutput.SetEmpty();
+        tx_.vout.insert(tx_.vout.begin(),emptyOutput);
+
+        walletTransactions[tx_.GetHash()] = tx_;
+        assert(tx_.IsCoinStake());
+    }
 };
 
 BOOST_FIXTURE_TEST_SUITE(FilteredTransactionCalculatorTests, FilteredTransactionsCalculatorTestFixture)
 
-BOOST_AUTO_TEST_CASE(testFixtureWorks)
+BOOST_AUTO_TEST_CASE(willApplyCalculationToConfirmedTransactions)
 {
+    setWalletTransactionRecordToSingleTx();
+    ON_CALL(txRecord,GetWalletTransactions()).WillByDefault(ReturnRef(walletTransactions));
+    ON_CALL(confsCalculator, GetNumberOfBlockConfirmations(_)).WillByDefault(Return(1));
+    ON_CALL(confsCalculator, GetBlocksToMaturity(_)).WillByDefault(Return(GetRandInt(1)));
+    EXPECT_CALL(utxoBalanceCalculator,calculate(_,_,_,_)).Times(1);
+
+    UtxoOwnershipFilter filter;
+    CAmount totalBalance = 0;
+    txBalancesCalculator.applyCalculationToMatchingTransactions(TxFlag::CONFIRMED,filter,totalBalance);
+}
+
+BOOST_AUTO_TEST_CASE(willApplyCalculationToUnconfirmedTransactionThatIsntReward)
+{
+    setWalletTransactionRecordToSingleTx();
+    ON_CALL(txRecord,GetWalletTransactions()).WillByDefault(ReturnRef(walletTransactions));
+    ON_CALL(confsCalculator, GetNumberOfBlockConfirmations(_)).WillByDefault(Return(0));
+    ON_CALL(confsCalculator, GetBlocksToMaturity(_)).WillByDefault(Return(GetRandInt(1)));
+    EXPECT_CALL(utxoBalanceCalculator,calculate(_,_,_,_)).Times(1);
+
+    UtxoOwnershipFilter filter;
+    CAmount totalBalance = 0;
+    txBalancesCalculator.applyCalculationToMatchingTransactions(TxFlag::UNCONFIRMED,filter,totalBalance);
+}
+
+BOOST_AUTO_TEST_CASE(willNotApplyCalculationToUnconfirmedCoinbaseTransactions)
+{
+    setWalletTransactionRecordToSingleCoinbaseTx();
+    ON_CALL(txRecord,GetWalletTransactions()).WillByDefault(ReturnRef(walletTransactions));
+    ON_CALL(confsCalculator, GetNumberOfBlockConfirmations(_)).WillByDefault(Return(0));
+    ON_CALL(confsCalculator, GetBlocksToMaturity(_)).WillByDefault(Return(GetRandInt(1)));
+    EXPECT_CALL(utxoBalanceCalculator,calculate(_,_,_,_)).Times(0);
+
+    UtxoOwnershipFilter filter;
+    CAmount totalBalance = 0;
+    txBalancesCalculator.applyCalculationToMatchingTransactions(TxFlag::UNCONFIRMED,filter,totalBalance);
+}
+BOOST_AUTO_TEST_CASE(willNotApplyCalculationToUnconfirmedCoinstakeTransactions)
+{
+    setWalletTransactionRecordToSingleCoinstakeTx();
+    ON_CALL(txRecord,GetWalletTransactions()).WillByDefault(ReturnRef(walletTransactions));
+    ON_CALL(confsCalculator, GetNumberOfBlockConfirmations(_)).WillByDefault(Return(0));
+    ON_CALL(confsCalculator, GetBlocksToMaturity(_)).WillByDefault(Return(GetRandInt(1)));
+    EXPECT_CALL(utxoBalanceCalculator,calculate(_,_,_,_)).Times(0);
+
+    UtxoOwnershipFilter filter;
+    CAmount totalBalance = 0;
+    txBalancesCalculator.applyCalculationToMatchingTransactions(TxFlag::UNCONFIRMED,filter,totalBalance);
 }
 
 BOOST_AUTO_TEST_SUITE_END()

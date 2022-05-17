81646fc68a193421b4a161c5b6da85c46bc1c46b
galpHub <the.talion@gmail.com>
2022-05-17 11:17:11 -0600

    Move UtxoBalanceCalculator into separate file

---
+++ divi/src/UtxoBalanceCalculator.h
@@ -0,0 +1,22 @@
+#ifndef UTXO_BALANCE_CALCULATOR_H
+#define UTXO_BALANCE_CALCULATOR_H
+#include <I_TransactionDetailCalculator.h>
+#include <amount.h>
+class I_UtxoOwnershipDetector;
+class I_SpentOutputTracker;
+class UtxoBalanceCalculator final: public I_TransactionDetailCalculator<CAmount>
+{
+private:
+    const I_UtxoOwnershipDetector& ownershipDetector_;
+    const I_SpentOutputTracker& spentOutputTracker_;
+public:
+    UtxoBalanceCalculator(
+        const I_UtxoOwnershipDetector& ownershipDetector,
+        const I_SpentOutputTracker& spentOutputTracker);
+    void calculate(
+        const CWalletTx& walletTransaction,
+        const int txDepth,
+        const UtxoOwnershipFilter& ownershipFilter,
+        CAmount& intermediateBalance) const override;
+};
+#endif// UTXO_BALANCE_CALCULATOR_H

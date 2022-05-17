94178137a2997427fbd0d3198e74223687de7909
galpHub <the.talion@gmail.com>
2022-05-17 12:00:32 -0600

    Test that spent utxos dont count towards balance

--- divi/src/test/UtxoBalanceCalculator_tests.cpp
+++ divi/src/test/UtxoBalanceCalculator_tests.cpp
@@ -5,6 +5,9 @@
 #include <MockSpentOutputTracker.h>
 #include <WalletTx.h>
 
+#include <random.h>
+#include <RandomTransactionGenerator.h>
+
 using ::testing::NiceMock;
 using ::testing::Invoke;
 using ::testing::Return;
@@ -28,8 +31,15 @@ public:
 
 BOOST_FIXTURE_TEST_SUITE(UtxoBalanceCalculatorTests, UtxoBalanceCalculatorTestFixture)
 
-BOOST_AUTO_TEST_CASE(testFixtureWorks)
+BOOST_AUTO_TEST_CASE(willIgnoreAllSpentUtxosEvenIfAllAreOwned)
 {
+    ON_CALL(utxoOwnershipDetector,isMine(_)).WillByDefault(Return(isminetype::ISMINE_SPENDABLE));
+    ON_CALL(spentOutputTracker,IsSpent(_,_,_)).WillByDefault(Return(true));
+    UtxoOwnershipFilter filter(isminetype::ISMINE_SPENDABLE);
+    CWalletTx tx = RandomTransactionGenerator()();
+    CAmount totalBalance = 0;
+    calculator.calculate(tx,GetRandInt(100),filter,totalBalance);
+    BOOST_CHECK_EQUAL(totalBalance, 0*COIN);
 }
 
 BOOST_AUTO_TEST_SUITE_END()

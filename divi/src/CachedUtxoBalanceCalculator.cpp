ac8ee4cb230291e7e9340712a78593b5967e778b
galpHub <the.talion@gmail.com>
2022-05-18 07:30:08 -0600

    Move cached utxo balance calculator implementation to separate file

---
+++ divi/src/CachedUtxoBalanceCalculator.cpp
@@ -0,0 +1,36 @@
+#include <CachedUtxoBalanceCalculator.h>
+
+#include <WalletTx.h>
+#include <IsMineType.h>
+
+CachedUtxoBalanceCalculator::CachedUtxoBalanceCalculator(
+    const I_TransactionDetailCalculator<CAmount>& utxoBalanceCalculator
+    ): utxoBalanceCalculator_(utxoBalanceCalculator)
+    , balanceCache_()
+{
+}
+
+void CachedUtxoBalanceCalculator::calculate(
+    const CWalletTx& walletTransaction,
+    const int txDepth,
+    const UtxoOwnershipFilter& ownershipFilter,
+    CAmount& intermediateBalance) const
+{
+    auto txid = walletTransaction.GetHash();
+    if(balanceCache_.count(txid) > 0 && balanceCache_[txid].count(ownershipFilter.underlyingBitMask()))
+    {
+        intermediateBalance += balanceCache_[txid][ownershipFilter.underlyingBitMask()];
+    }
+    else
+    {
+        CAmount balanceFromOutputs = 0;
+        utxoBalanceCalculator_.calculate(walletTransaction,txDepth,ownershipFilter,balanceFromOutputs);
+        balanceCache_[txid][ownershipFilter.underlyingBitMask()] = balanceFromOutputs;
+        intermediateBalance += balanceFromOutputs;
+    }
+}
+
+void CachedUtxoBalanceCalculator::recomputeCachedTxEntries(const CWalletTx& walletTransaction) const
+{
+    balanceCache_.erase(walletTransaction.GetHash());
+}

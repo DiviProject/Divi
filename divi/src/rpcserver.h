3ec5235c0f053f726956351b462185aadd6331c6
galpHub <the.talion@gmail.com>
2022-05-30 09:32:41 -0600

    Extract accepted connection interface into separate file

--- divi/src/rpcserver.h
+++ divi/src/rpcserver.h
@@ -24,16 +24,6 @@
 class CBlockIndex;
 class CNetAddr;
 
-class AcceptedConnection
-{
-public:
-    virtual ~AcceptedConnection() {}
-
-    virtual std::iostream& stream() = 0;
-    virtual std::string peer_address_to_string() const = 0;
-    virtual void close() = 0;
-};
-
 /** Start RPC threads */
 void StartRPCThreads();
 /**

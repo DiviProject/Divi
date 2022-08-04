df998b0804edb7522427547980bcf36e2c6e168a
galpHub <the.talion@gmail.com>
2022-08-04 14:43:47 -0600

    Inline check for input ownership

--- divi/src/wallet.cpp
+++ divi/src/wallet.cpp
@@ -412,7 +412,17 @@ bool CWallet::AllInputsAreMine(const CWalletTx& walletTransaction) const
 {
     bool allInputsAreMine = true;
     for (const CTxIn& txin : walletTransaction.vin) {
-        isminetype mine = isMine(txin);
+        isminetype mine = isminetype::ISMINE_NO;
+        {
+            LOCK(cs_wallet);
+            const CWalletTx* txPtr = GetWalletTx(txin.prevout.hash);
+            if (txPtr != nullptr)
+            {
+                const CWalletTx& prev = *txPtr;
+                if (txin.prevout.n < prev.vout.size())
+                    mine = isMine(prev.vout[txin.prevout.n]);
+            }
+        }
         allInputsAreMine &= static_cast<bool>(mine == isminetype::ISMINE_SPENDABLE);
     }
     return allInputsAreMine;

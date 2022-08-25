15130bab8d749dce9506c752d05e552bb268d0bc
galpHub <the.talion@gmail.com>
2022-08-25 14:34:41 -0600

    Move BlockSubmitter into separate files

---
+++ divi/src/BlockSubmitter.cpp
@@ -0,0 +1,50 @@
+#include <BlockSubmitter.h>
+
+#include <sync.h>
+#include <ChainstateManager.h>
+#include <primitives/block.h>
+#include <main.h>
+#include <Logging.h>
+#include <utilmoneystr.h>
+#include <spork.h>
+#include <chain.h>
+#include <ValidationState.h>
+
+extern CCriticalSection cs_main;
+
+BlockSubmitter::BlockSubmitter(const CSporkManager& sporkManager): sporkManager_(sporkManager)
+{
+
+}
+
+bool BlockSubmitter::IsBlockValidChainExtension(CBlock* pblock) const
+{
+    {
+        LOCK(cs_main);
+        const ChainstateManager::Reference chainstate;
+        if (pblock->hashPrevBlock != chainstate->ActiveChain().Tip()->GetBlockHash())
+            return error("%s : generated block is stale",__func__);
+    }
+    return true;
+}
+
+bool BlockSubmitter::submitBlockForChainExtension(CBlock& block) const
+{
+    LogPrintf("%s\n", block);
+    LogPrintf("generated %s\n", FormatMoney(block.vtx[0].vout[0].nValue));
+
+    ChainstateManager::Reference chainstate;
+
+    // Process this block the same as if we had received it from another node
+    CValidationState state;
+    if (!IsBlockValidChainExtension(&block) || !ProcessNewBlock(*chainstate, sporkManager_, state, NULL, &block))
+        return error("%s : block not accepted",__func__);
+
+    return true;
+}
+
+bool BlockSubmitter::loadBlockForChainExtension(CValidationState& state, CBlock& block, CDiskBlockPos* blockfilePositionData) const
+{
+    ChainstateManager::Reference chainstate;
+    return ProcessNewBlock(*chainstate, sporkManager_, state, NULL, &block, blockfilePositionData);
+}

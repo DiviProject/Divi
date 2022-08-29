12659aaabae3a24e9d458fed015cf79517064013
galpHub <the.talion@gmail.com>
2022-08-29 15:02:56 -0600

    Move block invalidation helpers into separate file

---
+++ divi/src/BlockInvalidationHelpers.h
@@ -0,0 +1,35 @@
+#ifndef BLOCK_INVALIDATION_HELPERS_H
+#define BLOCK_INVALIDATION_HELPERS_H
+#include <uint256.h>
+#include <NodeId.h>
+#include <map>
+#include <BlockCheckingHelpers.h>
+
+class ChainstateManager;
+class CValidationState;
+class CBlockIndex;
+class Settings;
+class CCriticalSection;
+class I_ChainTipManager;
+
+BlockIndexCandidates& GetBlockIndexCandidates();
+void InvalidChainFound(
+    const bool isInitialBlockDownload,
+    const Settings& settings,
+    CCriticalSection& mainCriticalSection,
+    CBlockIndex* pindexNew);
+bool InvalidateBlock(
+    const I_ChainTipManager& chainTipManager,
+    const bool isInitialBlockDownload,
+    const Settings& settings,
+    CCriticalSection& mainCriticalSection,
+    ChainstateManager& chainstate,
+    CBlockIndex* pindex);
+void InvalidBlockFound(
+    const std::map<uint256, NodeId>& peerIdByBlockHash,
+    const bool isInitialBlockDownload,
+    const Settings& settings,
+    CCriticalSection& mainCriticalSection,
+    CBlockIndex* pindex,
+    const CValidationState& state);
+#endif// BLOCK_INVALIDATION_HELPERS_H

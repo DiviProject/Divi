ec77a9e77753dabca2a6e933bc824ce7a743af9c
galpHub <the.talion@gmail.com>
2022-09-06 15:34:07 -0600

    Reorder methods

--- divi/src/I_BlockValidator.h
+++ divi/src/I_BlockValidator.h
@@ -15,8 +15,8 @@ class I_BlockValidator
 {
 public:
     virtual ~I_BlockValidator() {}
-    virtual std::pair<CBlockIndex*, bool> validateAndAssignBlockIndex(const NodeAndBlockDiskPosition& nodeAndBlockDisk, CBlock& block, CValidationState& state) const = 0;
     virtual bool connectActiveChain(const CBlock& block, CValidationState& state) const = 0;
     virtual bool checkBlockRequirements(const NodeAndBlockDiskPosition& nodeAndBlockDisk, const CBlock& block, CValidationState& state) const = 0;
+    virtual std::pair<CBlockIndex*, bool> validateAndAssignBlockIndex(const NodeAndBlockDiskPosition& nodeAndBlockDisk, CBlock& block, CValidationState& state) const = 0;
 };
 #endif// I_BLOCK_VALIDATOR_H

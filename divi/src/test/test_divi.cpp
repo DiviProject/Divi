e975be337f3287db2f7b55a167e9ea67199985cc
galpHub <the.talion@gmail.com>
2022-09-09 23:30:05 -0600

    Simplify test harness setup

--- divi/src/test/test_divi.cpp
+++ divi/src/test/test_divi.cpp
@@ -3,74 +3,34 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #define BOOST_TEST_MODULE Divi Test Suite
+#include <boost/test/unit_test.hpp>
 
 #include <chainparams.h>
 #include <dbenv.h>
 #include <init.h>
-#include <I_ChainExtensionService.h>
-#include <Logging.h>
-#include <noui.h>
-#include <random.h>
-#include <Settings.h>
-#include <tinyformat.h>
-#include <TransactionInputChecker.h>
-#include <util.h>
-#include <utiltime.h>
-
-#include <boost/filesystem.hpp>
-#include <boost/test/unit_test.hpp>
-#include <boost/thread.hpp>
-
-extern Settings& settings;
-
-/* The global spork-manager instance is normally managed by init.cpp (where it
-   is also defined).  For unit tests, we override the init sequence, and
-   manually construct / destruct the instance.  Since code depends on
-   GetSporkManager() to retrieve the global instance, need to actually use
-   that one rather than e.g. an instance in the test fixture.
-
-   FIXME: Get rid of GetSporkManager(), pass the instance everywhere, and then
-   make the instance part of the test class.  */
-extern void RegisterNodeSignals();
-extern void UnregisterNodeSignals();
+#include <main.h>
 
 struct TestingSetup {
-    boost::filesystem::path pathTemp;
-    boost::thread_group threadGroup;
-    CDBEnv& bitdb_;
-
-    TestingSetup(): pathTemp(), threadGroup(), bitdb_(BerkleyDBEnvWrapper())
+    CDBEnv& berkleyEnvironment_;
+    TestingSetup(): berkleyEnvironment_(BerkleyDBEnvWrapper())
     {
-        SetupEnvironment();
-        setWriteToDebugLogFlag(false);
-        settings.SetParameter("-checkblockindex","1");
-        SelectParams(CBaseChainParams::UNITTEST);
-        noui_connect();
 #ifdef ENABLE_WALLET
-        bitdb_.MakeMock();
+        berkleyEnvironment_.MakeMock();
 #endif
-        pathTemp = GetTempPath() / strprintf("test_divi_%lu_%i", (unsigned long)GetTime(), (int)(GetRand(100000)));
-        boost::filesystem::create_directories(pathTemp);
-        settings.SetParameter("-datadir", pathTemp.string());
+        SelectParams(CBaseChainParams::UNITTEST);
         InitializeMainBlockchainModules();
         InitializeMultiWalletModule();
-        GetChainExtensionService().connectGenesisBlock();
-        TransactionInputChecker::SetScriptCheckingThreadCount(3);
-        TransactionInputChecker::InitializeScriptCheckingThreads(threadGroup);
         RegisterNodeSignals();
         EnableUnitTestSignals();
     }
     ~TestingSetup()
     {
-        threadGroup.interrupt_all();
-        threadGroup.join_all();
         UnregisterNodeSignals();
         FinalizeMultiWalletModule();
         FinalizeMainBlockchainModules();
 #ifdef ENABLE_WALLET
-        bitdb_.Flush(true);
+        berkleyEnvironment_.Flush(true);
 #endif
-        boost::filesystem::remove_all(pathTemp);
     }
 };
 

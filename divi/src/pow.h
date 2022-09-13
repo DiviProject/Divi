ced39e16847059bdf1f89955ea01aeb43c73f36c
galpHub <the.talion@gmail.com>
2022-09-13 07:40:07 -0600

    Drop deprecated enum for difficulty algorithm switching

--- divi/src/pow.h
+++ divi/src/pow.h
@@ -16,14 +16,6 @@ class uint256;
 class arith_uint256;
 class CChainParams;
 
-// Define difficulty retarget algorithms
-enum DiffMode {
-    DIFF_DEFAULT = 0, // Default to invalid 0
-    DIFF_BTC = 1,     // Retarget every x blocks (Bitcoin style)
-    DIFF_KGW = 2,     // Retarget using Kimoto Gravity Well
-    DIFF_DGW = 3,     // Retarget using Dark Gravity Wave v3
-};
-
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CChainParams& chainParameters);
 
 /** Check whether a block hash satisfies the proof-of-work requirement specified by nBits */

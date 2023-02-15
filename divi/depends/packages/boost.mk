d9ca2b5de54468ab1dae1e79a881804a00b1564c
galpHub <the.talion@gmail.com>
2023-02-15 10:07:09 -0700

    Update boost library

--- divi/depends/packages/boost.mk
+++ divi/depends/packages/boost.mk
@@ -1,8 +1,8 @@
 package=boost
-$(package)_version=1_63_0
-$(package)_download_path=https://sourceforge.net/projects/boost/files/boost/1.63.0
+$(package)_version=1_81_0
+$(package)_download_path=https://sourceforge.net/projects/boost/files/boost/1.81.0
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
-$(package)_sha256_hash=beae2529f759f6b3bf3f4969a19c2e9d6f0c503edcb2de4a61d1428519fcb3b0
+$(package)_sha256_hash=71feeed900fbccca04a3b4f2f84a7c217186f28a940ed8b7ed4725986baf99fa
 
 define $(package)_set_vars
 $(package)_config_opts_release=variant=release

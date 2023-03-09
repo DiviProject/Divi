0795275c49962d6d28f01bb5567958dedb082eb4
galpHub <the.talion@gmail.com>
2023-03-09 16:41:44 -0700

    Use BasicCoinselectionAlgorithm  as dependency of MinimumFeeCoinSelectionAlgorithm

--- divi/src/MinimumFeeCoinSelectionAlgorithm.h
+++ divi/src/MinimumFeeCoinSelectionAlgorithm.h
@@ -2,6 +2,8 @@
 #define MINIMUM_FEE_COIN_SELECTION_ALGORITHM_H
 #include <I_CoinSelectionAlgorithm.h>
 #include <I_UtxoPriorityAlgorithm.h>
+#include <memory>
+
 class CKeyStore;
 class I_SignatureSizeEstimator;
 class CFeeRate;
@@ -46,13 +48,14 @@ public:
         CAmount& fees) const override;
 };
 
-class MinimumFeeCoinSelectionAlgorithm final: public I_CoinSelectionAlgorithm
+class MinimumFeeCoinSelectionAlgorithm final: public I_CoinSelectionAlgorithm, private I_UtxoPriorityAlgorithm
 {
 private:
-    const CKeyStore& keyStore_;
-    const I_SignatureSizeEstimator& estimator_;
     const CFeeRate& minRelayTxFee_;
     const ChangeOutputType changeOutputType_;
+    std::unique_ptr<BasicCoinSelectionAlgorithm> basicCoinSelectionAlgorithm_;
+    bool operator()(const CAmount targetAmount, const InputToSpendAndSigSize& first, const InputToSpendAndSigSize& second) const override;
+
 public:
     MinimumFeeCoinSelectionAlgorithm(
         const CKeyStore& keyStore,

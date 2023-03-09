a47932137eac3b9fe0b903d052c75cde7c39e98f
galpHub <the.talion@gmail.com>
2023-03-09 15:10:53 -0700

    Fix interface

--- divi/src/I_UtxoPriorityAlgorithm.h
+++ divi/src/I_UtxoPriorityAlgorithm.h
@@ -1,10 +1,11 @@
 #ifndef I_UTXO_PRIORITY_ALGORITHM_H
 #define I_UTXO_PRIORITY_ALGORITHM_H
+#include <amount.h>
 struct InputToSpendAndSigSize;
 class I_UtxoPriorityAlgorithm
 {
 public:
     virtual ~I_UtxoPriorityAlgorithm(){}
-    virtual bool operator()(const InputToSpendAndSigSize& first, const InputToSpendAndSigSize& second) const = 0;
+    virtual bool operator()(const CAmount targetAmount, const InputToSpendAndSigSize& first, const InputToSpendAndSigSize& second) const = 0;
 };
 #endif //I_UTXO_PRIORITY_ALGORITHM_H

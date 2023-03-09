33dbd414ae21b1100651a352d4fbdaaca5e6eb04
galpHub <the.talion@gmail.com>
2023-03-09 15:17:23 -0700

    Create basic coin selection algorithm class

--- divi/src/MinimumFeeCoinSelectionAlgorithm.cpp
+++ divi/src/MinimumFeeCoinSelectionAlgorithm.cpp
@@ -23,6 +23,90 @@ InputToSpendAndSigSize::InputToSpendAndSigSize(
 {
 }
 
+BasicCoinSelectionAlgorithm::BasicCoinSelectionAlgorithm(
+    const CKeyStore& keyStore,
+    const I_SignatureSizeEstimator& estimator,
+    const CFeeRate& minRelayTxFee,
+    const ChangeOutputType changeOutputType,
+    const I_UtxoPriorityAlgorithm& utxoPriorityAlgorithm
+    ): keyStore_(keyStore)
+    , estimator_(estimator)
+    , minRelayTxFee_(minRelayTxFee)
+    , changeOutputType_(changeOutputType)
+    , utxoPriorityAlgorithm_(utxoPriorityAlgorithm)
+{
+}
+
+std::set<COutput> BasicCoinSelectionAlgorithm::SelectCoins(
+    const CMutableTransaction& transactionToSelectCoinsFor,
+    const std::vector<COutput>& vCoins,
+    CAmount& fees) const
+{
+    const unsigned nominalChangeOutputSize = static_cast<unsigned>(changeOutputType_); // Vault? Vault-Change : P2PKH change address
+    CTransaction initialTransaction = CTransaction(transactionToSelectCoinsFor);
+    const unsigned initialByteSize = ::GetSerializeSize(initialTransaction, SER_NETWORK, PROTOCOL_VERSION);
+
+    CAmount maximumAmountAvailable = 0;
+    std::vector<InputToSpendAndSigSize> inputsToSpendAndSignatureSizeEstimates;
+    inputsToSpendAndSignatureSizeEstimates.reserve(vCoins.size());
+    std::set<COutPoint> outputsUsed;
+    for(const CTxIn& txInput: initialTransaction.vin)
+    {
+        outputsUsed.insert(txInput.prevout);
+    }
+    CAmount amountAlreadyCovered = 0;
+    fees += minRelayTxFee_.GetFee(initialByteSize);
+    for(const COutput& input: vCoins)
+    {
+        if(outputsUsed.count(COutPoint(input.tx->GetHash(),input.i))>0)
+        {
+            InputToSpendAndSigSize inputWithSigSize(input,keyStore_,estimator_);
+            amountAlreadyCovered += inputWithSigSize.outputRef->Value();
+            fees += minRelayTxFee_.GetFee(inputWithSigSize.sigSize);
+            continue;
+        }
+        inputsToSpendAndSignatureSizeEstimates.emplace_back(input,keyStore_,estimator_);
+        maximumAmountAvailable+= input.Value();
+    }
+    const CAmount nTargetValue = transactionToSelectCoinsFor.GetValueOut() - amountAlreadyCovered;
+
+    std::set<COutput> inputsSelected;
+    inputsSelected.clear();
+    const CAmount minimumNoDustChange = FeeAndPriorityCalculator::instance().MinimumValueForNonDust();
+    const CAmount totalAmountNeeded = nTargetValue + minimumNoDustChange + fees;
+    if(totalAmountNeeded > maximumAmountAvailable) return {};
+
+    std::sort(
+        inputsToSpendAndSignatureSizeEstimates.begin(),
+        inputsToSpendAndSignatureSizeEstimates.end(),
+        [totalAmountNeeded, this](const InputToSpendAndSigSize& inputA, const InputToSpendAndSigSize& inputB)
+        {
+            return utxoPriorityAlgorithm_(totalAmountNeeded,inputA,inputB);
+        });
+    CAmount amountCovered =0;
+    unsigned cummulativeByteSize = nominalChangeOutputSize;
+    for(const InputToSpendAndSigSize& inputAndSigSize: inputsToSpendAndSignatureSizeEstimates)
+    {
+        inputsSelected.insert(*inputAndSigSize.outputRef);
+        amountCovered += inputAndSigSize.outputRef->Value() - minRelayTxFee_.GetFee(inputAndSigSize.sigSize);
+        cummulativeByteSize += inputAndSigSize.sigSize;
+        if(cummulativeByteSize >= MAX_STANDARD_TX_SIZE) return {};
+        if( amountCovered >= totalAmountNeeded )
+        {
+            fees += minRelayTxFee_.GetFee(cummulativeByteSize);
+            if(minRelayTxFee_.GetMaxTxFee() < fees)
+            {
+                return {};
+            }
+            else
+            {
+                return inputsSelected;
+            }
+        }
+    }
+    return {};
+}
+
 MinimumFeeCoinSelectionAlgorithm::MinimumFeeCoinSelectionAlgorithm(
     const CKeyStore& keyStore,
     const I_SignatureSizeEstimator& estimator,
--- divi/src/MinimumFeeCoinSelectionAlgorithm.h
+++ divi/src/MinimumFeeCoinSelectionAlgorithm.h
@@ -1,6 +1,7 @@
 #ifndef MINIMUM_FEE_COIN_SELECTION_ALGORITHM_H
 #define MINIMUM_FEE_COIN_SELECTION_ALGORITHM_H
 #include <I_CoinSelectionAlgorithm.h>
+#include <I_UtxoPriorityAlgorithm.h>
 class CKeyStore;
 class I_SignatureSizeEstimator;
 class CFeeRate;
@@ -20,6 +21,29 @@ struct InputToSpendAndSigSize
         const COutput& output,
         const CKeyStore& keyStore,
         const I_SignatureSizeEstimator& estimator);
+};
+
+class BasicCoinSelectionAlgorithm final: public I_CoinSelectionAlgorithm
+{
+private:
+    const CKeyStore& keyStore_;
+    const I_SignatureSizeEstimator& estimator_;
+    const CFeeRate& minRelayTxFee_;
+    const ChangeOutputType changeOutputType_;
+    const I_UtxoPriorityAlgorithm& utxoPriorityAlgorithm_;
+
+public:
+    BasicCoinSelectionAlgorithm(
+        const CKeyStore& keyStore,
+        const I_SignatureSizeEstimator& estimator,
+        const CFeeRate& minRelayTxFee,
+        const ChangeOutputType changeOutputType,
+        const I_UtxoPriorityAlgorithm& utxoPriorityAlgorithm);
+    bool isSelectable(const COutput& coin) const override { return true; };
+    std::set<COutput> SelectCoins(
+        const CMutableTransaction& transactionToSelectCoinsFor,
+        const std::vector<COutput>& vCoins,
+        CAmount& fees) const override;
 };
 
 class MinimumFeeCoinSelectionAlgorithm final: public I_CoinSelectionAlgorithm

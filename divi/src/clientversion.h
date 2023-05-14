7648bfa7f940e07414a18c6b6e478d18ffba1d30
galpHub <the.talion@gmail.com>
2023-05-14 07:15:15 -0600

    Update copyright year

--- divi/src/clientversion.h
+++ divi/src/clientversion.h
@@ -16,7 +16,7 @@
 #define CLIENT_VERSION_BUILD 0
 
 #define CLIENT_VERSION_IS_RELEASE true		// Todo: !! Set to true for release, false for prerelease or test build
-#define COPYRIGHT_YEAR 2018					// Todo: update this when changing our copyright comments in the source
+#define COPYRIGHT_YEAR 2023					// Todo: update this when changing our copyright comments in the source
 #endif //HAVE_CONFIG_H
 
 /**
